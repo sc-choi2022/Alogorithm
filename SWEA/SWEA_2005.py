f80102aa5873fe9ae5f5640a15783930397b30a4
sc-choi2022 <c.martin20222023@gmail.com>
2022-02-22 22:30:57 +0900

    0222

--- SWEA/SWEA_2005.py
+++ SWEA/SWEA_2005.py
@@ -4,16 +4,15 @@ T = int(input())
 for case in range(1, T + 1):
     # 삼각형의 크기 N
     N = int(input())
-    for _ in range(N):
-        # N을 기준으로 N*N 리스트에 규칙에 맞도록 담아줄 arr
-        arr = [[0]*N for _ in range(N)]
-        # 규칙에 맞게 arr에 값을 할당
-        for i in range(N):
-            for j in range(i+1):
-                if i == 0 and i==j:
-                    arr[i][j] = 1
-                else:
-                    arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+    # N을 기준으로 N*N 리스트에 규칙에 맞도록 담아줄 arr
+    arr = [[0]*N for _ in range(N)]
+    # 규칙에 맞게 arr에 값을 할당
+    for i in range(N):
+        for j in range(i+1):
+            if i == 0 and i==j:
+                arr[i][j] = 1
+            else:
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
 	# 테스트 케이스의 번호
     print(f'#{case}')
     # arr 안에 값이 0이 아니라면 출력
