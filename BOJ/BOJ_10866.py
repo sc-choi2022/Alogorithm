4cd6ba260932ed1d1a71db4e12cdde2d45c06eca
sc-choi2022 <c.martin20222023@gmail.com>
2022-07-17 23:28:03 +0900

    0717

---
+++ BOJ/BOJ_10866.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+# 명령의 수 N
+N = int(sys.stdin.readline())
+# dq 덱(deque) 생성
+dq = deque()
+# N개의 명령에 대한 for문
+for _ in range(N):
+    # 명령을 리스트로 받는다.
+    words = list(sys.stdin.readline().split())
+    # push_front라면 dq의 앞에 word[1]을 추가
+    if words[0] == 'push_front':
+        dq.appendleft(words[1])
+    # push_back라면 dq의 뒤에 word[1]을 추가
+    elif words[0] == 'push_back':
+        dq.append(words[1])
+    # pop_front라면 dq의 가장 앞에 있는 값을 빼서 출력, 정수가 없다면 -1 출력
+    elif words[0] == 'pop_front':
+        print(dq.popleft() if dq else -1)
+    # pop_back라면 dq의 가장 뒤에 있는 값을 빼서 출력, 정수가 없다면 -1 출력
+    elif words[0] == 'pop_back':
+        print(dq.pop() if dq else -1)
+    # size라면 dq에 들어있는 정수의 개수를 출력
+    elif words[0] == 'size':
+        print(len(dq))
+    # empty라면 dq가 비어있는 경우 1을 그렇지 않은 경우 0을 출력
+    elif words[0] == 'empty':
+        print(0 if dq else 1)
+    # front라면 dq의 가장 앞에 있는 값을 출력 정수가 없다면 -1 출력
+    elif words[0] == 'front':
+        print(dq[0] if dq else -1)
+    # back라면 dq의 가장 뒤에 있는 값을 출력 정수가 없다면 -1 출력
+    elif words[0] == 'back':
+        print(dq[-1] if dq else -1)
