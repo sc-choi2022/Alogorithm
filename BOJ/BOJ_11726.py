7b92d5f036d6bc48e2f256001a78168b106272c4
sc-choi2022 <c.martin20222023@gmail.com>
2022-08-09 23:56:56 +0900

    0809

--- BOJ/BOJ_11726.py
+++ BOJ/BOJ_11726.py
@@ -4,8 +4,7 @@ N = int(sys.stdin.readline())
 # N에 따른 직사각형을 채우는 방법 수를 담는 리스트 dp
 dp = [0]*1001
 # dp[1], dp[2] 값을 1, 2로 저장한다.
-dp[1] = 1
-dp[2] = 2
+dp[1], dp[2] = 1, 2
 # 점화식에 따라 계산 후 dp[i]에 저장
 for i in range(3, N+1):
     dp[i] = dp[i-2] + dp[i-1]
