ea04403c859512a77403739fab5ded64f8c7dc3c
sc-choi2022 <c.martin20222023@gmail.com>
2022-09-08 22:53:02 +0900

    0908

--- BOJ/BOJ_1629.py
+++ BOJ/BOJ_1629.py
@@ -1,14 +1,20 @@
+import sys
+
 def func(A, B, C):
+    # B값이 1이라면 A%C을 return
     if B == 1:
         return A%C
     else:
+        # A^(B//2)%C을 구하기 위해 func(A, B//2, C)을 실행
         k = func(A, B//2, C)
+        # B//2에 나머지가 없었다면 A^(B//2)%C의 제곱을 C로 나눈 나머지를 return
         if B%2==0:
             return k*k%C
         else:
+        # B//2에 나머지가 없었다면 A^(B//2)%C의 제곱에 A을 곱한 값을 C로 나눈 나머지를 return
             return k*k*A%C
 
 
 # 자연수 A, B, C
-A, B, C = map(int, input().split())
+A, B, C = map(int, sys.stdin.readline().split())
 print(func(A,B,C))
