832be00f37ce7cf4a576e733e0df870d31a060de
sc-choi2022 <c.martin20222023@gmail.com>
2022-09-19 23:47:06 +0900

    0919

--- BOJ/BOJ_2293.py
+++ BOJ/BOJ_2293.py
@@ -1,15 +1,21 @@
+import sys
+
 # 동전의 수 N, 목표 합 K
-N, K = map(int, input().split())
+N, K = map(int, sys.stdin.readline().split())
 # 동전의 가치를 담을 리스트 coins
-coins = [int(input()) for _ in range(N)]
+coins = [int(sys.stdin.readline()) for _ in range(N)]
 # 합이 index가 되는 경우의 수를 담는 dp
 dp = [0]*(K+1)
-# 1가 되는 경우의 수를 위해 index 0에 1 저장
+# 이중 for문의 coin-i이 0일 때 1개를 추가하기 위해 1로 값 초기화
 dp[0] = 1
 
+# coins의 모든 coin에 대해
 for coin in coins:
-    for j in range(coin, K+1):
-        if j-coin >= 0:
-            dp[j] += dp[j-coin]
+    # coin 1개로 만들 수 있는 값부터 K까지
+    for i in range(coin, K+1):
+        # i-coin>=0인 경우, i-coin을 만드는 경우의 수를 dp[i]에 더해주어
+        # 현재 coin을 사용한 경우까지 dp[i]에 갱신
+        if i-coin >= 0:
+            dp[i] += dp[i-coin]
 # K가 되는 경우의 수 출력
 print(dp[K])
--- BOJ/BOJ_2294.py
+++ BOJ/BOJ_2294.py
@@ -1,17 +1,26 @@
+import sys
+
 # 동전의 수 N, 목표 합 K
-N, K = map(int, input().split())
+N, K = map(int, sys.stdin.readline().split())
 # 동전의 가치를 담을 리스트 coins
-coins = [int(input()) for _ in range(N)]
-# 합이 index가 되는 경우의 수를 담는 dp
+coins = [int(sys.stdin.readline()) for _ in range(N)]
+# index가 되는 최소 동전의 개수를 담을 리스트 dp, 모든 값을 100001으로 초기화
 dp = [100001]*(K+1)
-# 1가 되는 경우의 수를 위해 index 0에 1 저장
+# 이중 for문의 coin-i이 0인 경우를 위해 0로 값 초기화
 dp[0] = 0
 
+# coins의 모든 coin에 대해
 for coin in coins:
-    for j in range(coin, K+1):
-        if j-coin >= 0:
-            dp[j] = min(dp[j-coin]+1, dp[j])
-if dp[K] != 100001:
-    print(dp[K])
-else:
+    # coin 1개로 만들 수 있는 값부터 K까지
+    for i in range(coin, K+1):
+        # i-coin>=0인 경우, 
+        # i-coin을 만드는 경우의 수인 dp[i-coin]+1과 기존 dp[i]값 중 작은 값을 dp[i]에 저장
+        # dp[i-coin]+1은 coin한개와 나머지 i-coin을 만드는 최소 경우의 수를 더한 것을 의미
+        if i-coin >= 0:
+            dp[i] = min(dp[i-coin]+1, dp[i])
+# dp[K]값이 갱신되지 않은 경우 -1 출력
+if dp[K] == 100001:
     print(-1)
+# dp[K]값이 갱신되었다면 dp[K]을 출력
+else:
+    print(dp[K])
