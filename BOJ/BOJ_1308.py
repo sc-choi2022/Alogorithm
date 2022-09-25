bff28dc9d26add0900cd1e633068d59c977bffd7
sc-choi2022 <c.martin20222023@gmail.com>
2022-09-25 23:51:15 +0900

    0925

--- BOJ/BOJ_1308.py
+++ BOJ/BOJ_1308.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 # 오늘의 날짜 년, 월, 일 y1, m1, d1
 y1, m1, d1 = map(int, input().split())
 # D-Day인 날의 날짜 년, 월, 일 y2, m2, d2
@@ -8,6 +8,6 @@ if y2-y1 > 1000 or (y2==y1+1000 and m2>=m1 and d2>=d1):
     print('gg')
 else:
     # 남은 일수를 gap에 저장
-    gap = datetime.datetime(y2, m2, d2) - datetime.datetime(y1, m1, d1)
+    gap = datetime(y2, m2, d2) - datetime(y1, m1, d1)
     # D- 와 gap으로 출력
     print('D-'+str(gap.days))
