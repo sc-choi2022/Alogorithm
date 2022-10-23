9ceca690380f8529cd9b0b4dc83763759c415c1a
sc-choi2022 <c.martin20222023@gmail.com>
2022-10-23 19:28:28 +0900

    1023

--- BOJ/BOJ_1245.py
+++ BOJ/BOJ_1245.py
@@ -1,31 +1,43 @@
 import sys
-
 def dfs(a, b):
     global check
-
+    # 방문을 표시
     visited[a][b] = True
-
+    # 인접한 격자를 모두 확인
     for di, dj in (1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, -1), (-1, 1):
-        x, y = a + di, b + dj
-
-        if -1 < x < N and -1 < y < M:
-            if farm[a][b] < farm[x][y]:
+        # 방향에 따른 인접 격자의 위치 ni, nj
+        ni, nj = a + di, b + dj
+        # ni, nj가 범위안에 존재하는 경우
+        if 0 <= ni < N and 0 <= nj < M:
+            # 현재 위치가 인접 격자의 위치보다 낮을 경우 check에 False 저장
+            if farm[a][b] < farm[ni][nj]:
                 check = False
-            if not visited[x][y] and farm[x][y] == farm[a][b]:
-                dfs(x, y)
+            # 확인하지 않은 위치이며 현재위치와 인접 격자 위치의 높이가 동일한 경우
+            if not visited[ni][nj] and farm[a][b] == farm[ni][nj]:
+                # 인접 위치로 dfs 실행
+                dfs(ni, nj)
 
-N, M = map(int, input().split())
+# 농장의 격자를 이루는 N, M
+N, M = map(int, sys.stdin.readline().split())
+# 농장의 높이를 담을 행렬 farm
 farm = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-
+# 하나의 격자가 인접한 경우를 확인하기 위한 행렬 visited
 visited = [[0]*M for _ in range(N)]
+# 출력할 산봉우리의 개수 cnt
 cnt = 0
 
 for i in range(N):
     for j in range(M):
-        if farm[i][j] > 0 and not visited[i][j]:
+        # 농장의 높이가 존재하고 확인하지 않은 곳인 경우
+        if farm[i][j] and not visited[i][j]:
+            # 변수 check를 True로 초기화
             check = True
+            # 함수 dfs를 실행
             dfs(i, j)
-
+            # dfs를 후 ckeck가 True로 봉우리가 맞는 경우
             if check:
+                print(i, j)
+                # 봉우리를 cnt
                 cnt += 1
+# 산봉우리의 개수 출력
 print(cnt)
--- BOJ/BOJ_1411.py
+++ BOJ/BOJ_1411.py
@@ -2,25 +2,39 @@ import sys
 
 # 단어의 개수 N
 N = int(sys.stdin.readline())
+# 모든 단어를 담을 리스트 words
 words = [sys.stdin.readline().strip() for _ in range(N)]
+# 바꾸는 알파벳 정보를 담을 딕셔너리 w_dic
 w_dic = {}
+# 단어의 길이 leng
 leng = len(words[0])
-
+# 출력할 쌍의 개수 cnt
 cnt = 0
+
 for i in range(N):
     w1 = words[i]
-    for j in range(i, N):
+    for j in range(i+1, N):
         w2 = words[j]
+        # 딕셔너리 w_dic 초기화
         w_dic = {}
         for l in range(leng):
             n1, n2 = w1[l], w2[l]
-            if n1 in w_dic and w_dic[n1] != n2:
-                break
-            elif n1 in w_dic and w_dic[n1] == n2:
-                continue
+            # w_dic에 key n1 존재하는 경우
+            if n1 in w_dic:
+                # 기존과 다른 알파벳으로 교환해야하는 경우 break
+                if w_dic[n1] != n2:
+                    break
+            # w_dic에 key n1 존재하지 않는 경우
             else:
-                w_dic[n1] = n2
-    else:
-        print(w1, w2)
-        cnt += 1
+                # key n1이 존재하지 않으나 n2이 value로 존재한다면 break
+                if n2 in w_dic.values():
+                    break
+                else:
+                    # w_dic에 key는 n1, value는 n2로 저장
+                    w_dic[n1] = n2
+        # 숌스러운 쌍인 경우
+        else:
+            # cnt 1 증가
+            cnt += 1
+# cnt 출력
 print(cnt)
