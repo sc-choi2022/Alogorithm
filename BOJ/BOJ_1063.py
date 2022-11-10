4c2bd4a24e6665abab3dc152926d112a4a44c6e7
sc-choi2022 <c.martin20222023@gmail.com>
2022-11-10 22:34:45 +0900

    1110

---
+++ BOJ/BOJ_1063.py
@@ -0,0 +1,23 @@
+import sys
+
+# 킹의 위치 king, 돌의 위치 stone, 움직이는 횟수 N
+king, stone, N = sys.stdin.readline().strip().split()
+k = list(map(int, [ord(king[0]) - 64, king[1]]))
+s = list(map(int, [ord(stone[0]) - 64, stone[1]]))
+move = {'R': [1, 0], 'L': [-1, 0], 'B': [0, -1], 'T': [0, 1], 'RT': [1, 1], 'LT': [-1, 1], 'RB': [1, -1], 'LB': [-1, -1]}
+
+for _ in range(int(N)):
+    m = sys.stdin.readline().strip()
+    nx = k[0] + move[m][0]
+    ny = k[1] + move[m][1]
+    if 0 < nx <= 8 and 0 < ny <= 8:
+        if nx == s[0] and ny == s[1]:
+            sx = s[0] + move[m][0]
+            sy = s[1] + move[m][1]
+            if 0 < sx <= 8 and 0 < sy <= 8:
+                k = [nx, ny]
+                s = [sx, sy]
+        else:
+            k = [nx, ny]
+print(f'{chr(k[0] + 64)}{k[1]}')
+print(f'{chr(s[0] + 64)}{s[1]}')
