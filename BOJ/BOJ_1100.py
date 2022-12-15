4fd2605735388160336e20a84170b90c939e894c
sc-choi2022 <c.martin20222023@gmail.com>
2022-12-15 22:56:15 +0900

    1215

--- BOJ/BOJ_1100.py
+++ BOJ/BOJ_1100.py
@@ -1,5 +1,5 @@
 import sys
-
+# 체스판의 정보를 담을 배열 chess
 chess = [list(map(str, sys.stdin.readline().strip())) for _ in range(8)]
 
 # 출력할 개수 ans
@@ -7,6 +7,7 @@ ans = 0
 
 for i in range(8):
     for j in range(8):
+        # 하얀칸이고 'F'로 위에 말이 있는 경우 ans 1 증가
         if (i + j) % 2 == 0 and chess[i][j] == 'F':
             ans += 1
 
--- BOJ/BOJ_1946.py
+++ BOJ/BOJ_1946.py
@@ -1,21 +1,33 @@
 import sys
-
+# 테스트케이스 T
 T = int(sys.stdin.readline())
 
 for _ in range(T):
+    # 지원자의 수 N
     N = int(sys.stdin.readline())
+    # 지원자의 성적순위를 담을 리스트 people
     people = [0] * N
 
     for i in range(N):
+        # 서류심사 성적 순위 t1, 면접성적 순위 t2
         t1, t2 = map(int, sys.stdin.readline().split())
+        # people[i]에 각 값을 저장
         people[i] = [t1, t2]
 
+    # people을 서류심사 성적 순위를 기준으로 정렬하여 peopleSort로 저장
     peopleSort = sorted(people, key=lambda x : x[0])
+    # peopleSort의 0번째는 선발하므로 hired 1 증가
     hired = 1
-    man = peopleSort[0][1]
+    # 첫 선발 신입사원의 면접성적 순위값을 score에 저장
+    score = peopleSort[0][1]
 
+    # 정렬의 첫번째와 마지막을 제외한 모든 지원자를 확인
     for j in range(1, N):
-        if peopleSort[j][1] < man:
-            man = peopleSort[j][1]
+        # poepleSort[j]의 면접성적 순위가 score보다 작은 경우
+        if peopleSort[j][1] < score:
+            # score값에 peopleSort[j][1] 저장
+            score = peopleSort[j][1]
+            # hired 1 증가
             hired += 1
+    # 선발할 수 있는 신입사원 최대 인원수 출력
     print(hired)
