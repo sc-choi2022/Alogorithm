a757108a697c026e9d4ddb0a1e4877617c751dff
sc-choi2022 <c.martin20222023@gmail.com>
2023-01-01 23:55:41 +0900

    0101

---
+++ BOJ/BOJ_1049.py
@@ -0,0 +1,18 @@
+import sys
+
+# 끊어진 기타줄의 개수 N, 기타줄 브랜드 개수 M
+N, M = map(int, sys.stdin.readline().split())
+
+# 패키지의 가격을 담을 배열 package, 낱개의 가격을 담을 배열 each
+package, each = [0] * M, [0] * M
+
+for i in range(M):
+    p, e = map(int, sys.stdin.readline().split())
+    package[i], each[i] = p, e
+
+# package와 each을 정렬
+package.sort()
+each.sort()
+
+ans = [package[0] * (N // 6 + 1), each[0] * N, package[0] * (N // 6) + each[0]* (N % 6)]
+print(min(ans))
