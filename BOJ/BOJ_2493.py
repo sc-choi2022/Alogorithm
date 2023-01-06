2677fc0e0b1c9369461e192cf7ea9cd24c5ee863
sc-choi2022 <c.martin20222023@gmail.com>
2023-01-06 23:41:33 +0900

    0106

--- BOJ/BOJ_2493.py
+++ BOJ/BOJ_2493.py
@@ -4,17 +4,32 @@ import sys
 N = int(sys.stdin.readline())
 # 탑들의 높이를 담을 배열 towers
 towers = list(map(int, sys.stdin.readline().split()))
-stack = towers.copy()
-stack.pop()
+# 스택으로 사용할 배열 stack
+stack = []
 
-ans = [0] * N
+for i in range(N):
+    # i + 1번째 탑의 높이 h
+    h = towers[i]
 
-for i in range(N - 1, 0, -1):
-    while stack:
-        temp = stack.pop()
+    if stack:
+        while stack:
+            # 레이저를 만날 수 없는 탑을 확인한 경우
+            if stack[-1][0] < h:
+                # stack.pop()
+                stack.pop()
+                # stack.pop() 후 stack이 비었다면 0을 출력
+                if not stack:
+                    print(0, end=' ')
+            # 레이저를 만나는 탑을 확인한 경우
+            else:
+                # 탑의 번호를 출력 후 break
+                print(stack[-1][1], end=' ')
+                break
 
-        if temp >= towers[i]:
-            ans[i] = len(stack) + 1
-            print(stack)
-            break
-print(*ans)
+        # while문을 나온 후 stack에 현재 탑의 높이와 번호를 저장
+        stack.append((h, i + 1))
+    # stack이 비어있다면
+    else:
+        # 0을 출력 후 stack에 현재 높이와 탑의 번호를 tuple로 저장
+        print(0, end=' ')
+        stack.append((h, i + 1))
