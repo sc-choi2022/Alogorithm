c54ba79f1f01963cfe41a739b95a254eaf64f617
sc-choi2022 <c.martin20222023@gmail.com>
2023-01-22 23:49:57 +0900

    0122

--- BOJ/BOJ_1300.py
+++ BOJ/BOJ_1300.py
@@ -5,19 +5,18 @@ N = int(sys.stdin.readline())
 # 인덱스 K
 K = int(sys.stdin.readline())
 # 이분 탐색을 위한 start, end
-start, end = 0, K
-ans = 0
+start, end = 1, K
 
 while start <= end:
     mid = (start + end) // 2
 
     tmp = 0
     for i in range(1, N + 1):
-        tmp += min(mid // i , N)
+        tmp += min(mid//i, N)
 
-        if tmp >= K:
-            ans = mid
-            end = mid - 1
-        else:
-            start = mid + 1
+    if tmp >= K:
+        ans = mid
+        end = mid - 1
+    else:
+        start = mid + 1
 print(ans)
