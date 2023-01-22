d346b3171c95f781662bb38260d0c28733987f61
sc-choi2022 <c.martin20222023@gmail.com>
2023-01-22 23:42:38 +0900

    0122

---
+++ BOJ/BOJ_1300.py
@@ -0,0 +1,23 @@
+import sys
+
+# NxN 배열의 기준 N
+N = int(sys.stdin.readline())
+# 인덱스 K
+K = int(sys.stdin.readline())
+# 이분 탐색을 위한 start, end
+start, end = 0, K
+ans = 0
+
+while start <= end:
+    mid = (start + end) // 2
+
+    tmp = 0
+    for i in range(1, N + 1):
+        tmp += min(mid // i , N)
+
+        if tmp >= K:
+            ans = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+print(ans)
--- BOJ/BOJ_2512.py
+++ BOJ/BOJ_2512.py
@@ -7,22 +7,27 @@ N = int(sys.stdin.readline())
 budget = list(map(int, sys.stdin.readline().split()))
 # 총 예산 M
 M = int(sys.stdin.readline())
+# 이분탐색의 기준이 되는 start, end
 start, end = 0, max(budget)
-ans = 0
 
 while start <= end:
     mid = (start + end) // 2
 
+    # mid을 기준으로 총 예산을 계산하여 저장할 변수 add
     add = 0
     for b in budget:
-        if mid < b:
+        # 상한액보다 b가 큰 경우
+        if b > mid:
             add += mid
+        # 상한액보다 b가 작은 경우
         else:
             add += b
 
-    if add <= M:
-        start = mid + 1
-        ans = mid
-    else:
+    # add가 예산액을 초과하는 경우
+    if add > M:
         end = mid - 1
-print(ans)
+    else:
+        start = mid + 1
+
+# 배정된 예산들 중 최댓값인 정수를 출력
+print(end)
