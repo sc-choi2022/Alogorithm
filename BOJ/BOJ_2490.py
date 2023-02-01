c5b4913e5299fb78dd2f973cfe8c227ba746c228
sc-choi2022 <c.martin20222023@gmail.com>
2023-02-01 22:56:44 +0900

    0201

---
+++ BOJ/BOJ_2490.py
@@ -0,0 +1,18 @@
+import sys
+
+for _ in range(3):
+    # 윳의 정보를 담을 배열 bars
+    bars = list(map(int, sys.stdin.readline().split()))
+    # 배의 개수를 저장할 변수 cnt
+    cnt = bars.count(0)
+
+    if cnt == 0:
+        print('E')
+    elif cnt == 1:
+        print('A')
+    elif cnt == 2:
+        print('B')
+    elif cnt == 3:
+        print('C')
+    else:
+        print('D')
