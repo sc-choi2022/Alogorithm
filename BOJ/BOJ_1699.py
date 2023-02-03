c4a0f205458715ed27f19e0226b0cb9db8128f93
sc-choi2022 <c.martin20222023@gmail.com>
2023-02-03 23:11:25 +0900

    0203

--- BOJ/BOJ_1699.py
+++ BOJ/BOJ_1699.py
@@ -2,13 +2,17 @@ import sys
 
 # 자연수 N
 N = int(sys.stdin.readline())
+# 자연수 x의 제곱항의 최대값을 초기값으로 가지는 배열 dp
 dp = [x for x in range(N + 1)]
 
 for i in range(1, N + 1):
     for j in range(1, i):
-        if j**2 > i:
+        # j**2이 제곱항으로 가능한 수보다 큰 경우
+        if j ** 2 > i:
             break
+        # 기존 dp[i]인 i를 만드는 제곱항의 개수보다 dp[i - j**2] + 1이 작다면 dp[i] 갱신
         if dp[i] > dp[i - j**2] + 1:
             dp[i] = dp[i - j**2] + 1
+
 # 제곱수 항의 최소 개수를 출력
 print(dp[N])
--- BOJ/BOJ_1946.py
+++ BOJ/BOJ_1946.py
@@ -2,6 +2,7 @@ import sys
 # 테스트케이스 T
 T = int(sys.stdin.readline())
 
+# for문과 sort
 for _ in range(T):
     # 지원자의 수 N
     N = int(sys.stdin.readline())
@@ -31,3 +32,48 @@ for _ in range(T):
             hired += 1
     # 선발할 수 있는 신입사원 최대 인원수 출력
     print(hired)
+
+# 코드 단순화
+for _ in range(T):
+    # 지원자의 수 N
+    N = int(sys.stdin.readline())
+    # 지원자의 정보
+    people = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    people.sort(key=lambda x: (x[0], x[1]))
+    mini = people[0]
+    # 출력할 최대 인원
+    maximum = N
+
+    for i in range(1, N):
+        if people[i][0] > mini[0] and people[i][1] > mini[1]:
+            maximum -= 1
+        else:
+            mini = people[i]
+    print(maximum)
+
+# sort를 제거한 방법
+for _ in range(T):
+    # 지원자의 수 N
+    N = int(sys.stdin.readline())
+    # 성적을 담을 배열 grade
+    grade = [0] * (N + 1)
+
+    for _ in range(N):
+        # 서류점수와 면접점수 paper, interview
+        paper, interview = map(int, sys.stdin.readline().split())
+        # 서류점수를 grade의 index로 하여 면접점수를 저장
+        grade[paper] = interview
+    # 서류점수 1등의 면접점수를 변수 limit에 저장
+    limit = grade[1]
+    # 출력할 최대 인원
+    maximum = N
+
+    for i in range(2, N + 1):
+        # limit보다 면접점수가 낮은 경우 인원에 포함되지 않아 maximum 1 감소
+        if grade[i] > limit:
+            maximum -= 1
+        # 인원에 포함되는 경우 limit값을 재설정
+        else:
+            limit = grade[i]
+    # 선발할 수 있는 신입사원 최대 인원수 출력
+    print(maximum)
