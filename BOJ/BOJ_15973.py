73887e3d18cf31e0382a9163d83c1541c06c195e
sc-choi2022 <c.martin20222023@gmail.com>
2023-02-21 20:15:19 +0900

    0221

--- BOJ/BOJ_15973.py
+++ BOJ/BOJ_15973.py
@@ -4,13 +4,12 @@ import sys
 x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
 # 첫번째 박스의 두 꼭지점 x3, y3, x4, y4
 x3, y3, x4, y4 = map(int, sys.stdin.readline().split())
-box1 = ((x1, y1), (x1, y2), (x2, y1), (x2, y2))
-box2 = ((x3, y3), (x3, y4), (x4, y3), (x4, y4))
 
-for b1 in box1:
-    if b1 in box2:
-        print('POINT')
-        break
-
-if x1 == x3 or x1 == x4 or x2 == x3 or x2 == x4 or y1 == y3 or y1 == y4 or y2 == y3 or y2 == y4:
+if x2 < x3 or x1 > x4 or y1 > y4 or y2 < y3:
+    print('NULL')
+elif (x2 == x3 and y2 == y3) or (x1 == x4 and y1 == y4) or (x2 == x3 and y1 == y4) or (x1 == x4 and y2 == y3):
+    print('POINT')
+elif x2 == x3 or x1 == x4 or y1 == y4 or y2 == y3:
     print('LINE')
+else:
+    print('FACE')
