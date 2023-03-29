15514769dcfe68cd43de9ca917da1fa03b8d888e
sc-choi2022 <c.martin20222023@gmail.com>
2023-03-29 22:43:55 +0900

    0329

--- BOJ/BOJ_17626.py
+++ BOJ/BOJ_17626.py
@@ -2,16 +2,17 @@ import sys
 
 # 자연수 N
 N = int(sys.stdin.readline())
+# 활용할 배열 dp
+dp = [0] * (N+1)
+dp[1] = 1
 
-dp = [0, 1]
-
-for i in range(2, N + 1):
-    value = i
+for i in range(2, N+1):
+    cnt = 1e9
     j = 1
-
-    while (j**2) <= i:
-        value = min(value, dp[i - (j**2)])
+    while j**2 <= i:
+        cnt = min(cnt, dp[i-(j**2)])
         j += 1
+    dp[i] = cnt + 1
 
-    dp.append(value + 1)
+# 합이 n과 같게 되는 제곱수들의 최소 개수 출력
 print(dp[N])
