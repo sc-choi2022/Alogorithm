6d074873cd7feaff73dc405582bb3c4fb26e3660
sc-choi2022 <c.martin20222023@gmail.com>
2023-04-06 23:09:07 +0900

    0406

---
+++ BOJ/BOJ_2629.py
@@ -0,0 +1,11 @@
+import sys
+
+# 추의 개수 N
+N = int(sys.stdin.readline())
+# 추의 종류
+weights = list(map(int, sys.stdin.readline().split()))
+
+# 구슬의 개수 M
+M = int(sys.stdin.readline())
+# 확인할 구슬의 무게
+marble = list(map(int, sys.stdin.readline().split()))
