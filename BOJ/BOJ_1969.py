ebb3547fc47fce5d686ec794261b79b8670c63e7
sc-choi2022 <c.martin20222023@gmail.com>
2023-04-18 23:42:12 +0900

    0418

---
+++ BOJ/BOJ_1969.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+import sys
+
+# DNA의 수 N, 문자열의 길이 M
+N, M = map(int, sys.stdin.readline().split())
+# DNA를 담은 배열 DNA
+DNA = [sys.stdin.readline().rstrip() for _ in range(N)]
+# Hamming Distance의 합이 가장 작은 DNA answer, 그 Hamming Distance의 합 cnt
+answer, cnt = '', 0
+
+for i in range(M):
+    alpha = defaultdict(int)
+
+    for j in range(N):
+        if DNA[j][i] in alpha:
+            alpha[DNA[j][i]] += 1
+        else:
+            alpha[DNA[j][i]] = 1
+    lst = sorted(list(alpha.items()), key=lambda x: (-x[1], x[0]))
+    answer += lst[0][0]
+    cnt += N - lst[0][1]
+# answer와 cnt 출력
+print(answer)
+print(cnt)
