d0bff859f93932e2d13a83ba7cae753d573ee0cc
sc-choi2022 <c.martin20222023@gmail.com>
2023-05-18 23:46:10 +0900

    0518

--- BOJ/BOJ_1340.py
+++ BOJ/BOJ_1340.py
@@ -1,24 +1,15 @@
 import sys
 
-# 월별 딕셔너리
-month = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6,
-         'July':7, 'August':8, 'September':9, 'October':10, 'November':10, 'December':12}
-day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-total = 365*24*60
-number = 0
-date = list(sys.stdin.readline().rstrip().replace(',','').replace(':',' ').split())
-for i in range(5):
-    if i == 0:
-        for j in range(1, month[date[i]]):
-            number += day[j]
+month, day, year, time = sys.stdin.readline().replace(',', '').split()
+day, year = int(day), int(year)
+hour, minute = map(int, time.split(':'))
 
-    elif i == 1:
-        number += int(date[i]) - 1
-        number *= 24*60
-    elif i == 2:
-        continue
-    elif i == 3:
-        number += int(date[i])*60
-    else:
-        number += int(date[i])
-print(number / total * 100)
+month_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+day_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# 윤년인 경우
+if not year%400 or (not year%4 and year%100):
+    day_list[1] += 1
+total = sum(day_list) * 24 * 60
+now = (sum(day_list[:month_list.index(month)]) + day-1)*24*60 + 60*hour + minute
+# 이번 해가 몇%지났는지 출력
+print(now/total*100)
--- BOJ/BOJ_9372.py
+++ BOJ/BOJ_9372.py
@@ -3,9 +3,9 @@ import sys
 
 def dfs(node, cnt):
     visit[node] = 1
-    for i in country[node]:
-        if not visit[i]:
-            cnt = dfs(i, cnt + 1)
+    for c in country[node]:
+        if not visit[c]:
+            cnt = dfs(c, cnt + 1)
     return cnt
 
 # 테스트케이스 개수 T
@@ -22,7 +22,19 @@ for _ in range(T):
         country[a].append(b)
         country[b].append(a)
 
-    cnt = 0
     visit = [0] * (N+1)
-    visit[1] = 0
     print(dfs(1, 0))
+
+# 모든 국가가 연결되어 있기 때문에 아래와 같은 방법도 정답이다.
+import sys
+
+# 테스트케이스 개수 T
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    # 국가의 수 N와 비행기의 종류 M
+    N, M = map(int, sys.stdin.readline().split())
+
+    for _ in range(M):
+        a, b = map(int, sys.stdin.readline().split())
+    print(N-1)
