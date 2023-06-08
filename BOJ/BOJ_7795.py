f2ea96f34af99d372ddb0e0bd7dcabb2c6395090
sc-choi2022 <c.martin20222023@gmail.com>
2023-06-08 23:31:00 +0900

    0608

---
+++ BOJ/BOJ_7795.py
@@ -0,0 +1,8 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N, M = map(int, sys.stdin.readline().split())
+    A = list(map(int, sys.stdin.readline().split()))
+    B = list(map(int, sys.stdin.readline().split()))
