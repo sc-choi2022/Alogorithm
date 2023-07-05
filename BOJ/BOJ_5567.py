043bc9b27f2fcd8386a28bc0db130b2e8816f11b
sc-choi2022 <c.martin20222023@gmail.com>
2023-07-05 23:49:40 +0900

    0705

---
+++ BOJ/BOJ_5567.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+
+# 상근이의 동기수 N
+N = int(sys.stdin.readline())
+# 리스트의 길이 M
+M = int(sys.stdin.readline())
+# 친구 배열 friends
+friends = [[] for _ in range(N+1)]
+visit = [0]*(N+1)
+visit[1] = 1
+for _ in range(M):
+    # 친구관계 A, B
+    A, B = map(int, sys.stdin.readline().split())
+
+    friends[A].append(B)
+    friends[B].append(A)
+
+for f in friends[1]:
+    visit[f] = 1
+
+queue = deque(friends[1])
+while queue:
+    current = queue.popleft()
+    for friend in friends[current]:
+        if not visit[friend]:
+            visit[friend] = 1
+print(sum(visit)-1)
