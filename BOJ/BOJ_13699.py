5fcc59e32d5cfdbc3a7bafbc97331e4acf69fcf4
sc-choi2022 <c.martin20222023@gmail.com>
2023-07-23 23:19:48 +0900

    0723

--- BOJ/BOJ_13699.py
+++ BOJ/BOJ_13699.py
@@ -2,11 +2,11 @@ import sys
 
 # 수열의 매개변수 N
 N = int(sys.stdin.readline())
-dp = [0]*36
-dp[0] = 1
+# 수열 t(n)을 저장하는 배열 dp
+dp = [1] + [0]*N
 
 for i in range(1, N+1):
     for j in range(i):
-        dp[i] += dp[j]*dp[i-j-1]
-# dp[N]을 출력
+        dp[i] += dp[j] * dp[i-j-1]
+# t(n) 출력
 print(dp[N])
--- BOJ/BOJ_16395.py
+++ BOJ/BOJ_16395.py
@@ -1,11 +1,15 @@
 import sys
 
-pascal = [[1 for _ in range(i)] for i in range(1, 31)]
-
-for j in range(2, 30):
-    for k in range(1, i):
-        pascal[j][k] = pascal[j-1][k-1] + pascal[j-1][k]
-
-# N번째 행의 K번째 수
+# N번째 행에 있는 K번째 수 N, K
 N, K = map(int, sys.stdin.readline().split())
+# 1. N번째 행은 N개의 수
+# 2. 첫번째 항은 1이다.
+# 3. 두 번째 행부터, 각 행의 양 끝의 값은 1
+pascal = [[1]*i for i in range(1, N+1)]
+
+for j in range(1, N):
+    for k in range(1, j):
+        # 3. 나머지 수의 값은 바로 위 행의 인접한 두 수의 합
+        pascal[j][k] = pascal[j-1][k-1] + pascal[j-1][k]
+# N번째 행에 있는 K번째 수 출력
 print(pascal[N-1][K-1])
