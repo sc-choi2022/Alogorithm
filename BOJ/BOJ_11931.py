b36436da7d02f982981ae4e182593a8d24a50719
sc-choi2022 <c.martin20222023@gmail.com>
2023-07-29 23:31:08 +0900

    0729

--- BOJ/BOJ_11931.py
+++ BOJ/BOJ_11931.py
@@ -10,3 +10,14 @@ numbers.sort(reverse=True)
 # N개의 줄에 내림차순으로 정렬한 결과를 한 줄에 하나씩 출력
 for number in numbers:
     print(number)
+
+# selection sort (시간초과)
+N = int(sys.stdin.readline())
+numbers = [int(sys.stdin.readline()) for _ in range(N)]
+
+for i in range(N-1):
+    for j in range(i+1, N):
+        if numbers[i] < numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+for number in numbers:
+    print(number)
--- BOJ/BOJ_2822.py
+++ BOJ/BOJ_2822.py
@@ -1,5 +1,5 @@
 import sys
-# 참가자의 번호와 점수를 저장하는 배열 scores
+# 문제의 번호와 점수를 저장하는 배열 scores
 scores = [(0, 0) for _ in range(8)]
 
 for i in range(1, 8+1):
@@ -9,11 +9,10 @@ scores.sort(key=lambda x:-x[1])
 
 # 참가자의 총점과 상위 5개의 점수를 가진 참가자의 번호를 저장하는 배열 answer
 total = 0
-answer = [0]*5
 for j in range(5):
     total += scores[j][1]
-    answer[j] = scores[j][0]
 # 참가자의 총점을 출력
 print(total)
+answer = [scores[k][0] for k in range(5)]
 # 어떤 문제가 최종 점수에 포함되는지를 공백으로 구분하여 출력
 print(*sorted(answer))
