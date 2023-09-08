6ae69df1f6b99277ac8c2108dea8d37e4b90fa30
sc-choi2022 <c.martin20222023@gmail.com>
2023-09-08 23:53:47 +0900

    0908

--- BOJ/BOJ_1937.py
+++ BOJ/BOJ_1937.py
@@ -1,10 +1,13 @@
 import sys
 
+def dfs(i, j):
+    return
+
 # 대나무 숲의 크기 N
 N = int(sys.stdin.readline())
 # 대나무 숲의 정보를 저장하는 배열 bamboo
 bamboo = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+dp = [[0]*N  for _ in range(N)]
 # 판다가 이동할 수 있는 칸의 최대 수 answer
 answer = 0
-
-# dp + dfs
