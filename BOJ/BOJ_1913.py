c9d3f5e840a583767d74e32b783c9f05561f5699
sc-choi2022 <c.martin20222023@gmail.com>
2023-09-28 12:13:11 +0900

    0928

--- BOJ/BOJ_1913.py
+++ BOJ/BOJ_1913.py
@@ -16,31 +16,48 @@ ci, cj = N//2, N//2
 numbers[ci][cj] = 1
 
 # for문
-# for _ in range(N-1):
-#     for _ in range(2):
-#         di, dj = direction[direct]
-#         for _ in range(cnt):
-#             ni, nj = ci + di, cj + dj
-#             numbers[ni][nj] = numbers[ci][cj] + 1
-#             if numbers[ni][nj] == M:
-#                 mi, mj = ni, nj
-#             ci, cj = ni, nj
-#         direct = (direct+1)%4
-#     cnt += 1
-# for _ in range(cnt-1):
-#     di, dj = direction[direct]
-#     ni, nj = ci + di, cj + dj
-#     numbers[ni][nj] = numbers[ci][cj] + 1
-#     if numbers[ni][nj] == M:
-#         mi, mj = ni, nj
-#     ci, cj = ni, nj
-
-two = 1
-# while문
-while True:
+for _ in range(N-1):
+    for _ in range(2):
+        di, dj = direction[direct]
+        for _ in range(cnt):
+            ni, nj = ci + di, cj + dj
+            numbers[ni][nj] = numbers[ci][cj] + 1
+            if numbers[ni][nj] == M:
+                mi, mj = ni, nj
+            ci, cj = ni, nj
+        direct = (direct+1)%4
+    cnt += 1
+for _ in range(cnt-1):
     di, dj = direction[direct]
-    break
+    ni, nj = ci + di, cj + dj
+    numbers[ni][nj] = numbers[ci][cj] + 1
+    if numbers[ni][nj] == M:
+        mi, mj = ni, nj
+    ci, cj = ni, nj
 
+# while문
+number = 2
+flag = False
+while ci or cj:
+    for _ in range(2):
+        for _ in range(cnt):
+            di, dj = direction[direct]
+            ci += di
+            cj += dj
+            numbers[ci][cj] = number
+            if number == M:
+                mi, mj = ci, cj
+            if ci == 0 and cj == 0:
+                flag = True
+                break
+            number += 1
+        if flag:
+            break
+        direct = (direct + 1)%4
+    cnt += 1
+
+# 표를 출력
 for number in numbers:
     print(*number)
+# 입력받은 자연수의 좌표를 나타내는 두 정수를 한 칸 띄어서 출력
 print(mi+1, mj+1)
