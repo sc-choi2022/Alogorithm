ca5246e555adaa9c10c4ee3ac52e0b68244e4b11
sc-choi2022 <c.martin20222023@gmail.com>
2023-10-09 23:50:56 +0900

    1009

--- BOJ/BOJ_2346.py
+++ BOJ/BOJ_2346.py
@@ -4,4 +4,18 @@ import sys
 # 풍선의 개수 N
 N = int(sys.stdin.readline())
 # 풍선의 종이를 저장하는 배열
-orders = list(map(int, sys.stdin.readline().split()))
+orders = deque(enumerate(map(int, sys.stdin.readline().split())))
+# 터진 풍선의 번호를 저장하는 배열 answer
+answer = []
+while orders:
+    idx, paper = orders.popleft()
+    answer.append(idx + 1)
+    # 종이에 0은 적혀있지 않다.
+    # 종이의 값이 양수인 경우
+    if paper > 0:
+        orders.rotate(-(paper-1))
+    # 종이의 값이 음수인 경우
+    else:
+        orders.rotate(-paper)
+# 터진 풍선의 번호를 차례로 나열
+print(*answer)
