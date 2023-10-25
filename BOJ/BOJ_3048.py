11502380d1c8924ca9d6634a33591d1507aa6ecf
sc-choi2022 <c.martin20222023@gmail.com>
2023-10-25 23:47:13 +0900

    1025

---
+++ BOJ/BOJ_3048.py
@@ -0,0 +1,10 @@
+import sys
+
+# 첫번째 그룹의 개미의 수 N1, 두 번째 그룹의 개미의 수 N2
+N1, N2 = map(int, sys.stdin.readline().split())
+
+A1 = sys.stdin.readline().rstrip()
+A2 = sys.stdin.readline().rstrip()
+
+# T초
+T = 0
