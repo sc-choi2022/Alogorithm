56e2c59a0c55b2d749508f6fedb16eb40e9d083b
sc-choi2022 <c.martin20222023@gmail.com>
2023-11-04 21:16:17 +0900

    1104

--- BOJ/BOJ_16194.py
+++ BOJ/BOJ_16194.py
@@ -1,13 +1,15 @@
 import sys
 
-# 카드의 개수 N
+# 민규가 구매하려는 카드의 개수 N
 N = int(sys.stdin.readline())
+# 카드팩의 가격 P
 P = [0] + list(map(int, sys.stdin.readline().split()))
-dp = [1000001] * (N+1)
+# i개의 카드를 구매했을 때 최솟값을 저장하는 배열 dp
+dp = [10000001] * (N+1)
 dp[0] = 0
 
-for i in range(N+1):
+for i in range(1, N+1):
     for j in range(i+1):
-        dp[i] = min(dp[i], dp[i-j] + p[j])
-
+        dp[i] = min(dp[i], dp[i-j] + P[j])
+# 카드 N개를 갖기 위해 지불해야 하는 금액의 최솟값을 출력
 print(dp[-1])
--- BOJ/BOJ_17413.py
+++ BOJ/BOJ_17413.py
@@ -1,24 +1,22 @@
 import sys
+# 문자열 S
 S = list(sys.stdin.readline().rstrip())
-
-i = 0
+idx = 0
 start = 0
-
-while i < len(S):
-    if S[i] == '<':       # 열린 괄호를 만나면
-        i += 1
-        while S[i] != '>':      # 닫힌 괄호를 만날 때 까지
-            i += 1
-        i += 1               # 닫힌 괄호를 만난 후 인덱스를 하나 증가시킨다
-    elif S[i].isalnum(): # 숫자나 알파벳을 만나면
-        start = i
-        while i < len(S) and S[i].isalnum():
-            i += 1
-        tmp = S[start:i] # 숫자,알파벳 범위에 있는 것들을
-        tmp.reverse()       # 뒤집는다
-        S[start:i] = tmp
-    else:                   # 괄호도 아니고 알파,숫자도 아닌것 = 공백
-        i += 1                # 그냥 증가시킨다
-
+while idx < len(S):
+    # 태그의 시작인 경우
+    if S[idx] == '<':
+        while S[idx] != '>':
+            idx += 1
+        idx += 1
+    # 태그가 아닌 문자인 경우
+    elif S[idx].isalnum():
+        start = idx
+        while idx < len(S) and S[idx].isalnum():
+            idx += 1
+        S[start:idx] = S[start:idx][::-1]
+    # 태그 밖 띄어쓰기 인 경우
+    else:
+        idx += 1
 # 문자열 S의 단어를 뒤집어서 출력
 print(''.join(S))
