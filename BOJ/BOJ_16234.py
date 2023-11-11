a873bbdb266840ed9cba10e7ee08893aefd326fd
sc-choi2022 <c.martin20222023@gmail.com>
2023-11-11 20:34:25 +0900

    1111

--- BOJ/BOJ_16234.py
+++ BOJ/BOJ_16234.py
@@ -1,9 +1,48 @@
 from collections import deque
 import sys
 
+def bfs(si, sj):
+    queue = deque([(si, sj)])
+    # 이동가능한 위치를 저장하는 배열 tmp, 위치들의 인구수를 저장하는 변수 add
+    tmp, add = [], 0
+
+    while queue:
+        ci, cj = queue.popleft()
+        for di, dj in (0, 1), (1, 0), (0, -1), (-1, 0):
+            ni, nj = ci + di, cj + dj
+            if 0 <= ni < N and 0 <= nj < N and L <= abs(nation[ni][nj] - nation[ci][cj]) <= R and not visit[ni][nj]:
+                visit[ni][nj] = 1
+                queue.append((ni, nj))
+                tmp.append((ni, nj))
+                add += nation[ni][nj]
+    return [tmp, add]
+
 # N×N 크기의 땅, L명 이상, R명 이하
 N, L, R = map(int, sys.stdin.readline().split())
 # 각 나라의 인구수를 담을 배열 nation
 nation = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 # 인구 이동이 일어나는 일수 day
 day = 0
+
+while True:
+    visit = [[0]*N for _ in range(N)]
+    flag = False
+
+    for i in range(N):
+        for j in range(N):
+            if not visit[i][j]:
+                visit[i][j] = 1
+                # 이동하는 위치를 담은 배열 moves return
+                moves, number = bfs(i, j)
+                if len(moves):
+                    flag = True
+                    moves.append((i, j))
+                    number += nation[i][j]
+                    num = number // len(moves)
+                    for mi, mj in moves:
+                        nation[mi][mj] = num
+    if not flag:
+        break
+    day += 1
+# 인구 이동이 일어나는 일수 출력
+print(day)
--- BOJ/BOJ_2852.py
+++ BOJ/BOJ_2852.py
@@ -2,21 +2,68 @@ import sys
 
 # 골이 들어간 횟수 N
 N = int(sys.stdin.readline())
-# 팀 1, 2가 이긴 횟수를 저장하는 변수 win1, win2 각 팀의 이긴 시간 time1, time2
-win1, win2, time1, time2 = 0, 0, 0, 0
+# 이전 경기에 대한 정보를 저장하는 배열 pre [이기고 있는 팀/ 마지막 골 시간]
+pre = [0, 0]
+# 팀1과 팀2의 현재 골 수와 이긴 시간을 저장하는 배열 team1, team2
+team1, team2 = [0, 0], [0, 0]
 
-tmp = 0
 for _ in range(N):
-    # 팀 번호 team, 득점시간 time
-    team, time = sys.stdin.readline().rstrip().split()
-    # 득점시간 MM, SS
+    # 골을 넣은 팀 team, 시간 time
+    team, time = sys.stdin.readline().split()
+    # 득점시간 분 MM, 초 SS
     MM, SS = map(int, time.split(':'))
+    MMSS = 60 * MM + SS
+    # 동점이었던 경우
+    if pre[0] == 0:
+        if team == '1':
+            team1[0] += 1
+            pre = [1, MMSS]
+        else:
+            team2[0] += 1
+            pre = [2, MMSS]
+    # 이기고 있는 팀이 팀1인 경우
+    elif pre[0] == 1:
+        if team == '1':
+            team1[0] += 1
+            team1[1] += MMSS - pre[1]
+            pre[1] = MMSS
+        # 팀2가 골을 넣은 경우
+        else:
+            # 여전히 팀1이 이기는 경우
+            if team1[0] > team2[0] + 1:
+                team1[1] += MMSS - pre[1]
+                pre = [1, MMSS]
+            # 동점이 되는 경우
+            elif team1[0] == team2[0] + 1:
+                team1[1] += MMSS - pre[1]
+                pre = [0, MMSS]
+            team2[0] += 1
+    # 이기고 있는 팀이 팀2인 경우
+    else:
+        if team == '2':
+            team2[0] += 1
+            team2[1] += MMSS - pre[1]
+            pre[1] = MMSS
+        # 팀1이 골을 넣은 경우
+        else:
+            # 여전히 팀2가 이기는 경우
+            if team1[0] + 1 < team2[0]:
+                team2[1] += MMSS - pre[1]
+                pre = [2, MMSS]
+            # 동점이 되는 경우
+            elif team1[0] + 1 == team2[0]:
+                team2[1] += MMSS - pre[1]
+                pre = [0, MMSS]
+            team1[0] += 1
 
-    if win1 > win2:
-        time1 += (MM*60 + SS) - tmp
-    elif win2 < win2:
-        time2 += (MM*60 + SS) - tmp
-        
-TMM1, TSS1, TMM2, TSS2 = str(time1//60), str(time1%60), str(time2//60), str(time2%60)
-print(f'{TMM1 if len(TMM1) == 2 else "0"+TSS1}:{TSS1 if len(TSS1) == 2 else "0"+TSS1}')
-print(f'{TMM1 if len(TMM2) == 2 else "0"+TSS2}:{TSS2 if len(TSS2) == 2 else "0"+TSS2}')
+# 팀 1이 이기고 있는 경우
+if pre[0] == 1:
+    team1[1] += 48*60 - MMSS
+# 팀 2이 이기고 있는 경우
+elif pre[0] == 2:
+    team2[1] += 48*60 - MMSS
+
+# 첫째 줄에 1번 팀이 이기고 있던 시간, 둘째 줄에 2번 팀이 이기고 있던 시간을 출력
+MM1, SS1, MM2, SS2 = str(team1[1]//60), str(team1[1]%60), str(team2[1]//60), str(team2[1]%60)
+print(f'{MM1 if len(MM1) == 2 else "0"+MM1}:{SS1 if len(SS1) == 2 else "0"+SS1}')
+print(f'{MM2 if len(MM2) == 2 else "0"+MM2}:{SS2 if len(SS2) == 2 else "0"+SS2}')
