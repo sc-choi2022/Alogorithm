b51865b8c47a85717f31956c48d1f4427bcf3b46
sc-choi2022 <c.martin20222023@gmail.com>
2023-11-12 22:43:20 +0900

    1112

--- BOJ/BOJ_11508.py
+++ BOJ/BOJ_11508.py
@@ -1,11 +1,14 @@
 import sys
 
+# 유제품의 수 N
 N = int(sys.stdin.readline())
+# 유제품의 가격을 저장하는 배열 C
 C = [int(sys.stdin.readline()) for _ in range(N)]
 C.sort(reverse=True)
+# 유제품을 모두 살 때 필요한 최소비용 answer
+answer = sum(C)
 
-answer = 0
 for i in range(2, N, 3):
-    answer += C[i]
-
-print(sum(C) - answer)
+    answer -= C[i]
+# N개의 유제품을 모두 살 때 필요한 최소비용을 출력
+print(answer)
--- BOJ/BOJ_1735.py
+++ BOJ/BOJ_1735.py
@@ -1,14 +1,16 @@
 import sys
 
 def gcd(numerator, denominator):
-    while numerator % denominator:
-        mod = numerator % denominator
+    while numerator%denominator:
+        mod = numerator%denominator
         numerator = denominator
         denominator = mod
     return denominator
 
-a, b = map(int, sys.stdin.readline().split())
-c, d = map(int, sys.stdin.readline().split())
+# 분자 A 분모 B
+A, B = map(int, sys.stdin.readline().split())
+# 분자 C 분모 D
+C, D = map(int, sys.stdin.readline().split())
 
-g = gcd(a*d+b*c, b*d)
-print((a*d+b*c)//g, (b*d)//g)
+number = gcd(A*D+B*C, B*D)
+print((A*D+B*C)//number, B*D//number)
