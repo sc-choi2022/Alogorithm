228f4711f7fb549b08159056b6f38d02e818d098
sc-choi2022 <c.martin20222023@gmail.com>
2023-11-16 23:44:53 +0900

    1116

---
+++ BOJ/BOJ_4134.py
@@ -0,0 +1,22 @@
+import sys
+
+def isPrime(number):
+    if number == 0 or number == 1:
+        return False
+    for i in range(2, int(N**0.5)+1):
+        if N % i == 0:
+            return False
+    return True
+
+# 테스트 케이스의 개수 T
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    # 정수 N
+    N = int(sys.stdin.readline())
+    while True:
+        if isPrime(N):
+            print(N)
+            break
+        else:
+            N += 1
