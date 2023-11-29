967caaea785a9ff5abcae2843cf29dbcdb9ee7e0
sc-choi2022 <c.martin20222023@gmail.com>
2023-11-29 22:33:59 +0900

    1129

--- BOJ/BOJ_15970.py
+++ BOJ/BOJ_15970.py
@@ -2,25 +2,22 @@ import sys
 
 # 접들의 개수 N
 N = int(sys.stdin.readline())
-# 점의 위치와 색상정보를 저장하는 배열 dots
-dots = [(0, 0)] * N
-# 화살표들의 길이의 합 answer
+# 점의 색을 idx로 위치를 저장하는 배열 dots
+dots = [[] for _ in range(N)]
+# 화살표들의 길이 합 answer
 answer = 0
 
-for i in range(N):
-    dots[i] = tuple(map(int, sys.stdin.readline().split()))
-dots.sort(key=lambda x:x[0])
+for _ in range(N):
+    # 위치 l, 색 c
+    l, c = map(int, sys.stdin.readline().split())
+    dots[c-1].append(l)
 
-for j in range(N):
-    tmp = N
-    for k in range(j-1, -1, -1):
-        if dots[j][1] == dots[k][1]:
-            tmp = dots[j][0] - dots[k][0]
-            break
-
-    for l in range(j+1, N):
-        if dots[j][1] == dots[l][1]:
-            tmp = min(tmp, dots[l][0]-dots[j][0])
-            break
-    answer += tmp
+for dot in dots:
+    if not dot:
+        continue
+    dot.sort()
+    answer += dot[1]-dot[0] + dot[-1]-dot[-2]
+    for i in range(1, len(dot)-1):
+        answer += min(dot[i]-dot[i-1], dot[i+1]-dot[i])
+# 표준 출력으로 모든 점에서 시작하는 화살표들의 길이 합을 출력
 print(answer)
--- BOJ/BOJ_4134.py
+++ BOJ/BOJ_4134.py
@@ -3,8 +3,8 @@ import sys
 def isPrime(number):
     if number == 0 or number == 1:
         return False
-    for i in range(2, int(N**0.5)+1):
-        if N % i == 0:
+    for i in range(2, int(number**0.5)+1):
+        if number % i == 0:
             return False
     return True
 
