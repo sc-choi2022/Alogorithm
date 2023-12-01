7980bdaccb4d984a8bb00d1c27eaff795130489d
sc-choi2022 <c.martin20222023@gmail.com>
2023-12-01 23:50:30 +0900

    1201

--- BOJ/BOJ_9658.py
+++ BOJ/BOJ_9658.py
@@ -2,4 +2,16 @@ import sys
 
 # 돌의 개수 N
 N = int(sys.stdin.readline())
-dp = [0]*1001
+dp = [0]*(N+1)
+
+for i in range(N+1):
+    flag = False
+    for stone in (1, 3, 4):
+        if i-stone <= 0:
+            break
+        if dp[i-stone]:
+            flag = True
+            break
+    if not flag:
+        dp[i] = 1
+print('CY' if dp[N] else 'SK')
