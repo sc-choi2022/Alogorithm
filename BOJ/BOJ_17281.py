608633ee2d9989d022108316b32b84d3bc94e2ea
sc-choi2022 <c.martin20222023@gmail.com>
2023-12-17 23:42:54 +0900

    1217

--- BOJ/BOJ_17281.py
+++ BOJ/BOJ_17281.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 import sys
 
 # 이닝 수 N
@@ -6,3 +7,18 @@ N = int(sys.stdin.readline())
 score = 0
 # 이닝의 점수를 저장하는 배열 innings
 innings = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+for permu in list(permutations(range(1,9), 8)):
+    # 1번 선수는 고정으로 4번타자 이므로 순서에 맞춰 정해준다.
+    permu = permu[:3] + [0] + permu[3:]
+    #
+
+    # 현재 선수순서로 이닝 진행시 점수 now
+    now = 0
+    # 각 베이스의 선수상황을 저장하는 plates
+    plates = [0, 0, 0, 0]
+
+    while True:
+        # 아웃이 3번이면 이닝이 끝난다.
+        if out == 3:
+            break
