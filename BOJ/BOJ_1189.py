ba8362e97298c75e09df013653192a98c713fa5c
sc-choi2022 <c.martin20222023@gmail.com>
2023-12-22 16:41:37 +0900

    1222

--- BOJ/BOJ_1189.py
+++ BOJ/BOJ_1189.py
@@ -1,9 +1,29 @@
 import sys
 
-def dfs():
-    return
+def dfs(ci, cj, cnt):
+    global answer
+
+    if cnt > K:
+        return
+    if (ci, cj) == (0, C-1) and cnt == K:
+        answer += 1
+        return
+    for di, dj in (0, 1), (1, 0), (0, -1), (-1, 0):
+        ni, nj = ci + di, cj + dj
+
+        if 0 <= ni < R and 0 <= nj < C and graph[ni][nj] != 'T' and not visit[ni][nj]:
+            visit[ni][nj] = 1
+            dfs(ni, nj, cnt+1)
+            visit[ni][nj] = 0
 
 # 맵의 크기 RxC, 거리의 길이 K
 R, C, K = map(int, sys.stdin.readline().split())
 # 맵의 내용을 저장하는 배열 graph
 graph = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
+visit = [[0]*C for _ in range(R)]
+visit[R-1][0] = 1
+# 거리가 K인 가짓수 answer
+answer = 0
+
+dfs(R-1, 0, 1)
+print(answer)
--- BOJ/BOJ_3182.py
+++ BOJ/BOJ_3182.py
@@ -1,17 +1,30 @@
 import sys
 
-def dfs():
-    return
-#
-N = int(sys.stdin.readline())
-graph = [[] for _ in range(N+1)]
+def dfs(number):
+    visit = [0]*(N+1)
+    visit[number] = 1
+    stack = [number]
 
-for i in range(1, N+1):
-    # 선배의 대답 answer
-    answer = int(sys.stdin.readline())
-    graph[answer].append(i)
+    while stack:
+        current = stack.pop()
+        if not visit[graph[current]]:
+            visit[graph[current]] = 1
+            stack.append(graph[current])
+
+    return sum(visit)
+
+# 선배의 명수 N
+N = int(sys.stdin.readline())
+# 선배들의 연결정보를 저장하는 배열 graph
+graph = [0] + [int(sys.stdin.readline()) for _ in range(N)]
+# 현재까지 가장 많은 만나는 수 num, 선배의 번호 answer
+num, answer = 0, 0
 
 for j in range(1, N+1):
     tmp = dfs(j)
 
-print(result)
+    if tmp > num:
+        num = tmp
+        answer = j
+# answer 출력
+print(answer)
