4dea97d3e1b4769cf18d4f3dcf1f245ea86f6eac
sc-choi2022 <c.martin20222023@gmail.com>
2023-12-25 23:27:05 +0900

    1225

--- BOJ/BOJ_17281.py
+++ BOJ/BOJ_17281.py
@@ -8,11 +8,9 @@ score = 0
 # 이닝의 점수를 저장하는 배열 innings
 innings = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 # 1번 선수를 제외한 선수들의 모든 라인업을 저장하는 배열 permu
-permu = list(permutations(range(1,9), 8))
-
-for p in permu:
+for p in permutations(range(1,9), 8):
     # 1번 선수는 고정으로 4번타자 이므로 순서에 맞춰 정해준다.
-    p = p[:3] + [0] + p[3:]
+    p = list(p[:3]) + [0] + list(p[3:])
 
     # 배열 p의 현재 선수 위치 batter
     batter = 0
@@ -29,7 +27,6 @@ for p in permu:
         while True:
             if out == 3:
                 break
-
             # i번째 이닝의 타자 batter의 정보를 저장하는 변수 hit
             hit = innings[i][p[batter]]
 
@@ -41,7 +38,19 @@ for p in permu:
                 # 3루의 값을 스코어에 저장해준다.
                 now += plates[2]
                 # 베이스플레이트 상황을 갱신
-                print(plates)
-                plates = [1] + plates[:2]
-                print(plates)
-                print('---------------------------------')
+                plates = [1, plates[0], plates[1]]
+            elif hit == 2:
+                now += plates[1] + plates[2]
+                plates = [0, 1, plates[0]]
+            elif hit == 3:
+                now += plates[0] + plates[1] + plates[2]
+                plates = [0, 0, 1]
+            else:
+                now += plates[0] + plates[1] +plates[2] + 1
+                plates = [0, 0, 0]
+            batter = (batter+1)%9
+    score = max(score, now)
+# 얻을 수 있는 최대 점수를 출력
+print(score)
+
+# dfs
