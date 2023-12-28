7b851506ae27fd1b9fd8294a1ef7f8090016c138
sc-choi2022 <c.martin20222023@gmail.com>
2023-12-28 23:12:12 +0900

    1228

--- BOJ/BOJ_5557.py
+++ BOJ/BOJ_5557.py
@@ -1,15 +1,16 @@
 import sys
 
-def dfs():
-    global answer
-
-
 # 숫자의 개수 N
 N = int(sys.stdin.readline())
 numbers = list(map(int, sys.stdin.readline().split()))
-visit = [0] * N
-# 만들 수 있는 올바른 등식의 개수 answer
-answer = 0
+dp = [[0]*21 for _ in range(N-1)]
+dp[0][numbers[0]] = 1
 
+for i in range(1, N-1):
+    for j in range(21):
+        if 0 <= j - numbers[i]:
+            dp[i][j-numbers[i]] += dp[i-1][j]
+        if j + numbers[i] <= 20:
+            dp[i][j+numbers[i]] += dp[i-1][j]
 
-print(answer)
+print(dp[-1][numbers[-1]])
