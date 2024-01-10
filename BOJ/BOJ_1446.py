ffa25d629013c12e32611805b6e82fe96ca82102
sc-choi2022 <c.martin20222023@gmail.com>
2024-01-10 18:41:47 +0900

    0110

--- BOJ/BOJ_1446.py
+++ BOJ/BOJ_1446.py
@@ -2,13 +2,25 @@ import sys
 
 # 지름길의 개수 N과 고속도로의 길이 D
 N, D = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(D+1)]
+# 지름길의 정보를 저장하는 배열 shortcuts
+shortcuts = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+shortcuts.sort(key=lambda x:(x[0], x[1], x[2]))
+# i 위치까지 가는데 걸리는 최단거리를 저장하는 배열 dp
+dp = [i for i in range(D+1)]
 
-for i in range(D):
-    graph[i].append((i+1, 1))
+for j in range(D+1):
+    # j번째 위치까지의 길이를 최솟값으로 갱신
+    dp[j] = min(dp[j], dp[j-1]+1)
 
-for j in range(N):
-    start, end, length = map(int, sys.stdin.readline().split())
-    if end > D:
-        continue
-    graph[start].append((end, length))
+    # 지름길이 존재하는 경우
+    while shortcuts:
+        # j 위치가 가장 근처의 지름길의 위치가 아닌 경우 break
+        if shortcuts[0][0] > j:
+            break
+        # 지름길의 시작위치 S, 도착위치 E, 지름길의 길이 L
+        S, E, L = shortcuts.pop(0)
+        # 지름길을 통해 목표 길이내에 거리 갱신이 가능한 경우
+        if S == j and E <= D and dp[j] + L < dp[E]:
+            dp[E] = dp[j] + L
+# 운전해야하는 최솟값을 출력
+print(dp[D])
--- BOJ/BOJ_14469.py
+++ BOJ/BOJ_14469.py
@@ -7,12 +7,13 @@ cows = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 cows.sort(key=lambda x:(x[0], x[1]))
 # 모든 소가 농장에 입장하는 데 걸리는 최소 시간 answer
 answer = -1
-
+print(cows)
 for cow in cows:
     # 소의 도착시간이 answer 이상인 경우
     if cow[0] >= answer:
         answer = sum(cow)
-    # 이동시간을 추가가    else:
+    # 이미 소가 도착한 경우 이동시간을 추가
+    else:
         answer += cow[1]
 # answer 출력
 print(answer)
