b798b5098280efa03018d1e10075fffa6c9bc92f
sc-choi2022 <c.martin20222023@gmail.com>
2024-01-12 16:23:29 +0900

    0112

--- BOJ/BOJ_1577.py
+++ BOJ/BOJ_1577.py
@@ -1,39 +1,39 @@
 import sys
 
-def walk():
-    for i in range(1, M+1):
-        for j in range(1, N+1):
-            if roads[i-1][j] == -1 and roads[i][j-1] == -1:
-                return
-            elif roads[i-1][j] == -1:
-                roads[i][j] = roads[i][j-1]
-            elif roads[i][j-1] == -1:
-                roads[i][j] = roads[i-1][j]
-            else:
-                roads[i][j] = roads[i-1][j] + roads[i][j-1]
+# 길이 공사중인지 확인하는 함수 check
+def check(current, x1, y1, x2, y2):
+    if current == [x1, y1, x2, y2] or current == [x2, y2, x1, y1]:
+        return True
+    else:
+        return False
 
 # 도로의 가로 크기 N, 세로 크기 M
 N, M = map(int, sys.stdin.readline().split())
 # 공사중인 도로의 개수 K
 K = int(sys.stdin.readline())
-# 도록
-roads = [[0]*(N+1) for _ in range(M+1)]
+# 공사정보를 저장하는 배열 roads
+roads = [list(map(int, sys.stdin.readline().split())) for _ in range(K)]
+# [i][j]위치까지 최단거리로 가는 경우의 수를 저장하는 배열 dp
+dp = [[0]*(N+1) for _ in range(M+1)]
+# 세준이의 집에서 출발하는 방법 1가지를 저장
+dp[0][0] = 1
 
-for _ in range(K):
-    a, b, c, d = map(int, sys.stdin.readline().split())
-    roads[a][b], roads[c][d] = -1, -1
-roads[0][0], roads[M][N] = 0, 0
-
-for ii in range(M+1):
-    if roads[ii][0] == -1:
-        break
-    roads[ii][0] = 1
-for jj in range(N+1):
-    if roads[0][jj] == -1:
-        break
-    roads[0][jj] = 1
-walk()
-
-for road in roads:
-    print(*road)
-print(roads[M][N])
+for i in range(M+1):
+    for j in range(N+1):
+        # 시작점이 아닌 곳
+        # 가로방향으로 움직이는 경우
+        if j:
+            for a, b, c, d in roads:
+                if check([i, j-1, i, j], b, a, d, c):
+                    break
+            else:
+                dp[i][j] += dp[i][j-1]
+        # 세로방향으로 움직이는 경우
+        if i:
+            for a, b, c, d in roads:
+                if check([i-1, j, i, j], b, a, d, c):
+                    break
+            else:
+                dp[i][j] += dp[i-1][j]
+# (0, 0)에서 (N, M)까지 가는 경우의 수를 출력
+print(dp[M][N])
--- BOJ/BOJ_25192.py
+++ BOJ/BOJ_25192.py
@@ -2,6 +2,20 @@ import sys
 
 # 채팅방의 기록 수 N
 N = int(sys.stdin.readline())
+# 채팅 기록 중 곰곰티콘이 사용된 횟수 answer
+answer = 0
+# ENTER를 기점으로 곰곰티콘을 사용하는 사람을 저장하는 셋 cnt
+cnt = set()
 
 for _ in range(N):
     recode = sys.stdin.readline().rstrip()
+
+    # 새로운 유저가 입장한 경우, 즉 곰곰티콘이 사용되는 조건
+    if recode == 'ENTER':
+        answer += len(cnt)
+        cnt = set()
+        continue
+    # 곰곰티콘을 사용한 유저를 저장
+    cnt.add(recode)
+# 채팅 기록 중 곰곰티콘이 사용된 횟수를 출력
+print(answer+len(cnt) if cnt else answer)
