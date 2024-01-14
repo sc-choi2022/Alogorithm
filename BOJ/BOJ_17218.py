cfb9fb023f82b3ee89b572f577f3de117ac14123
sc-choi2022 <c.martin20222023@gmail.com>
2024-01-14 18:39:50 +0900

    0114

--- BOJ/BOJ_17218.py
+++ BOJ/BOJ_17218.py
@@ -3,5 +3,26 @@ import sys
 # 두 개의 문자열 S1, S2
 S1 = list(sys.stdin.readline().rstrip())
 S2 = list(sys.stdin.readline().rstrip())
-# 두 문자열 중 긴 문자열의 길이 N
-N = max(len(S1), len(S2))
+# S1, S2의 길이 N, M
+N, M = len(S1), len(S2)
+dp = [[0]*(M+1) for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(M):
+        if S1[i] == S2[j]:
+            dp[i+1][j+1] = dp[i][j] + 1
+        else:
+            dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+
+answer = ''
+while dp[N][M]:
+    if dp[N][M] == dp[N][M-1]:
+        M -= 1
+    elif dp[N][M] == dp[N-1][M]:
+        N -= 1
+    else:
+        answer += S1[N-1]
+        N -= 1
+        M -= 1
+# 주어진 두 문자열로 만든 비밀번호를 출력
+print(answer[::-1])
--- BOJ/BOJ_17219.py
+++ BOJ/BOJ_17219.py
@@ -1,6 +1,8 @@
 import sys
 
+# 저장된 사이트 주소의 수 N과 비밀번호를 찾으려는 사이트 주소의 수 M
 N, M = map(int, sys.stdin.readline().split())
+# 주소와 비밀번호를 저장하는 딕셔너리 info
 info = {}
 
 for _ in range(N):
@@ -8,5 +10,7 @@ for _ in range(N):
     info[site] = pwd
 
 for _ in range(M):
+    # 비밀번호를 찾으려는 사이트 주소
     address = sys.stdin.readline().rstrip()
+    # 비밀번호를 찾으려는 사이트 주소의 비밀번호 출력
     print(info[address])
