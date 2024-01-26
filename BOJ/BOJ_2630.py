248b72819a3bd6326405009b5ff877bad7622397
sc-choi2022 <c.martin20222023@gmail.com>
2024-01-26 23:08:24 +0900

    0126

--- BOJ/BOJ_2630.py
+++ BOJ/BOJ_2630.py
@@ -5,21 +5,29 @@ def sol(x, y, N):
     color = paper[x][y]
     for i in range(x, x + N):
         for j in range(y, y + N):
+            # 현재 종이의 색이 다른 경우
             if color != paper[i][j]:
                 sol(x, y, N//2)
                 sol(x, y + N//2, N//2)
                 sol(x + N//2, y, N//2)
                 sol(x + N//2, y + N//2, N//2)
                 return
+
+    # 해당 색종이의 개수를 저장
     if color == 0:
         cnt0 += 1
     else:
         cnt1 += 1
 
+# 종이의 한 변의 길이 N
 N = int(sys.stdin.readline())
+# 색종이의 색의 저장하는 배열 paper
 paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
-
+# 하얀색의 개수 cnt0, 파란색의 개수 cnt1
 cnt0, cnt1 = 0, 0
+
 sol(0, 0, N)
+
+# 잘라진 햐얀색 색종이의 개수를 출력하고, 둘째 줄에는 파란색 색종이의 개수를 출력
 print(cnt0)
 print(cnt1)
--- BOJ/BOJ_2659.py
+++ BOJ/BOJ_2659.py
@@ -1,5 +1,30 @@
 from collections import deque
 import sys
 
-# 입력되는 카드를 저장하는 배열 card
+def find(number):
+    finding = deque(number)
+    # 반환하는 시계수 numberR
+    numberR = 10000
+
+    for _ in range(4):
+        tmp = finding[0]*1000+finding[1]*100+finding[2]*10+finding[3]
+        numberR = min(numberR, tmp)
+        finding.rotate(1)
+    return numberR
+
+# 입력되는 카드를 저장하는 배열의 시계수
 card = deque(map(int, sys.stdin.readline().split()))
+# 주어진 카드의 시계수
+clock = find(card)
+# 시계수를 개수를 저장하는 변수 answer
+answer = 0
+
+# 시작 시계수 start
+start = 1111
+while start <= clock:
+    if find(list(map(int, list(str(start))))) == start:
+        answer += 1
+    start += 1
+
+# 입력된 카드의 시계수가 모든 시계수들 중에서 몇 번째로 작은 시계수인지를 출력
+print(answer)
