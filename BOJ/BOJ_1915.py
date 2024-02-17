f721d1072c59e9da56c08b189d24c89041b39a54
sc-choi2022 <c.martin20222023@gmail.com>
2024-02-17 23:04:15 +0900

    0217

--- BOJ/BOJ_1915.py
+++ BOJ/BOJ_1915.py
@@ -1,27 +1,22 @@
 import sys
 
-# 배열의 크기 NxM
+# NxM의 배열의 길이 N, M
 N, M = map(int, sys.stdin.readline().split())
-square = [list(map(int, list(sys.stdin.readline().rstrip()))) for _ in range(N)]
+# NxM의 배열 graph
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
+# 정사각형의 최대 모서리 길이를 저장하는 배열 dp
 dp = [[0]*M for _ in range(N)]
 
+# 가장 큰 정사각형의 한변의 길이 answer
+answer = 0
 for i in range(N):
-    for j in range(1,M):
-        if square[i][j]:
-            square[i][j] = square[i][j-1] + 1
-
-for s in square:
-    print(*s)
-print('==================================')
-
-dp[0] = square[0][::]
-for ii in range(N):
-    for jj in range(M):
-        # if jj == 0 and
-        #     dp[ii][jj] = square[ii][jj]
-        if not square[ii-1][jj] and not square[ii][jj-1]:
-            dp[ii][jj] = square[ii][jj]
-        # if square[ii][jj] and dp[ii-1][jj] and dp[ii][jj-1]:
-        #     dp[ii][jj] =
-for d in dp:
-    print(*d)
+    for j in range(M):
+        if i == 0 or j == 0:
+            dp[i][j] = graph[i][j]
+        elif not graph[i][j]:
+            dp[i][j] = 0
+        else:
+            dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+        answer = max(answer, dp[i][j])
+# 가장 큰 정사각형의 넓이를 출력
+print(answer*answer)
