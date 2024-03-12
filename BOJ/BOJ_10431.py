f8dd1a3c7770c9b8a127c3e8dfc7972e089e56be
sc-choi2022 <c.martin20222023@gmail.com>
2024-03-12 22:57:39 +0900

    0312

--- BOJ/BOJ_10431.py
+++ BOJ/BOJ_10431.py
@@ -6,7 +6,6 @@ P = int(sys.stdin.readline())
 for _ in range(P):
     # 테스트 케이스 번호 T, 줄 서 있는 학생들의 키를 저장하는 배열 heights
     T, *heights = list(map(int, sys.stdin.readline().split()))
-    order = [heights[0]]
 
     answer = 0
     for i in range(20):
@@ -14,6 +13,6 @@ for _ in range(P):
             if heights[i] > heights[j]:
                 answer += 1
                 heights[i], heights[j] = heights[j], heights[i]
-                
+
     # 테스트 케이스의 번호와 학생들이 뒤로 물러난 걸음 수의 총합 출력
     print(T, answer)
