7fcde32c8ee7c69b64c4b4b971b58c4713328c25
sc-choi2022 <c.martin20222023@gmail.com>
2024-03-21 23:01:38 +0900

    0321

---
+++ BOJ/BOJ_4883.py
@@ -0,0 +1,22 @@
+import sys
+
+# 테스트 케이스 T
+T = 1
+
+while True:
+    N = int(sys.stdin.readline())
+    if N == 0:
+        break
+    dp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+    dp[1][0] += dp[0][1]
+    dp[1][1] += min(dp[0][1], dp[0][1] + dp[0][2], dp[1][0])
+    dp[1][2] += min(dp[0][1], dp[0][1] + dp[0][2], dp[1][1])
+
+    for i in range(2, N):
+        dp[i][0] += min(dp[i-1][0], dp[i-1][1])
+        dp[i][1] += min(dp[i-1][0], dp[i-1][1], dp[i-1][2], dp[i][0])
+        dp[i][2] += min(dp[i-1][1], dp[i-1][2], dp[i][1])
+
+    print(f'{T}. {dp[N - 1][1]}')
+    T += 1
