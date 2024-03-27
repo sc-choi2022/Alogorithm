e1ffa162a18bc1322e536f2530ccf98034a91a0c
sc-choi2022 <c.martin20222023@gmail.com>
2024-03-27 22:37:42 +0900

    0327

--- BOJ/BOJ_24511.py
+++ BOJ/BOJ_24511.py
@@ -1,14 +1,24 @@
-from collections import deque
 import sys
 
 # 자료구조의 개수 N
 N = int(sys.stdin.readline())
 
-# 자료구조를 저장하는 배열 A
+# 수열 B의 각 위치의 자료구조를 저장하는 배열 A
 A = list(map(int, sys.stdin.readline().split()))
 # 수열 B
-B = deque(list(map(int, sys.stdin.readline().split())))
+B = list(map(int, sys.stdin.readline().split()))
 # 삽입할 수열의 길이 M
 M = int(sys.stdin.readline())
 # queuestack에 삽입할 원소를 담고 있는 수열 C
 C = list(map(int, sys.stdin.readline().split()))
+result = []
+
+for i in range(N):
+    # queue인 경우
+    if not A[i]:
+        result.append(B[i])
+result.reverse()
+result = result + C
+
+# 리턴값을 공백으로 구분하여 출력
+print(*result[:M])
--- BOJ/BOJ_4883.py
+++ BOJ/BOJ_4883.py
@@ -10,13 +10,15 @@ while True:
     dp = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
     dp[1][0] += dp[0][1]
-    dp[1][1] += min(dp[0][1], dp[0][1] + dp[0][2], dp[1][0])
-    dp[1][2] += min(dp[0][1], dp[0][1] + dp[0][2], dp[1][1])
+    dp[1][1] += min(dp[1][0], dp[0][1], dp[0][1] + dp[0][2])
+    dp[1][2] += min(dp[1][1], dp[0][1], dp[0][1] + dp[0][2])
 
     for i in range(2, N):
         dp[i][0] += min(dp[i-1][0], dp[i-1][1])
         dp[i][1] += min(dp[i-1][0], dp[i-1][1], dp[i-1][2], dp[i][0])
         dp[i][2] += min(dp[i-1][1], dp[i-1][2], dp[i][1])
 
+    # 테스트 케이스 번호와 최소비용 출력
     print(f'{T}. {dp[N - 1][1]}')
+    # 테스트 케이스 수 1 증가
     T += 1
