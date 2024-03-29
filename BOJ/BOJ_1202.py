62bcea44552382e14c3a82db40fbc32edf098e74
sc-choi2022 <c.martin20222023@gmail.com>
2024-03-29 21:44:53 +0900

    0329

--- BOJ/BOJ_1202.py
+++ BOJ/BOJ_1202.py
@@ -1,18 +1,32 @@
+from heapq import heappush, heappop
 import sys
 
 # 보석의 개수 N, 가방의 개수 K
 N, K = map(int, sys.stdin.readline().split())
+jewel = []
 
-jewel = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
-jewel.sort(key=lambda x: (-x[1], x[0]))
+for _ in range(N):
+    heappush(jewel, tuple(map(int, sys.stdin.readline().split())))
 
 # 가방의 허용무게를 담을 리스트 bags
 bags = sorted(list(int(sys.stdin.readline()) for _ in range(K)))
-values = [0] * K
+# 훔칠 수 있는 보석 가격의 합의 최댓값 answer
+answer = 0
 
-for j in jewel:
-    for i in range(K):
-        if j[0] < bags[i] and values[i] == 0:
-            values[i] = j[1]
-            break
-print(sum(values))
+# 현재 가방에 넣을 수 있는 보석을 저장하는 배열 values
+values = []
+for bag in bags:
+    # 보석이 남아 있고 가방에 들어가는 보석이 존재할 때
+    while jewel and bag >= jewel[0][0]:
+        # 보석의 가격을 높은 값부터 정렬하기 위해 음수값으로 가격을 heappush
+        heappush(values, -heappop(jewel)[1])
+    # 훔칠 수 있는 보석이 있는 경우
+    # 가격이 가장 높은 보석을 answer에 더함
+    if values:
+        answer -= heappop(values)
+    # 확인할 보석이 없는 경우 break
+    elif not jewel:
+        break
+
+# answer 출력
+print(answer)
