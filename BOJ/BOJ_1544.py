a23b9d8a76ecc220f42e882c553e8058236100df
sc-choi2022 <c.martin20222023@gmail.com>
2024-04-05 23:20:17 +0900

    0405

---
+++ BOJ/BOJ_1544.py
@@ -0,0 +1,16 @@
+from collections import deque
+import sys
+
+def rotate_word(word1, word2):
+    return
+
+# 단어의 개수 N
+N = int(sys.stdin.readline())
+# 주어지는 단어 words
+words = [sys.stdin.readline().rstrip() for _ in range(N)]
+
+for i in range(N):
+    for j in range(1, N):
+        continue
+
+print()
