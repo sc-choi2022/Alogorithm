49ea24bc2793d0ed12643cb9676c2602c1c35113
sc-choi2022 <c.martin20222023@gmail.com>
2024-04-12 22:46:34 +0900

    0412

---
+++ BOJ/BOJ_14606.py
@@ -0,0 +1,15 @@
+import sys
+
+# 피자판의 개수 N
+N = int(sys.stdin.readline())
+dp = [0, 0, 1] + [0] * (N-2)
+
+for i in range(3, N+1):
+    number = i//2
+    if i%2:
+        dp[i] = dp[number] + dp[number+1] + i//2 * (number+1)
+    else:
+        dp[i] = 2 * dp[number] + number**2
+
+# 얻을 수 있는 즐거움의 총합의 최댓값을 한 줄에 출력
+print(dp[N])
