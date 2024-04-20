56f1ce64fe4fa5bd0d5f9bd4c7c7c569cc7c4d97
sc-choi2022 <c.martin20222023@gmail.com>
2024-04-20 17:40:50 +0900

    0420

--- BOJ/BOJ_1520.py
+++ BOJ/BOJ_1520.py
@@ -1,21 +1,25 @@
 import sys
 
 def dfs(si, sj):
+    # 목적지점에 도달한 경우
     if si == M-1 and sj == N-1:
         return 1
+
     if dp[si][sj] == -1:
         dp[si][sj] = 0
         for di, dj in (0, 1), (1, 0), (0, -1), (-1, 0):
             ni, nj = si + di, sj + dj
+            # 내리막 길인 경우
             if 0 <= ni < M and 0 <= nj < N and zido[si][sj] > zido[ni][nj]:
                 dp[si][sj] += dfs(ni, nj)
     return dp[si][sj]
 
-# 지도의 세로의 크기 M과 가로의 크기 N
+# 세로의 크기 M, 가로의 크기 N
 M, N = map(int, sys.stdin.readline().split())
-# 지도의 숫자를 담을 배열 zido
+# 지도의 높이를 저장하는 배열 zido
 zido = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
-# 각 위치에서 가능한 경로의 수를 담을 배열 dp
-dp = [[-1]*N for _ in range(M)]
+# (i, j)위치에서 목표지점까지는 가는 경로의 수
+dp = [[-1] * N for _ in range(M)]
 
+# 이동 가능한 경로의 수 출력
 print(dfs(0, 0))
---
+++ BOJ/BOJ_1679.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+
+# 정수의 개수 N
+N = int(sys.stdin.readline())
+# 정수를 저장하는 배열 numbers
+numbers = list(map(int, sys.stdin.readline().split()))
+# 최대 사용 횟수 K
+K = int(sys.stdin.readline())
+# 최대로 만들 수 있는 정수 M
+M = numbers[-1] * K
+# 최대로 만들 수 있는 수까지 필요한 사용횟수를 저장하는 배열 dp
+dp = [50001] * (M + 1)
+dp[0] = 0
+
+for i in range(M+1):
+    for number in numbers:
+        if number + i <= M:
+            dp[number+i] = min(dp[number+i], dp[i]+1)
+
+for j in range(1, M+1):
+    if dp[j] > K:
+        # 홀수가 이긴 경우
+        if not j%2:
+            print(f'holsoon win at {j}')
+        # 짝수가 이긴 경우
+        else:
+            print(f'jjaksoon win at {j}')
+        break
