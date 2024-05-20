a75ce7143e7634a6f475b3ca397082c9d5d9cf4e
sc-choi2022 <c.martin20222023@gmail.com>
2024-05-20 23:19:07 +0900

    0520

---
+++ BOJ/BOJ_20437.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+import sys
+
+# 문자열 게임의 수 T
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    cnt = defaultdict(list)
+    # 문자열 W
+    W = sys.stdin.readline().rstrip()
+    # 정수 K
+    K = int(sys.stdin.readline())
+    # 문자열 W의 길이 L
+    L = len(W)
+    # 가장 짧은 연속 문자열의 길이 minL, 가장 긴 연속 문자열의 길이 maxL
+    minL, maxL = L, 0
+
+    for i in range(len(W)):
+        cnt[W[i]].append(i)
+
+    for c in cnt.values():
+        # 문자를 K개 이상인 경우
+        if len(c) >= K:
+            for i in range(len(c)-K+1):
+                length = c[i+K-1]-c[i]+1
+                minL, maxL = min(minL, length), max(maxL, length)
+    # 문자열에 문자를 K개 미만으로 포함한 경우
+    if (minL, maxL) == (L, 0):
+        print(-1)
+    # 3번과 4번에서 구한 연속 문자열의 길이를 공백을 사이에 두고 출력
+    else:
+        print(minL, maxL)
