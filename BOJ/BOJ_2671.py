b2e420b7108f47ac2a91b58b12c8706c4f1e8350
sc-choi2022 <c.martin20222023@gmail.com>
2024-06-01 23:43:20 +0900

    0601

---
+++ BOJ/BOJ_2671.py
@@ -0,0 +1,12 @@
+from collections import deque
+import sys
+
+# 판별해야하는 스트링 S
+S = deque(sys.stdin.readline().rstrip())
+
+# 첫 번째 문자열을 판별하는 경우를 저장하는 변수 flag, 0의 개수 확인하는 변수 cnt
+flag, cnt0, cnt1 = False, 0, 0
+while S:
+    break
+else:
+    print('SUBMARINE')
