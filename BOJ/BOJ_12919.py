ba6f52787be9dd95cc3c3820529847bf894f45ab
sc-choi2022 <c.martin20222023@gmail.com>
2024-06-07 23:48:23 +0900

    0607

--- BOJ/BOJ_12919.py
+++ BOJ/BOJ_12919.py
@@ -1,4 +1,17 @@
 import sys
 
+def change(target):
+    print(S, target)
+    if len(S) == len(target):
+        if S == target:
+            return 1
+        return 0
+    if target[-1] == 'A':
+        change(target[:-1])
+    if target[0] == 'B':
+        
+
 S = sys.stdin.readline().rstrip()
 T = sys.stdin.readline().rstrip()
+
+print(change(T))
