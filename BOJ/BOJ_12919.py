8d114d74d2a94c330060eec21e20f7c7cb9a7308
sc-choi2022 <c.martin20222023@gmail.com>
2024-06-09 23:57:28 +0900

    0609

--- BOJ/BOJ_12919.py
+++ BOJ/BOJ_12919.py
@@ -1,17 +1,24 @@
 import sys
 
 def change(target):
-    print(S, target)
+    global answer
     if len(S) == len(target):
+        # targer이 S인 경우
         if S == target:
-            return 1
-        return 0
+            answer = 1
+        return
     if target[-1] == 'A':
         change(target[:-1])
     if target[0] == 'B':
-        
+        change(target[1:][::-1])
 
+# 문자열 S, T
 S = sys.stdin.readline().rstrip()
 T = sys.stdin.readline().rstrip()
+# S를 T로 바꿀 수 있는 여부를 저장하는 변수 answer
+answer = 0
 
-print(change(T))
+# 문자열 T를 S로 바꾸는 함수 change
+change(T)
+# answer 출력
+print(answer)
--- BOJ/BOJ_5648.py
+++ BOJ/BOJ_5648.py
@@ -1,5 +1,6 @@
 import sys
 
+# 원소의 개수 N, 원소를 저장하는 numbers
 N, *numbers = sys.stdin.read().split()
 
 # 원소를 거꾸로 뒤집고 오름차순으로 정렬
@@ -7,5 +8,6 @@ for i in range(int(N)):
     numbers[i] = int(numbers[i][::-1])
 numbers = sorted(list(numbers))
 
+# 오름차순으로 정렬하고 출력
 for number in numbers:
     print(number)
