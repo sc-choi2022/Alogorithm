bad202ed6495d741014a3d321aa7889e33f40af9
sc-choi2022 <c.martin20222023@gmail.com>
2024-06-23 23:56:02 +0900

    0623

--- BOJ/BOJ_2116.py
+++ BOJ/BOJ_2116.py
@@ -2,3 +2,8 @@ import sys
 
 # 주사위의 개수 N
 N = int(sys.stdin.readline())
+
+for _ in range(N):
+    # 주사위 각 면에 적혀진 숫자를 저장하는 배열 numbers
+    numbers = list(map(int, sys.stdin.readline().split()))
+    
