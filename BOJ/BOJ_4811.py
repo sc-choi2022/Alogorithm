317ec06aadad9bef2bb933ee6c31d4c373f6cb3d
sc-choi2022 <c.martin20222023@gmail.com>
2024-06-26 23:56:58 +0900

    0626

--- BOJ/BOJ_4811.py
+++ BOJ/BOJ_4811.py
@@ -2,6 +2,13 @@ import sys
 
 dp = [[0]*31 for _ in range(31)]
 
+for j in range(31):
+    dp[0][j] = 1
+
+for di in range(1, 31):
+    for dj in range(i, 31):
+        continue
+
 while True:
     N = int(sys.stdin.readline())
 
