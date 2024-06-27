49081aa4e13c553ec2aa2c5e8902e17ffc97a91c
sc-choi2022 <c.martin20222023@gmail.com>
2024-06-27 22:58:07 +0900

    0627

---
+++ BOJ/BOJ_6064.py
@@ -0,0 +1,20 @@
+import sys
+
+# 테스트 케이스의 수 T
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    # 달력의 마지막 해의 정수 M, N, 구해야하는 해의 값 x, y
+    M, N, x, y = map(int, sys.stdin.readline().split())
+    # 구해야하는 k번째 해를 나타내는 변수 K
+    K = -1
+
+    tmp = x
+    while tmp <= M*N:
+        if not (tmp-y)%N:
+            K = tmp
+            break
+        tmp += M
+        
+    # 정수 K 출력
+    print(K)
