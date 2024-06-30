540b2c787659c58b92d825f62945fc0665325be5
sc-choi2022 <c.martin20222023@gmail.com>
2024-06-30 23:43:32 +0900

    0630

--- BOJ/BOJ_1107.py
+++ BOJ/BOJ_1107.py
@@ -1,16 +1,39 @@
 import sys
 
+def dfs(idx, tmp, goal):
+    global difference
+
+    if idx == goal:
+        difference = min(difference, abs(int(N)-int(tmp))+L)
+        return
+
+    for i in range(10):
+        if idx == 0 and i == 0:
+            continue
+        if not wrong[i]:
+            dfs(idx+1, tmp+str(i), goal)
+
 # 이동하려고 하는 채널 N
-N = int(sys.stdin.readline())
+N = sys.stdin.readline().rstrip()
 # 고장난 버튼의 개수 M
 M = int(sys.stdin.readline())
-# 고장난 버튼을 담은 리스트 wrong
-wrong = set(sys.stdin.readline().rstrip().split()) if M else set()
-answer = abs(100-N)
-for num in range(1000001):
-    for n in str(num):
-        if n in wrong:
-            break
+L = len(N)
+
+if N == '100':
+    print(0)
+elif M == 0:
+    print(min(L, abs(int(N)-100)))
+else:
+    numbers = list(map(int, sys.stdin.readline().split()))
+    wrong = [0] * 10
+    for number in numbers:
+        wrong[number] = 1
+    check = [0]*L
+
+    for i in range(L):
+        if not wrong[int(N[i])]:
+            check[i] = 1
+    if sum(check) == L:
+        print(L)
     else:
-        answer = min(answer, len(str(num)) + abs(num - N))
-print(answer)
+        difference = 500001
