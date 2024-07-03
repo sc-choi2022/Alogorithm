cbd62a8523a5067890f18664aaaaba2587e51882
sc-choi2022 <c.martin20222023@gmail.com>
2024-07-03 22:14:53 +0900

    0703

--- BOJ/BOJ_2022.py
+++ BOJ/BOJ_2022.py
@@ -6,4 +6,22 @@ def function(X, Y, W):
     C = h1 * h2 / (h1 + h2)
     return C
 
-X, Y, C = map(int, sys.stdin.readline().split())
+# 주어지는 사다리의 길이 X, Y와 두 사다리가 교차하는 높이 C
+X, Y, C = map(float, sys.stdin.readline().split())
+# 두 빌딩사이에 너비를 찾는 범위 S, E
+S, E = 0, min(X, Y)
+# 두 빌딩사이에 너비가 되는 수치 result
+result = 0
+
+# 오차가 10**-3까지 허용
+while E-S > 0.001:
+    M = (S+E)/2
+
+    if function(X, Y, M) >= C:
+        result = M
+        S = M
+    else:
+        E = M
+
+# result을 출력
+print(result)
