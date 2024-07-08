f1fb4125ae770bdf1b408e0869e8c24d405e3ff3
sc-choi2022 <c.martin20222023@gmail.com>
2024-07-08 23:51:44 +0900

    0708

--- BOJ/BOJ_2240.py
+++ BOJ/BOJ_2240.py
@@ -5,3 +5,8 @@ T, W = map(int, sys.stdin.readline().split())
 # 자두가 떨어지는 나무의 번호를 저장하는 배열 plum
 plum = [int(sys.stdin.readline()) for _ in range(T)]
 dp = [[0]*T for _ in range(T)]
+dp[0][0], dp[0][1] = plum[0]%2, plum[0]//2
+
+for t in range(1, T):
+    for w in range(1, W):
+        
