606d220593962a84d3f34ea47dd1f27be187d7d9
sc-choi2022 <c.martin20222023@gmail.com>
2024-07-13 23:37:29 +0900

    0713

--- BOJ/BOJ_9322.py
+++ BOJ/BOJ_9322.py
@@ -9,4 +9,15 @@ for _ in range(T):
     # 공개키 PK1, PK2
     PK1 = sys.stdin.readline().rstrip().split()
     PK2 = sys.stdin.readline().rstrip().split()
-    
+    # 공개키 PK2에 따른 PK1의 위치를 저장하는 배열 idxs
+    idxs = [0]*N
+
+    for i in range(N):
+        idxs[i] = PK2.index(PK1[i])
+
+    # 암호문 words
+    words = sys.stdin.readline().rstrip().split()
+    # 암호문을 해독한 평문 출력
+    for j in range(N):
+        print(words[idxs[j]], end=' ')
+    print()
