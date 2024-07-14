ce2a481342f9184bb5fe7299fcff69bccdbc5b81
sc-choi2022 <c.martin20222023@gmail.com>
2024-07-14 23:03:06 +0900

    0714

--- BOJ/BOJ_2023.py
+++ BOJ/BOJ_2023.py
@@ -1,7 +1,23 @@
 import sys
 
-def dfs():
-    return 
+# 소수를 확인하는 함수 prime
+def prime(number):
+    for i in range(2, int(number**0.5)+1):
+        if number % i == 0:
+            return False
+    return True
 
-# 소수의 N자리 수 정수 N
+def dfs(num):
+    if len(str(num)) == N:
+        print(num)
+        return
+    for j in range(1, 10, 2):
+        if prime(num * 10 + j):
+            dfs(num * 10 + j)
+
+# 소수의 자리 수 N
 N = int(sys.stdin.readline())
+
+# 한 자리수 소수로 매개변수로 dfs 실행
+for p in (2, 3, 5, 7):
+    dfs(p)
