f023915a6ac04e9c6bcc45b49515906f3228972d
sc-choi2022 <c.martin20222023@gmail.com>
2024-07-31 23:38:34 +0900

    0731

--- BOJ/BOJ_12026.py
+++ BOJ/BOJ_12026.py
@@ -4,12 +4,14 @@ import sys
 N = int(sys.stdin.readline())
 # 보도블럭에 쓰여있는 글자 words
 words = sys.stdin.readline().rstrip()
-dp = [1000] * N
+# 보도블록에 도달했을 때의 최솟값을 저장하는 배열 dp
+# 기본 값을 최댓값 1e6, 0번 위치의 값을 0으로 초기화
+dp = [1e6] * N
 dp[0] = 0
 
 for i in range(N):
     for j in range(i+1, N):
-        if dp[i] != -1:
+        if dp[i] != 1e6:
             if words[i] == 'B' and words[j] == 'O':
                 dp[j] = min(dp[j], dp[i] + (j-i)**2)
             elif words[i] == 'O' and words[j] == 'J':
@@ -17,4 +19,6 @@ for i in range(N):
             elif words[i] == 'J' and words[j] == 'B':
                 dp[j] = min(dp[j], dp[i] + (j-i)**2)
 
-print(dp[-1] if dp[-1] != 1000 else -1)
+# 스타트가 링크를 만나는데 필요한 에너지 양의 최솟값을 출력
+# 스타트가 링크를 만날 수 없는 경우에는 -1을 출력
+print(dp[-1] if dp[-1] != 1e6 else -1)
