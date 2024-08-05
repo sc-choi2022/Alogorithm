2ded7849ffe2e2fafc91022d55f19bbd7e32da49
sc-choi2022 <c.martin20222023@gmail.com>
2024-08-05 22:28:53 +0900

    0805

--- BOJ/BOJ_1660.py
+++ BOJ/BOJ_1660.py
@@ -2,13 +2,29 @@ import sys
 
 # 대포알의 개수 N
 N = int(sys.stdin.readline())
-balls = []
+# 대포알을 이루는 삼각형 여부를 저장하는 배열 balls
+balls = [0] * (N+1)
 B = 0
-i = 1
+m = 1
 
-while B < N:
-    B += (i * (i+1))//2
-    balls.append(B)
-    i += 1
+while True:
+    B += (m * (m+1))//2
+    if B < N:
+        break
+    balls[B] = 1
+    m += 1
+
 MAX = sys.maxsize
 dp = [MAX] * (N+1)
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if balls[j]:
+            if j >= i:
+                if j == i:
+                    dp[i] = 1
+                break
+            dp[i] = min(dp[i], dp[i-j]+1)
+print(dp[N])
+
+print(dp)
