45b97caa8f51cd261dab65189fec6a68a76e9838
sc-choi2022 <c.martin20222023@gmail.com>
2024-08-07 23:48:09 +0900

    0807

--- BOJ/BOJ_1660.py
+++ BOJ/BOJ_1660.py
@@ -3,7 +3,7 @@ import sys
 # 대포알의 개수 N
 N = int(sys.stdin.readline())
 # 대포알을 이루는 삼각형 여부를 저장하는 배열 balls
-balls = [0] * (N+1)
+balls = []
 B = 0
 m = 1
 
@@ -11,18 +11,18 @@ while True:
     B += (m * (m+1))//2
     if B > N:
         break
-    balls[B] = 1
+    balls.append(B)
     m += 1
 
 MAX = sys.maxsize
 dp = [MAX] * (N+1)
 
 for i in range(1, N+1):
-    for j in range(1, N+1):
-        if balls[j] == 1:
-            if j >= i:
-                if j == i:
-                    dp[i] = 1
-                break
-            dp[i] = min(dp[i], dp[i-j]+1)
+    for ball in balls:
+        if ball >= i:
+            if ball == i:
+                dp[i] = 1
+            break
+        dp[i] = min(dp[i], dp[i-ball]+1)
+# 만들 수 있는 사면체 개수의 최솟값을 출력
 print(dp[N])
