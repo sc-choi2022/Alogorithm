af45a5cf2b027cf089cae2b7500564c39ee42dad
sc-choi2022 <c.martin20222023@gmail.com>
2024-08-13 18:10:01 +0900

    0813

--- BOJ/BOJ_15658.py
+++ BOJ/BOJ_15658.py
@@ -2,7 +2,7 @@ import sys
 
 def dfs(idx, numb):
     global maxN, minN
-    if idx == N-1:
+    if idx == N:
         maxN = max(maxN, numb)
         minN = min(minN, numb)
         return
@@ -10,14 +10,18 @@ def dfs(idx, numb):
     for i in range(4):
         if operator[i]:
             operator[i] -= 1
+            # 덧셈
             if i == 0:
-                dfs(idx+1, numb+numbers[idx+1])
+                dfs(idx+1, numb+numbers[idx])
+            # 뺏셈
             elif i == 1:
-                dfs(idx+1, numb-numbers[idx+1])
+                dfs(idx+1, numb-numbers[idx])
+            # 곱셈
             elif i == 2:
-                dfs(idx+1, numb*numbers[idx+1])
+                dfs(idx+1, numb*numbers[idx])
+            # 나눗셈
             else:
-                dfs(idx+1, numb//numbers[idx+1])
+                dfs(idx+1, int(numb/numbers[idx]))
             operator[i] += 1
 
 # 수의 개수 N
@@ -30,6 +34,8 @@ operator = list(map(int, sys.stdin.readline().split()))
 # 최댓값 maxN, 최솟값 minN
 maxN, minN = -1e9, 1e9
 
-dfs(0, numbers[0])
+dfs(1, numbers[0])
+
+# 최댓값과 최솟값 출력
 print(maxN)
 print(minN)
