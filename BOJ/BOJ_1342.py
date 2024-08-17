8e7d788fcf20ebf4422b3be0265655ef903cb016
sc-choi2022 <c.martin20222023@gmail.com>
2024-08-17 11:59:45 +0900

    0817

---
+++ BOJ/BOJ_1342.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+import sys
+
+def dfs(pre, cnt):
+    global answer
+    # 행운의 문자열이 완성된 경우
+    if cnt == len(S):
+        answer += 1
+        return
+    for key, value in number.items():
+        # 행운의 문자열에 부합하지 않는 경우 continue
+        if key == pre or value == 0:
+            continue
+        number[key] -= 1
+        dfs(key, cnt+1)
+        number[key] += 1
+
+# 문자열 S
+S = sys.stdin.readline().rstrip()
+# 문자열의 각 문자의 개수를 저장하는 딕셔너리 number
+number = defaultdict(int)
+for s in S:
+    number[s] += 1
+
+# 서로 다른 행운의 문자열의 개수
+answer = 0
+for k, v in number.items():
+    number[k] -= 1
+    dfs(k, 1)
+    number[k] += 1
+
+# answer 출력
+print(answer)
