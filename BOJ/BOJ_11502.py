5ee284a049854813681082caabf36d780a64c02d
sc-choi2022 <c.martin20222023@gmail.com>
2024-08-23 23:27:44 +0900

    0823

---
+++ BOJ/BOJ_11502.py
@@ -0,0 +1,16 @@
+import sys
+
+def prime():
+    return
+
+def solve(x, lst):
+    return 
+
+# 테스트케이스 수 T
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    flag = False
+    # 정수 K
+    K = int(sys.stdin.readline())
+    solve(K, [])
