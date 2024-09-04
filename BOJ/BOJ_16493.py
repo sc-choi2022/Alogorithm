9810ef8e662878d82b957f13a454285faf63beba
sc-choi2022 <c.martin20222023@gmail.com>
2024-09-04 22:59:56 +0900

    0904

---
+++ BOJ/BOJ_16493.py
@@ -0,0 +1,19 @@
+import sys
+
+# 남은 기간 N과 챕터의 수 M
+N, M = map(int, sys.stdin.readline().split())
+# 책의 챕터당 읽는데 소요되는 일 수와 페이지 수를 저장하는 배열 chapters
+
+chapters = [(0, 0)] + [tuple(map(int, sys.stdin.readline().split())) for _ in range(M)]
+chapters.sort()
+dp = [[0]*(N+1) for _ in range(M+1)]
+
+for i in range(1, M+1):
+    for j in range(1, N+1):
+        if j >= chapters[i][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-chapters[i][0]] + chapters[i][1])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+# 읽을 수 있는 최대 페이지 수를 출력
+print(dp[M][N])
