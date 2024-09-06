96deea24d8dffce37683ec656f7cd4450e9f9f58
sc-choi2022 <c.martin20222023@gmail.com>
2024-09-06 13:23:52 +0900

    0906

--- BOJ/BOJ_11123.py
+++ BOJ/BOJ_11123.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 
 # 테스트 케이스의 수 T
@@ -7,3 +8,20 @@ for _ in range(T):
     # 그리드의 높이 H, 너비 W
     H, W = map(int, sys.stdin.readline().split())
     grid = [list(sys.stdin.readline().rstrip()) for _ in range(H)]
+    visit = [[0] * W for _ in range(H)]
+    answer = 0
+    for i in range(H):
+        for j in range(W):
+            if grid[i][j] == '#' and not visit[i][j]:
+                answer += 1
+                queue = deque([(i, j)])
+                while queue:
+                    ci, cj = queue.popleft()
+
+                    for di, dj in (0, 1), (1, 0), (0, -1), (-1, 0):
+                        ni, nj = ci + di, cj + dj
+                        if 0 <= ni < H and 0 <= nj < W and grid[ni][nj] == '#' and not visit[ni][nj]:
+                            queue.append((ni, nj))
+                            visit[ni][nj] = 1
+    # 양 무리의 개수를 출력
+    print(answer)
