aec7d378c5e00c95d3ed72d90a3c2655eaed535a
sc-choi2022 <c.martin20222023@gmail.com>
2024-09-19 23:53:41 +0900

    0919

--- BOJ/BOJ_2436.py
+++ BOJ/BOJ_2436.py
@@ -3,14 +3,12 @@ import sys
 # 서로소를 찾는 함수 find
 def find(a, b):
     while b > 0:
-        print(a, b)
         a, b = b, a%b
-    print(f'return {a}')
     return a
 
 # 최대공약수와 최소공배수로 하는 두 개의 자연수 A, B(A<B)
 A, B = map(int, sys.stdin.readline().split())
-# B // A를 저장하는 변수 C
+# B // A를 저장하는 변수 C서로소의 곱이 되는 값)
 C = B // A
 
 for i in range(int(C**0.5), 0, -1):
