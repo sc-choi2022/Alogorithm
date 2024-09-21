02ae265576bbab92b50050843ce025ef3c9584c9
sc-choi2022 <c.martin20222023@gmail.com>
2024-09-21 23:38:39 +0900

    0921

--- BOJ/BOJ_16206.py
+++ BOJ/BOJ_16206.py
@@ -3,7 +3,9 @@ import sys
 # 롤케이크의 개수 N과 자를 수 있는 최대 횟수 M
 N, M = map(int, sys.stdin.readline().split())
 # 롤케이크의 길이를 저장하는 배열 A
-A = list(sorted(map(int, sys.stdin.readline().split()), key=lambda x : (x % 10, x)))
+A = list(sorted(map(int, sys.stdin.readline().split()), key=lambda x: (x % 10, x)))
+
+# 만들 수 있는 길이가 10인 롤케이크 개수의 최댓값 cnt
 cnt = 0
 for a in A:
     tmp = a//10
@@ -21,4 +23,5 @@ for a in A:
         else:
             cnt += M
             M = 0
+# cnt 출력
 print(cnt)
