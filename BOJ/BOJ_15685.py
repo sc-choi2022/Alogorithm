19e4ed89c147d1906d7320c0143556ca9d2a3c65
sc-choi2022 <c.martin20222023@gmail.com>
2024-09-24 23:46:09 +0900

    0924

--- BOJ/BOJ_15685.py
+++ BOJ/BOJ_15685.py
@@ -1,8 +1,13 @@
 import sys
 
+def curve(ci, cj):
+    return 
+
 # 드래곤 커브의 개수 N
 N = int(sys.stdin.readline())
 direct = {0: (0, 1), 1: (-1, 0), 2: (0, -1), 3: (1, 0)}
+# 드래곤 커브를 위해 사용할 배열 dragon
+dragon = [[0]*101 for _ in range(101)]
 
 for _ in range(N):
     # 드래곤 커브의 시작 점 (x, y), 시작 방향 d, 세대 g
