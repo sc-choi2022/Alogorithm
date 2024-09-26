4da54acc4e3ed1d2290908bd293fd5cdaff668e3
sc-choi2022 <c.martin20222023@gmail.com>
2024-09-26 23:25:13 +0900

    0926

---
+++ BOJ/BOJ_16235.py
@@ -0,0 +1,6 @@
+import sys
+
+# 땅의 크기 N, 나무의 그루수 M, 지나는 해 K년
+N, M, K = map(int, sys.stdin.readline().split())
+# 나무의 정보를 저장하는 배열 trees
+trees = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
