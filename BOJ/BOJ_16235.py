c945b43cb0b2897be4aa5139e75cdd5014396877
sc-choi2022 <c.martin20222023@gmail.com>
2024-09-28 23:55:13 +0900

    0928

--- BOJ/BOJ_16235.py
+++ BOJ/BOJ_16235.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import sys
 
 def func():
@@ -5,10 +6,16 @@ def func():
 
 # 땅의 크기 N, 나무의 그루수 M, 지나는 해 K년
 N, M, K = map(int, sys.stdin.readline().split())
-# 나무의 정보를 저장하는 배열 trees
-trees = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+# 겨울에 추가 되는 양분의 정보를 저장하는 배열 A
+A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+# 땅의 양분을 저장하는 배열 land
+land = [[5]*N for _ in range(N)]
+# 묘목의 정보를 저장하는 딕셔너리 tree
+tree = defaultdict(list)
 
-for _ in range(K):
-    func()
+for _ in range(M):
+    # 나무의 위치 x, y, 나무의 나이 z
+    x, y, z = map(int, sys.stdin.readline().split())
+    tree[(x, y)].append(z)
 
-print(answer)
+print(tree)
