158e301181e0958872b5e35029e37c66c3c1a6f7
sc-choi2022 <c.martin20222023@gmail.com>
2024-10-04 23:32:49 +0900

    1004

---
+++ BOJ/BOJ_30804.py
@@ -0,0 +1,10 @@
+import sys
+
+# 과일의 개수 N
+N = int(sys.stdin.readline())
+# 과일을 저장하는 배열 fruits
+fruits = [0] * N
+
+for i in range(N):
+    number = int(sys.stdin.readline())
+    function[i] = number
