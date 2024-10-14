a0cf0238824f38157b94847d20afebcec4b84db2
sc-choi2022 <c.martin20222023@gmail.com>
2024-10-14 23:46:19 +0900

    1014

--- BOJ/BOJ_12849.py
+++ BOJ/BOJ_12849.py
@@ -1,4 +1,14 @@
 import sys
 
+# 산책하는 시간 D분
 D = int(sys.stdin.readline())
+# 배열 dp
 dp = [0] * 8
+dp[0] = 1
+
+for _ in range(D):
+    tmp = [0] * 8
+    tmp[0] = (dp[1] + dp[2])%1000000007
+    tmp[1] = (dp[0] + dp[2] + dp[3])%1000000007
+    tmp[2] = (dp[0] + dp[1] + dp[3] + dp[4])%1000000007
+    tmp[3] = (dp[1] + dp[2] + dp[4] + dp[5])%1000000007
