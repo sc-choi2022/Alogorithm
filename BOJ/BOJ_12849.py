f2ee9cb10506c48b5a8f5dc3ded4e0dd3f8d6d8e
sc-choi2022 <c.martin20222023@gmail.com>
2024-10-15 18:05:54 +0900

    1015

--- BOJ/BOJ_12849.py
+++ BOJ/BOJ_12849.py
@@ -5,10 +5,21 @@ D = int(sys.stdin.readline())
 # 배열 dp
 dp = [0] * 8
 dp[0] = 1
+# 나머지 출력을 위한 정수 N
+N = 1000000007
 
 for _ in range(D):
     tmp = [0] * 8
-    tmp[0] = (dp[1] + dp[2])%1000000007
-    tmp[1] = (dp[0] + dp[2] + dp[3])%1000000007
-    tmp[2] = (dp[0] + dp[1] + dp[3] + dp[4])%1000000007
-    tmp[3] = (dp[1] + dp[2] + dp[4] + dp[5])%1000000007
+    tmp[0] = (dp[1] + dp[2]) % N
+    tmp[1] = (dp[0] + dp[2] + dp[3]) % N
+    tmp[2] = (dp[0] + dp[1] + dp[3] + dp[4]) % N
+    tmp[3] = (dp[1] + dp[2] + dp[4] + dp[5]) % N
+    tmp[4] = (dp[2] + dp[3] + dp[5] + dp[6]) % N
+    tmp[5] = (dp[3] + dp[4] + dp[7]) % N
+    tmp[6] = (dp[4] + dp[7]) % N
+    tmp[7] = (dp[5] + dp[6]) % N
+
+    dp = tmp
+    
+# 가능한 경로의 수 출력
+print(dp[0])
