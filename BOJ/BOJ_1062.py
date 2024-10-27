f0e35c0e41de724c369531f3daaa13f7d3c4af72
sc-choi2022 <c.martin20222023@gmail.com>
2024-10-27 23:44:52 +0900

    1027

--- BOJ/BOJ_1062.py
+++ BOJ/BOJ_1062.py
@@ -1,5 +1,14 @@
 import sys
 
+def dfs(idx, cnt):
+    global answer
+
+    if cnt == K-5:
+        tmp = 0
+
+        answer = max(answer, tmp)
+        return 
+
 # 단어의 개수 N, 가르칠 수 있는 글자의 개수 K
 N, K = map(int, sys.stdin.readline().split())
 # 단어를 이루는 알파벳을 저장하는 배열 words
@@ -17,4 +26,5 @@ words.sort(key=lambda x: len(x))
 if K < 5:
     print(0)
 else:
+    answer = 0
     dfs()
