5c4fa957530679d16bc842bb25248324291b5c16
sc-choi2022 <c.martin20222023@gmail.com>
2024-10-29 21:03:14 +0900

    1029

--- BOJ/BOJ_16987.py
+++ BOJ/BOJ_16987.py
@@ -1,41 +1,40 @@
 import sys
 
-def solve(current):
-    if current == N:
-        cnt = 0
-        for durability, weight in eggs:
-            if durability <= 0:
-                cnt += 1
-        return cnt
+def hit(idx, cnt):
+    global answer
 
-    if eggs[current][0] <= 0:
-        return solve(current+1)
-
-    for i in range(N):
-        if i == current:
-            continue
-        if eggs[i][0] > 0:
-            break
+    if idx == N or cnt == N-1:
+        answer = max(answer, cnt)
+        return
+    # idx번째 계란이 이미 깨진 경우
+    if S[idx] <= 0:
+        hit(idx+1, cnt)
     else:
-        return solve(current+1)
+        for e in range(N):
+            if e == idx:
+                continue
+            # e번째 계란이 깨지지 않은 계란인 경우
+            if S[e] > 0:
+                S[e], S[idx] = S[e]-W[idx], S[idx]-W[e]
+                if S[e] <= 0 and S[idx] <= 0:
+                    hit(idx+1, cnt+2)
+                elif S[e] <= 0 or S[idx] <= 0:
+                    hit(idx+1, cnt+1)
+                else:
+                    hit(idx+1, cnt)
+                S[e], S[idx] = S[e]+W[idx], S[idx]+W[e]
 
-    answer = 0
-    for j in range(N):
-        if j == current or eggs[i][0] <= 0:
-            continue
-
-        eggs[j][0] -= eggs[current][1]
-        eggs[current][0] -= eggs[j][1]
-
-        answer = max(answer, solve(current+1))
-
-        eggs[j][0] += eggs[current][1]
-        eggs[current][0] += eggs[j][1]
-    return answer
-
-# 계란의 개수 N
+# 계란의 수 N
 N = int(sys.stdin.readline())
-# 계란의 내구도와 무게에 대한 정보를 담을 배열 eggs
-eggs = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+# 계란의 내구성을 저장하는 배열 S, 계란의 무게를 저장하는 배열 W
+S, W = [0] * N, [0] * N
 
-print(solve(0))
+for i in range(N):
+    S[i], W[i] = map(int, sys.stdin.readline().split())
+
+# 깰 수 있는 계란의 최대 개수 answer
+answer = 0
+hit(0, 0)
+
+# answer을 출력
+print(answer)
