50383327b40bb994547ae6f705e3cfd0f1f59a8f
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-01 20:10:28 +0900

    1101

--- BOJ/BOJ_1967.py
+++ BOJ/BOJ_1967.py
@@ -1,5 +1,4 @@
 # dfs
-
 import sys
 sys.setrecursionlimit(10**5)
 
@@ -34,3 +33,44 @@ dfs(goal, 0)
 
 # 트리의 지름을 출력
 print(max(visit))
+
+# bfs
+from collections import deque
+import sys
+
+def bfs(start):
+    global answer
+    # 배열 visit 초기화
+    visit = [-1] * (N+1)
+    visit[start] = 0
+    queue = deque([start])
+
+    while queue:
+        current = queue.popleft()
+
+        for next, d in graph[current]:
+            if visit[next] == -1:
+                visit[next] = visit[current] + d
+                queue.append(next)
+    # 시작 노드에서 최대길이를 갱신
+    answer = max(answer, max(visit))
+
+    return visit.index(answer)
+
+# 노드의 개수 N
+N = int(sys.stdin.readline())
+graph = [[] for _ in range(N+1)]
+visit = [-1] * (N+1)
+
+for _ in range(N-1):
+    # 부모 노드 번호 A, 자식 노드 번호 B, 간선의 가중치 M
+    A, B, M = map(int, sys.stdin.readline().split())
+    graph[A].append((B, M))
+    graph[B].append((A, M))
+
+# 트리의 지름 answer
+answer = 0
+# 루트노드에서의 최대 길이인 노드를 구하고 그 노드를 시작으로 트리의 지름을 구한다.
+bfs(bfs(1))
+# answer 출력
+print(answer)
