ecc0515f197c79718555973804892a5b64d17ff9
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-02 23:41:22 +0900

    1102

--- BOJ/BOJ_2229.py
+++ BOJ/BOJ_2229.py
@@ -4,9 +4,10 @@ import sys
 N = int(sys.stdin.readline())
 # 점수를 저장하는 배열 scores
 scores = list(map(int, sys.stdin.readline().split()))
-dp = [0] * (N+1)
+dp = [0] * N
 
-for i in range(N):
-    continue
+for i in range(1, N):
+    for j in range(1, i+2):
+        continue
 
 print(dp[-1])
