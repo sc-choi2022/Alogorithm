3373d4e9e25f56142140d74257b386a8479a9723
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-06 23:32:54 +0900

    1106

--- BOJ/BOJ_1068.py
+++ BOJ/BOJ_1068.py
@@ -1,14 +1,10 @@
 import sys
 
+def dfs(idx):
+    return
 # 트리의 노드의 개수 N
 N = int(sys.stdin.readline())
 # 부모노드를 저장한 배열 parents
 parents = list(map(int, sys.stdin.readline().split()))
-graph = [[] for _ in range(N)]
-
-for i in range(N):
-    if parents[i] == -1:
-        continue
-    graph[parents[i]].append(i)
 # 지우는 노드의 번호 num
 num = int(sys.stdin.readline())
