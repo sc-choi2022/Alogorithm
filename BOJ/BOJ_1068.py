2ad4434f156a03f0b7b0c9ae7eac5f9099e8cd7e
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-07 23:46:00 +0900

    1107

--- BOJ/BOJ_1068.py
+++ BOJ/BOJ_1068.py
@@ -1,10 +1,23 @@
 import sys
 
 def dfs(idx):
-    return
+    parents[idx] = -2
+
+    for n in range(N):
+        if idx == parents[n]:
+            dfs(n)
+
 # 트리의 노드의 개수 N
 N = int(sys.stdin.readline())
 # 부모노드를 저장한 배열 parents
 parents = list(map(int, sys.stdin.readline().split()))
 # 지우는 노드의 번호 num
 num = int(sys.stdin.readline())
+
+dfs(num)
+answer = 0
+for i in range(N):
+    if parents[i] != -2 and i not in parents:
+        answer += 1
+
+print(answer)
