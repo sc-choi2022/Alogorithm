7721c0a7c63eeae14dffded81f8032819531eb4d
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-09 22:39:10 +0900

    1109

--- BOJ/BOJ_1062.py
+++ BOJ/BOJ_1062.py
@@ -3,28 +3,41 @@ import sys
 def dfs(idx, cnt):
     global answer
 
-    if cnt == K-5:
-        tmp = 0
+    # K의 문자를 가르친 경우
+    if cnt == K:
+        reading = 0
+        for word in words:
+            for w in word:
+                if not visit[ord(w)-97]:
+                    break
+            else:
+                reading += 1
+        answer = max(answer, reading)
+        return
 
-        answer = max(answer, tmp)
-        return 
+    for i in range(idx, 26):
+        if not visit[i]:
+            visit[i] = 1
+            dfs(i+1, cnt+1)
+            visit[i] = 0
 
 # 단어의 개수 N, 가르칠 수 있는 글자의 개수 K
 N, K = map(int, sys.stdin.readline().split())
 # 단어를 이루는 알파벳을 저장하는 배열 words
-words = []
+words = [set(list(sys.stdin.readline().rstrip())) for _ in range(N)]
 
-for _ in range(N):
-    word = set(list(sys.stdin.readline().rstrip()[4:-4]))
-
-    for w in ('a', 'c', 'i', 'n', 't'):
-        if w in word:
-            word.remove(w)
-    words.append(word)
-words.sort(key=lambda x: len(x))
-
+# 남극언어를 읽을 수 없는 경우
 if K < 5:
     print(0)
+# 남극언어를 읽을 수 있는 경우
 else:
+    # 배운 글자를 확인하는 배열 visit
+    # 단어의 기본 글자를 1로 초기화
+    visit = [0] * 26
+    visit[0], visit[2], visit[8], visit[13], visit[19] = 1, 1, 1, 1, 1
+
+    # 읽을 수 있는 단어의 최대개수 answer
     answer = 0
-    dfs()
+    dfs(0, 5)
+    # answer 출력
+    print(answer)
--- BOJ/BOJ_9205.py
+++ BOJ/BOJ_9205.py
@@ -1,4 +1,28 @@
+from collections import deque
 import sys
+
+def bfs():
+    global answer
+
+    # 시작 위치와 맥주의 개수로 시작
+    queue = deque([(hi, hj)])
+    # 편의점의 방문여부를 저장하는 배열 visit
+    visit = [0] * N
+
+    while queue:
+        # 현재 위치와 맥주의 개수
+        ci, cj = queue.popleft()
+        # 락 페스티벌에 도착가능한 경우 answer 갱신 후 return
+        if (abs(ci-ri) + abs(cj-rj)) / 50 <= 20:
+            answer = 'happy'
+            return
+
+        for i in range(N):
+            ni, nj = CVS[i]
+            # 미방문 편의점이고 맥주양으로 도착가능한 경우
+            if not visit[i] and (abs(ci-ni) + abs(cj-nj)) / 50 <= 20:
+                visit[i] = 1
+                queue.append((ni, nj))
 
 # 테스트 케이스 개수 T
 T = int(sys.stdin.readline())
@@ -6,7 +30,14 @@ T = int(sys.stdin.readline())
 for _ in range(T):
     # 편의점의 개수 N
     N = int(sys.stdin.readline())
+    # 집의 좌표
+    hi, hj = map(int, sys.stdin.readline().split())
+    # 편의점의 좌표를 저장하는 배열 CVS
+    CVS = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
     # 락 페스티벌 좌표
-    x, y = map(int, sys.stdin.readline().split())
-    CVS = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-    
+    ri, rj = map(int, sys.stdin.readline().split())
+    answer = 'sad'
+    bfs()
+    # 행복하게 페스티벌에 갈 수 있으면 happy 출력
+    # 중간에 맥주가 바닥나서 더 이동할 수 없으면 sad 출력
+    print(answer)
