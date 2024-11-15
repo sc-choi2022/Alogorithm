9d4d4f4ee6b79079bb2847e175b03a2f98d9a46f
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-15 23:50:38 +0900

    1115

--- BOJ/BOJ_15684.py
+++ BOJ/BOJ_15684.py
@@ -3,3 +3,7 @@ import sys
 # 세로선의 개수 N, 가로선의 개수 M, 세로선마다 가로선을 놓을 수 있는 위치 개수 H
 N, M, H = map(int, sys.stdin.readline().split())
 lader = [[0]*N for _ in range(H)]
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    lader[a-1][b-1] = 1
