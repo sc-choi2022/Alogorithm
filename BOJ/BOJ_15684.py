524da3c21d475ada83639d4714aaa8eb2d14ca15
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-16 23:34:42 +0900

    1116

--- BOJ/BOJ_15684.py
+++ BOJ/BOJ_15684.py
@@ -1,5 +1,11 @@
 import sys
 
+def plus():
+    return
+
+def play():
+    return
+
 # 세로선의 개수 N, 가로선의 개수 M, 세로선마다 가로선을 놓을 수 있는 위치 개수 H
 N, M, H = map(int, sys.stdin.readline().split())
 lader = [[0]*N for _ in range(H)]
@@ -7,3 +13,10 @@ lader = [[0]*N for _ in range(H)]
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
     lader[a-1][b-1] = 1
+
+answer = M+1
+
+if answer > 3 or answer == M+1:
+    print(-1)
+else:
+    print(answer)
