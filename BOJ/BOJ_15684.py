26bbfb16978c97dc5861fbddabff6046567358fa
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-18 23:52:58 +0900

    1118

--- BOJ/BOJ_15684.py
+++ BOJ/BOJ_15684.py
@@ -1,27 +1,36 @@
+from collections import deque
 import sys
 
 def plus(num):
+    global answer
     for pi in range(M):
         ci, cj = lader[pi]
         garo[ci][cj] = 1
         used[pi] = 1
         if sum(used) == num:
-            play()
-        
-
+            if play():
+                return num
+        plus(num)
+        used[pi] = 0
 
 def play():
-    global flag
-
-    for ii in range(H):
-        for jj in range(N):
-            continue
+    for jj in range(N):
+        now = jj
+        for ii in range(H):
+            if garo[ii][jj]:
+                now += 1
+            elif now > 0 and garo[ii][now-1]:
+                now -= 1
+        if now != jj:
+            return False
+    return True
 
 # 세로선의 개수 N, 가로선의 개수 M, 세로선마다 가로선을 놓을 수 있는 위치 개수 H
 N, M, H = map(int, sys.stdin.readline().split())
 lader = [(0, 0) for _ in range(M)]
 used = [0] * M
 garo = [[0]*N for _ in range(H)]
+answer = 0
 
 for l in range(M):
     a, b = map(int, sys.stdin.readline().split())
