8e8d87043ea787549696151bec42cd66381d1d8f
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-19 23:40:02 +0900

    1119

--- BOJ/BOJ_15684.py
+++ BOJ/BOJ_15684.py
@@ -1,23 +1,32 @@
-from collections import deque
 import sys
 
-def plus(num):
+def plus(cnt, x, y):
     global answer
-    for pi in range(M):
-        ci, cj = lader[pi]
-        garo[ci][cj] = 1
-        used[pi] = 1
-        if sum(used) == num:
-            if play():
-                return num
-        plus(num)
-        used[pi] = 0
+
+    if answer <= cnt:
+        return
+    elif play():
+        answer = min(answer, cnt)
+        return
+    elif cnt == 3:
+        return
+
+    for pi in range(x, H):
+        if pi == x:
+            k = y
+        else:
+            k = 0
+        for pj in range(k, N-1):
+            if not garo[pi][pj]:
+                garo[pi][pj] = 1
+                plus(cnt+1, pi, pj+2)
+                garo[pi][pj] = 0
 
 def play():
     for jj in range(N):
         now = jj
         for ii in range(H):
-            if garo[ii][jj]:
+            if garo[ii][now]:
                 now += 1
             elif now > 0 and garo[ii][now-1]:
                 now -= 1
@@ -27,14 +36,12 @@ def play():
 
 # 세로선의 개수 N, 가로선의 개수 M, 세로선마다 가로선을 놓을 수 있는 위치 개수 H
 N, M, H = map(int, sys.stdin.readline().split())
-lader = [(0, 0) for _ in range(M)]
-used = [0] * M
 garo = [[0]*N for _ in range(H)]
-answer = 0
+answer = 4
 
 for l in range(M):
     a, b = map(int, sys.stdin.readline().split())
-    lader[l] = (a-1, b-1)
+    garo[a-1][b-1] = 1
 
-for num in range(1, 4):
-    plus(num)
+plus(0, 0, 0)
+print(answer if answer <= 3 else -1)
