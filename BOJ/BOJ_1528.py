2f0c4eafda2caab7f7676ac03e7b517eaaeea9cc
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-22 23:38:42 +0900

    1122

--- BOJ/BOJ_1528.py
+++ BOJ/BOJ_1528.py
@@ -1,6 +1,12 @@
 from collections import deque
 import sys
 
+def trace(k, visited):
+    if k == 0:
+        return
+    trace(visited[k], visited)
+    print(k-visited[k], end=' ')
+
 # 금민수의 합으로 나타내야하는 정수 N
 N = int(sys.stdin.readline())
 gold = []
@@ -20,4 +26,23 @@ for i in range(2, 200):
         else:
             s += '7'
     gold.append(int(s))
-print(gold)
+
+visit = [-1] * 1000001
+queue = deque([0])
+
+while queue:
+    now = queue.popleft()
+    if now == N:
+        break
+    for ii in range(len(gold)):
+        next = now + gold[ii]
+        if next > N:
+            continue
+        if visit[next] == -1:
+            visit[next] = now
+            queue.append(next)
+
+if visit[N] == -1:
+    print(-1)
+else:
+    trace(N, visit)
