344349fc3121ea1fc0923f1607314127f46d36be
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-25 23:37:03 +0900

    1125

--- BOJ/BOJ_3107.py
+++ BOJ/BOJ_3107.py
@@ -1,19 +1,26 @@
 import sys
 
+def restore(lst):
+    L = len(lst)
+
+    for i in range(L):
+        lst[i] = '0'*(4-len(lst[i]))+lst[i]
+
 # IPv6주소 IPv6
-IPv6 = sys.stdin.readline().rstrip().split(':')
+IPv6 = sys.stdin.readline().rstrip().split('::')
+# 원래 IPv6 주소를 저장하는 변수 result
+result = ''
 
-result = ['0000'] * 8
-flag = False
-for i in range(8):
-    if IPv6[i] == '' and IPv6[i+1] == '':
-        flag = True
-        break
-    result[i] = '0'*(4-len(IPv6[i])) + IPv6[i]
-if flag:
-    for j in range(1, 8):
-        if IPv6[-j] == '' and IPv6[-j-1] == '':
-            break
-        result[-j] = '0'*(4-len(IPv6[-j])) + IPv6[-j]
-
-print(':'.join(result))
+# 2번째 규칙이 적용된 경우
+if len(IPv6) == 2:
+    pre, post = IPv6[0].split(':'), IPv6[1].split(':')
+    restore(pre)
+    restore(post)
+    result = ':'.join(pre + ['0000']*(8-len(pre)-len(post)) + post)
+# 1번째 규칙만 적용된 경우
+else:
+    IPv6 = IPv6[0].split(':')
+    restore(IPv6)
+    result = ':'.join(IPv6)
+# result를 출력
+print(result)
