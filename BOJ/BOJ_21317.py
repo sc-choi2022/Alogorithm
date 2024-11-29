1e21dc0687387d7dba7430e010a34b2d63a02b1e
sc-choi2022 <c.martin20222023@gmail.com>
2024-11-29 23:46:24 +0900

    1129

--- BOJ/BOJ_21317.py
+++ BOJ/BOJ_21317.py
@@ -13,13 +13,4 @@ queue = deque([(0, 0, 0)])
 while queue:
     current, energy, flag = queue.popleft()
 
-    for i in range(1, 4):
-        if current + i < N:
-            if not dp[current+i]:
-                if i == 3 and not flag and energy+K < dp[current+i]:
-                    dp[current+i] = energy+K
-                    queue.append((current+i, energy+K, True))
-                elif not dp[current+i] or energy+stones[current][i-1] < dp[current+i]:
-                    dp[current + i] = energy + stones[current][i-1]
-                    queue.append((current+i, energy+stones[current][i-1], flag))
 print(dp[N-1])
