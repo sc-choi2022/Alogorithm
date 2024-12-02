9cdaa66ee815b05c391f6a00234a017b891193cc
sc-choi2022 <c.martin20222023@gmail.com>
2024-12-02 23:50:35 +0900

    1202

--- BOJ/BOJ_15992.py
+++ BOJ/BOJ_15992.py
@@ -6,6 +6,13 @@ T = int(sys.stdin.readline())
 dp = [[0]*1001 for _ in range(1001)]
 dp[1][1], dp[2][1], dp[3][1] = 1, 1, 1
 
+for i in range(2, 1001):
+    for j in range(1, 1001):
+        for k in range(1, 4):
+            if j-k > 0:
+                dp[j][i] = (dp[j][i]+dp[j-k][i-1])%1000000009
+
 for _ in range(T):
     # 정수 N, M
     N, M = map(int, sys.stdin.readline().split())
+    print(dp[N][M])
