ebadfc81cdb5eedc5e443b53da0835a8594465fc
sc-choi2022 <c.martin20222023@gmail.com>
2024-12-08 23:51:10 +0900

    1208

--- BOJ/BOJ_1229.py
+++ BOJ/BOJ_1229.py
@@ -1,27 +1,15 @@
 import sys
 
-answers = [0, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 2] + [6]*1000000
-six = [0]*1000000
-def makesix(N):
-    i = 1
-    for i in range(1000000)
-        tmp = (i-1)*6 + six[i-1] - (2*(i-2)+1)
-        if tmp > N:
-            return six
-        six[i] = tmp
-
+# 정수 N
 N = int(sys.stdin.readline())
-if N < 13:
-    print(answers[N])
-    exit()
+dp = [0, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 2]
+answer = 0
 
-six = makesix(N)
+for i in range(2, 7072):
+    dp[i] = dp[i-1] + 6*(i-1) - (2*(i-2)+1)
 
-for i in range(13, N + 1):
-    ans = 6
-    for k in six:
-        if k > i:
-            break
-        ans = min(ans, answers[i-k])
-    answers[i] = ans + 1
-print(answers[N])
+for j in range(7071, -1, -1):
+    if N > dp[j]:
+
+
+print(dp[-1])
