45803ba462a5fe65a2352379e9e18e8530d6d7e6
sc-choi2022 <c.martin20222023@gmail.com>
2024-12-09 23:51:26 +0900

    1209

--- BOJ/BOJ_1229.py
+++ BOJ/BOJ_1229.py
@@ -2,14 +2,9 @@ import sys
 
 # 정수 N
 N = int(sys.stdin.readline())
-dp = [0, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 2]
-answer = 0
+dp = [0] * (N+1)
 
 for i in range(2, 7072):
     dp[i] = dp[i-1] + 6*(i-1) - (2*(i-2)+1)
 
-for j in range(7071, -1, -1):
-    if N > dp[j]:
-
-
-print(dp[-1])
+print(dp[N])
