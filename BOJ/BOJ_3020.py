4734813fca84af4514aad252425227271942ec75
sc-choi2022 <c.martin20222023@gmail.com>
2024-12-15 23:40:03 +0900

    1215

--- BOJ/BOJ_3020.py
+++ BOJ/BOJ_3020.py
@@ -6,3 +6,13 @@ N, H = map(int, sys.stdin.readline().split())
 cave = [int(sys.stdin.readline()) for _ in range(N)]
 # 동굴의 높이에 따른 석순과 종유석의 정보를 담을 배열 line
 line = [0] * H
+
+for i in range(N):
+    length = int(sys.stdin.readline())
+
+    if i%2:
+        line[0] += 1
+        line[length] -= 1
+    else:
+        line[H-length] -= 1
+        
