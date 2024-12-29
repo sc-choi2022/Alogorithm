451ebbe3e4eefbc6ba31d616138c1cb095a3008e
sc-choi2022 <c.martin20222023@gmail.com>
2024-12-29 22:50:29 +0900

    1229

--- BOJ/BOJ_1229.py
+++ BOJ/BOJ_1229.py
@@ -19,3 +19,7 @@ for i in range(1, N+1):
     for j in range(1, len(numbers)):
         if numbers[j] > i:
             break
+        tmp = min(tmp, dp[i-numbers[j]]+1)
+    dp.append(tmp)
+
+print(dp[-1])
