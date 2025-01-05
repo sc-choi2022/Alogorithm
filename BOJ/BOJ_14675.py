7c44d714c04e3532706dd5a2f16679259c2ea51f
sc-choi2022 <c.martin20222023@gmail.com>
2025-01-05 19:16:33 +0900

    0105

--- BOJ/BOJ_14675.py
+++ BOJ/BOJ_14675.py
@@ -2,9 +2,14 @@ import sys
 
 # 정점의 개수 N
 N = int(sys.stdin.readline())
-# 간선의 정보를 저장하는 배열 edges
-edges = [(0, 0)] + [tuple(map(int, sys.stdin.readline().split())) for _ in range(N-1)]
-tree = [[] for _ in range(N+1)]
+# 트리에 속한 정점의 연결 개수를 저장하는 배열 tree
+tree = [0] * (N+1)
+
+for _ in range(N-1):
+    # 간선의 정보 a, b
+    a, b = map(int, sys.stdin.readline().split())
+    tree[a] += 1
+    tree[b] += 1
 
 # 질의의 개수 Q
 Q = int(sys.stdin.readline())
@@ -14,6 +19,12 @@ for _ in range(Q):
     t, k = map(int, sys.stdin.readline().split())
 
     if t == 1:
-        continue
+        # 단절점인 경우
+        if tree[k] > 1:
+            print('yes')
+        # 단절점이 아닌 경우
+        else:
+            print('no')
     else:
-        continue
+        # 반드시 단절선이므로 yes 출력
+        print('yes')
