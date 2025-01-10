f545db0c36edfdde06301b63756b0254c46e2f42
sc-choi2022 <c.martin20222023@gmail.com>
2025-01-10 18:31:23 +0900

    0110

--- BOJ/BOJ_1107.py
+++ BOJ/BOJ_1107.py
@@ -1,8 +1,20 @@
 import sys
 
 # 이동하려고 하는 채널 N
-N = sys.stdin.readline().rstrip()
+N = int(sys.stdin.readline().rstrip())
 # 고장난 버튼의 개수 M
 M = int(sys.stdin.readline())
-# 이동하려는 채녈의 개수 L
-L = len(N)
+broken = list(map(int, sys.stdin.readline().split()))
+
+answer = abs(100-N)
+
+for i in range(1000001):
+    i = str(i)
+
+    for j in range(len(i)):
+        if int(i[j]) in broken:
+            break
+        elif j == len(i)-1:
+            answer = min(answer, abs(int(i)-N)+len(i))
+
+print(answer)
