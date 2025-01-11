0f61dd787df99727ceebf22f1c932336fdf60054
sc-choi2022 <c.martin20222023@gmail.com>
2025-01-11 22:34:34 +0900

    0111

--- BOJ/BOJ_1107.py
+++ BOJ/BOJ_1107.py
@@ -1,20 +1,26 @@
 import sys
 
+def howmany():
+    global answer
+    for number in range(1000001):
+        number = str(number)
+
+        if abs(int(number)-N)+len(number) > answer:
+            continue
+
+        for i in range(len(number)):
+            if int(number[i]) in broken:
+                break
+        else:
+            answer = min(answer, abs(int(number)-N)+len(number))
+
 # 이동하려고 하는 채널 N
 N = int(sys.stdin.readline().rstrip())
 # 고장난 버튼의 개수 M
 M = int(sys.stdin.readline())
+# 고장난 버튼를 저장하는 배열 broken
 broken = list(map(int, sys.stdin.readline().split()))
 
 answer = abs(100-N)
-
-for i in range(1000001):
-    i = str(i)
-
-    for j in range(len(i)):
-        if int(i[j]) in broken:
-            break
-        elif j == len(i)-1:
-            answer = min(answer, abs(int(i)-N)+len(i))
-
+howmany()
 print(answer)
