6f53a38a0053ed557b85ff7eacb8d18d617a62a8
sc-choi2022 <c.martin20222023@gmail.com>
2025-01-17 18:26:25 +0900

    0117

--- BOJ/BOJ_10830.py
+++ BOJ/BOJ_10830.py
@@ -1,27 +1,25 @@
 import sys
 
-def power():
-    T = [[0]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            T[i][j] = fill(i, j)
-    return T
+def square(A, B):
+    if B == 1:
+        for si in range(N):
+            for sj in range(N):
+                A[si][sj] %= 1000
+        return A
 
-def fill(fi, fj):
-    tmp = 0
-    for f in range(N):
-        tmp += A[fi][f]*C[fj][f]
-    return tmp%1000
+    tmp = square(A, B//2)
+
+    # 제곱 개수가 홀수인 경우
+    if B%2:
+        return 
 
 # 행렬 A의 크기 N, 제곱하는 수 B
 N, B = map(int, sys.stdin.readline().split())
 # 행렬 A
 A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-C = list(map(list, zip(*A)))
+# 행렬 A를 제곱한 결과
+answer = square(A, B)
 
-for _ in range(B-1):
-    A = power()
-
-for a in A:
+for a in answer:
     print(*a)
