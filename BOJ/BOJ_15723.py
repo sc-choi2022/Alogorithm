d3a5a7123f3239703801209d18af79787598c01e
sc-choi2022 <c.martin20222023@gmail.com>
2025-01-21 23:37:02 +0900

    0121

--- BOJ/BOJ_15723.py
+++ BOJ/BOJ_15723.py
@@ -1,11 +1,12 @@
 import sys
+from collections import defaultdict, deque
 
 # 정수 N
 N = int(sys.stdin.readline())
-INF = int(1e9)
-graph = [[INF]*26 for _ in range(26)]
+graph = defaultdict(list)
 
 for _ in range(N):
-    a, b = map(str, sys.stdin.readline().split(' is '))
+    tmp = list(map(int, sys.stdin.readline().split()))
+    graph[tmp[0]].append(tmp[2])
 
 M = int(sys.stdin.readline())
