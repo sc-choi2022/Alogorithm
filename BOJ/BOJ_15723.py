68391f213f3bb2a7b313ed279dd244a73970380e
sc-choi2022 <c.martin20222023@gmail.com>
2025-01-22 23:34:38 +0900

    0122

--- BOJ/BOJ_15723.py
+++ BOJ/BOJ_15723.py
@@ -1,6 +1,19 @@
 import sys
 from collections import defaultdict, deque
 
+def bfs(start, end):
+    queue = deque([start])
+
+    while queue:
+        x = queue.popleft()
+
+        if x == end:
+            return True
+
+        for node in graph[x]:
+            queue.append(node)
+    return False
+
 # 정수 N
 N = int(sys.stdin.readline())
 graph = defaultdict(list)
@@ -10,3 +23,11 @@ for _ in range(N):
     graph[tmp[0]].append(tmp[2])
 
 M = int(sys.stdin.readline())
+
+for _ in range(M):
+    lst = list(map(int, sys.stdin.readline().split()))
+
+    if bfs(lst[0], lst[1]):
+        print('T')
+    else:
+        print('F')
