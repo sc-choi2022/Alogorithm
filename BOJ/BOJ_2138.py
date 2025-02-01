3163d439d0bddd770c0777f8d9256029ff75bf15
sc-choi2022 <c.martin20222023@gmail.com>
2025-02-01 23:43:36 +0900

    0201

--- BOJ/BOJ_2138.py
+++ BOJ/BOJ_2138.py
@@ -1,3 +1,14 @@
 import sys
 
+def test():
+    return 
+
+# 스위치의 개수 N
 N = int(sys.stdin.readline())
+# 전구의 현재 상태 배열 now
+now = list(sys.stdin.readline().rstrip())
+# 만들고자 하는 전구의 상태 배열 goal
+goal = list(sys.stdin.readline().rstrip())
+
+print(now)
+print(goal)
