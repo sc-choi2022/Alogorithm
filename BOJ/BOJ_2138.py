fa9ba1572627c611c9e80487d96eb03379fdca80
sc-choi2022 <c.martin20222023@gmail.com>
2025-02-08 23:49:52 +0900

    0208

--- BOJ/BOJ_2138.py
+++ BOJ/BOJ_2138.py
@@ -1,30 +1,25 @@
 import sys
 
-def test():
-    cnt = 0
-    for i in range(1, N-1):
-        if now[i-1] != goal[i-1]:
-            cnt += 1
-            for j in range(i-1, i+2):
-                now[j] = not now[j]
-    if now[N-1] != goal[N-1]:
-        cnt += 1
-        now[N-2] = not now[N-2]
-        now[N-1] = not now[N-1]
-    if now == goal:
-        return cnt
-    else:
-        return -1
+def change():
+    return
 
 # 스위치의 개수 N
 N = int(sys.stdin.readline())
 # 전구의 현재 상태 배열 now
 now = list(sys.stdin.readline().rstrip())
 # 만들고자 하는 전구의 상태 배열 goal
+goal = list(sys.stdin.readlin().rstrip())
 
-on = off[::]
-on[0] = not on[0]
-on[1] = not on[1]
+# 0번 스위치를 누른 상태를 저장한 배열 zero
+zero = now[::]
+zero[0], zero[1] = not zero[0], not zero[1]
 
-if off == goal:
+if now == goal:
     print(0)
+else:
+    cnt = change(now, 0)
+    if cnt == -1:
+        print(cnt)
+    else:
+        cnt = change(zero, 1)
+        print(cnt if cnt else -1)
