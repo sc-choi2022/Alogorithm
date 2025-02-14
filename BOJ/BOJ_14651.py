73f39828deab52fb1db70796ce2dffb0b36c0204
sc-choi2022 <c.martin20222023@gmail.com>
2025-02-14 23:45:21 +0900

    0214

--- BOJ/BOJ_14651.py
+++ BOJ/BOJ_14651.py
@@ -1,10 +1,10 @@
 import sys
 
+# 입력받은 자리 수 N
 N = int(sys.stdin.readline())
 
-dp = [0] * (N+1)
-
-if N >= 2:
-    for i in range(3, N+1):
-        dp[i] = (dp[i-1]*3)%1000000009
-print(dp[N])
+if N == 1:
+    print(0)
+else:
+    # 0, 1, 2만 가지고 만들 수 있는 N자리 3의 배수의 개수를 출력
+    print((3**(N-2)*2) % 1000000009)
