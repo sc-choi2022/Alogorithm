e39a51df3242cddeadd9a6aa8c367dcc34ef82e9
sc-choi2022 <c.martin20222023@gmail.com>
2025-02-19 23:54:09 +0900

    0219

--- BOJ/BOJ_14567.py
+++ BOJ/BOJ_14567.py
@@ -1,7 +1,11 @@
+from collections import deque
 import sys
 
 # 과목의 수 N, 선수 조건의 수 M
 N, M = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
+    # 선수과목의 순서 A, B
     A, B = map(int, sys.stdin.readline().split())
+    graph[A].append(B)
