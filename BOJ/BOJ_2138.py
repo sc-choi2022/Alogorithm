6493b9d905c90eaebe9102aecd165c535abd430f
sc-choi2022 <c.martin20222023@gmail.com>
2025-02-21 20:05:54 +0900

    0221

--- BOJ/BOJ_2138.py
+++ BOJ/BOJ_2138.py
@@ -6,13 +6,17 @@ def change(status, cnt):
             cnt += 1
             for j in range(i-1, i+2):
                 status[j] = not status[j]
+
+    # 마지막 스위치를 눌러야하는 경우
     if status[N-1] != goal[N-1]:
         cnt += 1
         status[N-2] = not status[N-2]
         status[N-1] = not status[N-1]
 
+    # 만들고자하는 전구들의 상태인 경우 cnt
     if status == goal:
         return cnt
+    # 만들어지지 못한 경우 -1
     else:
         return -1
 
@@ -31,11 +35,14 @@ zero[0], zero[1] = not zero[0], not zero[1]
 if now == goal:
     print(0)
 else:
+    # 0번 스위치를 제외하고 스위치를 변경
     cnt = change(now, 0)
-    # 불가능한 경우 -1 출력
+
+    # 0번 전구의 변경 없이 전구를 완성한 경우 cnt 출력
     if cnt != -1:
         print(cnt)
     else:
-        # 0번째 전구를 눌렀을 때
+        # 0번 전구을 포함하여 스위치를 변경
         cnt = change(zero, 1)
-        print(cnt if cnt else -1)
+        # 최소 스위치 조작 횟수를 출력, 불가능한 경우에는 -1을 출력
+        print(cnt)
