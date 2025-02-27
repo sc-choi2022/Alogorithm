c0f8aeb58949423421c6e954f425cf5fda6b5b4a
sc-choi2022 <c.martin20222023@gmail.com>
2025-02-27 23:50:47 +0900

    0227

--- BOJ/BOJ_14728.py
+++ BOJ/BOJ_14728.py
@@ -6,12 +6,3 @@ N, T = map(int, sys.stdin.readline().split())
 study = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 dp = [[0]*T for _ in range(N)]
-
-for i in range(N):
-    for j in range(T):
-        if j >= study[i][0]:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-study[i][0]] + study[i][1])
-        else:
-            dp[i][j] = dp[i-1][j]
-
-print(dp[N-1][T-1])
