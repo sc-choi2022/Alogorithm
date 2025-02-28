d400dfa987bc2330ff5064816a93be69c0c97701
sc-choi2022 <c.martin20222023@gmail.com>
2025-02-28 23:50:03 +0900

    0228

--- BOJ/BOJ_14728.py
+++ BOJ/BOJ_14728.py
@@ -3,6 +3,6 @@ import sys
 # 단원의 개수 N, 공부할 수 있는 총 시간 T
 N, T = map(int, sys.stdin.readline().split())
 # 예상 공부 시간 K, 문제의 배점 S을 저장하는 배열 study
-study = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+study = [(0, 0)] + [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 dp = [[0]*T for _ in range(N)]
