ab3a1c2e5ed45d2d45fd0820f3fc88068fc34d0b
sc-choi2022 <c.martin20222023@gmail.com>
2025-03-04 23:58:05 +0900

    0304

--- BOJ/BOJ_14728.py
+++ BOJ/BOJ_14728.py
@@ -3,9 +3,9 @@ import sys
 # 단원의 개수 N, 공부할 수 있는 총 시간 T
 N, T = map(int, sys.stdin.readline().split())
 # 예상 공부 시간 K, 문제의 배점 S을 저장하는 배열 study
-study = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+study = [(0, 0)] + [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-dp = [[0]*T for _ in range(N)]
+dp = [[0]*(T+1) for _ in range(N+1)]
 
 for i in range(N):
     for j in range(T):
@@ -14,4 +14,5 @@ for i in range(N):
         else:
             dp[i][j] = dp[i-1][j]
 
+# 얻을 수 있는 최대 점수를 출력
 print(dp[N-1][T-1])
