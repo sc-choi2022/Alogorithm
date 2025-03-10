d0b2950666e8b897bc380e91223919af66df858a
sc-choi2022 <c.martin20222023@gmail.com>
2025-03-10 23:54:01 +0900

    0310

--- BOJ/BOJ_14728.py
+++ BOJ/BOJ_14728.py
@@ -7,12 +7,12 @@ study = [(0, 0)] + [tuple(map(int, sys.stdin.readline().split())) for _ in range
 
 dp = [[0]*(T+1) for _ in range(N+1)]
 
-for i in range(N):
-    for j in range(T):
+for i in range(1, N+1):
+    for j in range(1, T+1):
         if j >= study[i][0]:
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-study[i][0]] + study[i][1])
         else:
             dp[i][j] = dp[i-1][j]
 
 # 얻을 수 있는 최대 점수를 출력
-print(dp[N-1][T-1])
+print(dp[N][T])
