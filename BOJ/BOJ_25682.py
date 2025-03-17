1c04b06425a8326d59342227f1075c5fb5781960
sc-choi2022 <c.martin20222023@gmail.com>
2025-03-17 23:28:40 +0900

    0317

--- BOJ/BOJ_25682.py
+++ BOJ/BOJ_25682.py
@@ -5,4 +5,11 @@ N, M, K = map(int, sys.stdin.readline().split())
 # 보드의 상태를 저장하는 배열 board
 board = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
 
-answer = 0
+B = [[0]*(M+1) for _ in range(N+1)]
+W = [[0]*(M+1) for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(M):
+        if (i+j)%2:
+            if board[i][j] == 'B':
+                
