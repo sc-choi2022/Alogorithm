af875b127d4e10ca4b1450500cd509386f66f042
sc-choi2022 <c.martin20222023@gmail.com>
2025-04-01 21:20:49 +0900

    0401

--- BOJ/BOJ_17143.py
+++ BOJ/BOJ_17143.py
@@ -1,4 +1,11 @@
 import sys
+sys.setrecursionlimit(10**8)
+
+def fish():
+    return
+
+def move():
+    return 
 
 R, C, M = map(int, sys.stdin.readline().split())
 board = [[0]*C for _ in range(R)]
