c42d74a42552b0a9556c8a132303b5b27f1c8b91
sc-choi2022 <c.martin20222023@gmail.com>
2025-04-03 23:56:26 +0900

    0403

--- BOJ/BOJ_17143.py
+++ BOJ/BOJ_17143.py
@@ -7,9 +7,12 @@ def fish():
 def move():
     return
 
+# 격자판의 크기 R, C, 상어의 수 M
 R, C, M = map(int, sys.stdin.readline().split())
 board = [[0]*C for _ in range(R)]
 
 for _ in range(M):
+    # 상어의 정보: 위치(r, c), 속력 s, 이동방향 d, 크기 z
     r, c, s, d, z = map(int, sys.stdin.readline().split())
-    
+    r, c = r-1, c-1
+    board[r][c] = (s, d, z)
