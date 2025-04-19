62672f11fffa1695138915473ef430c29b8beb26
sc-choi2022 <c.martin20222023@gmail.com>
2025-04-19 23:58:16 +0900

    0419

--- BOJ/BOJ_17406.py
+++ BOJ/BOJ_17406.py
@@ -1,6 +1,20 @@
 import sys
 
+def turn():
+    return
+
 # 배열 A의 크기 N, M, 회전 연산의 개수 K
 N, M, K = map(int, sys.stdin.readline().split())
 # 배열 A
 A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+for _ in range(K):
+    # 회전 연산의 세 정수 r, c, s
+    r, c, s = map(int, sys.stdin.readline().split())
+
+
+answer = 1e8
+for i in range(N):
+    answer = min(answer, sum(A[i]))
+
+print(answer)
