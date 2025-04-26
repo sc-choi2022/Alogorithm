e6dcd17446df3a83a35ba03dbfb374d731628025
sc-choi2022 <c.martin20222023@gmail.com>
2025-04-26 23:59:38 +0900

    0426

--- BOJ/BOJ_14567.py
+++ BOJ/BOJ_14567.py
@@ -4,10 +4,6 @@ import sys
 def sorting():
     queue = deque()
 
-    for i in range(N):
-        if not degree[i]:
-            queue.append(i)
-            answer[i] = 1
 
     for j in range(N):
         now = queue.popleft()
