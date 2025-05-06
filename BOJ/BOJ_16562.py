bb9e6edfa811cec2238c90be46aecf98ffcdb1d2
sc-choi2022 <c.martin20222023@gmail.com>
2025-05-06 23:45:23 +0900

    0506

---
+++ BOJ/BOJ_16562.py
@@ -0,0 +1,13 @@
+import sys
+
+# 학생 수 N, 친구관계 수 M, 가지고 있는 돈 K
+N, M, K = map(int, sys.stdin.readline().split())
+# 친구비를 저장하는 배열 money
+money = list(map(int, sys.stdin.readline().split()))
+# 친구 관계를 저장하는 배열 friends
+friends = [[] for _ in range(N+1)]
+
+for i in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    friends[a].append(b)
+    friends[b].append(a)
