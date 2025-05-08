89837f6f2b8a1449ed5c33624a4aa9bbd1e38ef2
sc-choi2022 <c.martin20222023@gmail.com>
2025-05-08 23:50:55 +0900

    0508

--- BOJ/BOJ_16562.py
+++ BOJ/BOJ_16562.py
@@ -1,7 +1,10 @@
 import sys
 
-def make():
-    return 
+def make(si):
+    for i in range(1, N+1):
+        if not visit[i]:
+
+    return
 
 # 학생 수 N, 친구관계 수 M, 가지고 있는 돈 K
 N, M, K = map(int, sys.stdin.readline().split())
@@ -10,10 +13,8 @@ money = list(map(int, sys.stdin.readline().split()))
 # 친구 관계를 저장하는 배열 friends
 friends = [[] for _ in range(N+1)]
 
-for i in range(M):
-    a, b = map(int, sys.stdin.readline().split())
-    friends[a].append(b)
-    friends[b].append(a)
+for _ in range(M):
+    a, m = map(int, sys.stdin.readline().split())
 
 # 친구로 만드는 최소비용 answer
 answer = 1e9
