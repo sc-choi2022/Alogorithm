f4ac86b5b1e07baa6269c11b7523f02fff398af4
sc-choi2022 <c.martin20222023@gmail.com>
2025-05-22 23:43:19 +0900

    0522

--- BOJ/BOJ_17143.py
+++ BOJ/BOJ_17143.py
@@ -4,13 +4,12 @@ import sys
 R, C, M = map(int, sys.stdin.readline().split())
 board = [[0]*C for _ in range(R)]
 
-direction = [(0, 0), (-1, 0), (1, 0), (0, 1), (0, -1)]
+direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
 for _ in range(M):
     # 상어의 정보: 위치(r, c), 속력 s, 이동방향 d, 크기 z
     r, c, s, d, z = map(int, sys.stdin.readline().split())
-    r, c = r - 1, c - 1
-    board[r][c] = (s, d, z)
+    board[r-1][c-1] = (s, d-1, z)
 
 # 두 상어가 같은 크기를 갖는 경우는 없고, 하나의 칸에 둘 이상의 상어가 있는 경우는 없다.
 # 낚시왕이 잡은 상어 크기의 합 answer 출력
@@ -28,7 +27,6 @@ for j in range(C):
             # 상어가 있는 경우
             if board[ii][jj]:
                 ss, dd, zz = board[ii][jj]
-
                 ni, nj = ii, jj
                 # 상하로 움직이는 상어인 경우
                 if dd == 0 or dd == 1:
@@ -56,6 +54,7 @@ for j in range(C):
                     move[ni][nj] = (ss, dd, zz)
                 elif move[ni][nj][2] < zz:
                     move[ni][nj] = (ss, dd, zz)
+    # 상어의 위치 정보 갱신
     board = move
 
 print(answer)
