106bc5d0d72210d00ce1b727762e2aa12c6db300
sc-choi2022 <c.martin20222023@gmail.com>
2025-05-27 23:23:30 +0900

    0527

--- BOJ/BOJ_15719.py
+++ BOJ/BOJ_15719.py
@@ -1,13 +1,19 @@
 import sys
 
-def number(n, lst):
-    add = 0
-    tmp = ''
-
-
 # 수열의 크기 N
 N = int(sys.stdin.readline())
 # 수열 A
 A = sys.stdin.readline().rstrip()
 
-print(number(N, A))
+add = 0
+tmp = ''
+
+for a in A:
+    if a.isdigit():
+        tmp += a
+    else:
+        add += int(tmp)
+        tmp = ''
+add += int(tmp)
+
+print(add-sum(range(1, int(N))))
