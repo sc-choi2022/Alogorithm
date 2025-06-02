d05dab568f91a3cb159c96431770490a4e9ed3a6
sc-choi2022 <c.martin20222023@gmail.com>
2025-06-02 23:09:30 +0900

    0602

--- BOJ/BOJ_11663.py
+++ BOJ/BOJ_11663.py
@@ -1,9 +1,11 @@
 import sys
 
 def find():
-    return 
+    return
 
 # 점의 개수 N, 선분의 개수 M
 N, M = map(int, sys.stdin.readline().split())
 # 점의 좌표를 저장하는 배열 dots
-dots = list(map(int, sys.stdin.readline().split()))
+dots = sorted(list(map(int, sys.stdin.readline().split())))
+# 선분의 시작, 끝 점을 저장하는 배열 lines
+lines = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
