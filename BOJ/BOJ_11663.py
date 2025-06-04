db0d59aa2fc2e8187427a7a2fb5720d19a6cb54a
sc-choi2022 <c.martin20222023@gmail.com>
2025-06-04 22:27:02 +0900

    0604

--- BOJ/BOJ_11663.py
+++ BOJ/BOJ_11663.py
@@ -1,6 +1,10 @@
 import sys
 
-def find():
+def find_start():
+
+    return
+
+def find_end():
     return
 
 # 점의 개수 N, 선분의 개수 M
@@ -12,5 +16,5 @@ lines = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
 
 for line in lines:
     start, end = line
-    start_idx = find()
-    end_idx = find()
+    start_idx = find_start()
+    end_idx = find_end()
