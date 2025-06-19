66594cdeda3b59b668505b4c86f63cece9828d8c
sc-choi2022 <c.martin20222023@gmail.com>
2025-06-19 12:34:37 +0900

    0619

--- BOJ/BOJ_19638.py
+++ BOJ/BOJ_19638.py
@@ -1,33 +1,37 @@
 from heapq import heappush, heappop
 import sys
 
-# 거인의 인구수 N, 센티의 키 H, 뿅망치의 횟수 제한 T
+# 센티를 제외한 거인 나라의 인구수 N, 센티의 키 H, 마법의 뿅망치 횟수 제한 T
 N, H, T = map(int, sys.stdin.readline().split())
 
-# 거인의 키를 저장 height
-height = []
-for _ in range(N):
-    heappush(height, -(int(sys.stdin.readline())))
+# 키를 저장하는 배열 heights
+heights = []
 
-# 뿅망치의 사용 횟수 cnt
+for _ in range(N):
+    heappush(heights, -int(sys.stdin.readline()))
+
+# 마법의 뿅망치 사용 횟수 cnt
 cnt = 0
 
-for i in range(T):
-    magic = -1*heappop(height)
+for _ in range(T):
+    # 거인 중 가장 큰 거인의 키 magic
+    magic = -1*heappop(heights)
 
-    if magic >= H:
-        heappush(height, -1*(magic//2))
+    if magic == 1:
+        heappush(heights, -1)
+    elif magic >= H:
+        heappush(heights, -1*(magic//2))
         cnt += 1
     else:
-        heappush(height, -1*magic)
+        heappush(heights, -1*magic)
 
-# 뿅망치의 최소 사용 횟수 혹은 키가 가장 큰 거인의 키
-result = -1*heappop(height)
+# 뿅망치 사용 후 가장 큰 거인의 키 giants
+giants = -1*heappop(heights)
 
 # 뿅망치 횟수를 사용 후 세티보다 키가 큰 거인이 있는 경우
-if result >= H:
+if giants >= H:
     print('NO')
-    print(result)
+    print(giants)
 # 모든 거인이 센티보다 키가 작도록 할 수 있는 경우
 else:
     print('YES')
