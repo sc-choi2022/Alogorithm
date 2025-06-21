546ce28e2495c0438c65c7a39fc48500397fda1e
sc-choi2022 <c.martin20222023@gmail.com>
2025-06-21 23:49:46 +0900

    0621

--- BOJ/BOJ_15719.py
+++ BOJ/BOJ_15719.py
@@ -5,15 +5,3 @@ N = int(sys.stdin.readline())
 # 수열 A
 A = sys.stdin.readline().rstrip()
 
-add = 0
-tmp = ''
-
-for a in A:
-    if a.isdigit():
-        tmp += a
-    else:
-        add += int(tmp)
-        tmp = ''
-add += int(tmp)
-
-print(add-sum(range(1, int(N))))
