3a8114735c865772460540ab2994ea24eb57286d
sc-choi2022 <c.martin20222023@gmail.com>
2025-06-24 23:56:25 +0900

    0624

--- BOJ/BOJ_25682.py
+++ BOJ/BOJ_25682.py
@@ -3,7 +3,7 @@ import sys
 # 보드의 크기 N, M 체스판의 크기 K
 N, M, K = map(int, sys.stdin.readline().split())
 # 보드의 상태를 저장하는 배열 board
-board = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
+board = [sys.stdin.readline().rstrip() for _ in range(N)]
 
 B = [[0]*(M+1) for _ in range(N+1)]
 W = [[0]*(M+1) for _ in range(N+1)]
