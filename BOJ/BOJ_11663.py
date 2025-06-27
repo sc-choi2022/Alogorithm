b4cf89cbddc23ba142594ad1be02c42a8a1a74e5
sc-choi2022 <c.martin20222023@gmail.com>
2025-06-27 23:46:34 +0900

    0627

--- BOJ/BOJ_11663.py
+++ BOJ/BOJ_11663.py
@@ -26,10 +26,3 @@ N, M = map(int, sys.stdin.readline().split())
 dots = sorted(list(map(int, sys.stdin.readline().split())))
 # 선분의 시작, 끝 점을 저장하는 배열 lines
 lines = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
-
-for line in lines:
-    start, end = line
-    start_idx = find_start(start)
-    end_idx = find_end(end)
-
-    print(end_idx - start_idx)
