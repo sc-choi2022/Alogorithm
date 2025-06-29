2b84931152e1b27323707410cba2c0fbca18b098
sc-choi2022 <c.martin20222023@gmail.com>
2025-06-29 23:57:33 +0900

    0629

--- BOJ/BOJ_24039.py
+++ BOJ/BOJ_24039.py
@@ -4,9 +4,11 @@ import sys
 N = int(sys.stdin.readline())
 # 소수의 내용을 저장하는 배열 prime
 prime = [1 for _ in range(N+1)]
-prime_number = []
+# 특별한 수를 저장하는 배열 special
+special = []
 start = 0
 
+# 특별한 수가 6인 경우
 if N < 4:
     print(6)
 else:
@@ -17,12 +19,12 @@ else:
             j += 1
     for ii in range(2, N+1):
         if prime[ii]:
-            prime_number.append(ii)
+            special_number.append(ii)
             if ii <= int(N**0.5):
                 start = ii
 
-    for iii in range(prime_number.index(start), len(prime_number)-1):
-        result = prime_number[iii]*prime_number[iii+1]
+    for iii in range(prime_number.index(start), len(special_number)-1):
+        result = special_number[iii]*special_number[iii+1]
         if result > N:
             print(result)
             break
