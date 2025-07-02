eb6fb0e96d94e14bab4d9b84f1c4104b8ebec294
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-02 23:57:43 +0900

    0702

--- BOJ/BOJ_17503.py
+++ BOJ/BOJ_17503.py
@@ -3,4 +3,14 @@ import sys
 
 # 축제가 열리는 기간 N, 선호도의 합 M, 마실 수 있는 맥주 종류의 수 K
 N, M, K = map(int, sys.stdin.readline().split())
-V = [map(int, sys.stdin.readline().split()) for _ in range(K)]
+beer = [map(int, sys.stdin.readline().split()) for _ in range(K)]
+beer.sort(key=lambda x:x[1])
+
+like = []
+
+answer = 0
+for b in beer:
+    if len(like) < N:
+        heappush(like, b)
+        answer += b[0]
+        
