9b9876facabe7452984953ec663266a36be9d0ea
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-06 23:42:03 +0900

    0706

--- BOJ/BOJ_17503.py
+++ BOJ/BOJ_17503.py
@@ -16,7 +16,7 @@ def test():
 
 # 축제가 열리는 기간 N, 선호도의 합 M, 마실 수 있는 맥주 종류의 수 K
 N, M, K = map(int, sys.stdin.readline().split())
-beer = [map(int, sys.stdin.readline().split()) for _ in range(K)]
+beer = [tuple(map(int, sys.stdin.readline().split())) for _ in range(K)]
 beer.sort(key=lambda x:x[1])
 
 like = []
