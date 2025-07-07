9269330d858bb560c325d071766c8d9f0ab46db1
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-07 23:59:08 +0900

    0707

--- BOJ/BOJ_17503.py
+++ BOJ/BOJ_17503.py
@@ -2,16 +2,16 @@ from heapq import heappush, heappop
 import sys
 
 def test():
-    global answer
+    global prefer
     for b in beer:
         if len(like) < N:
             heappush(like, b)
-            answer += b[0]
+            prefer += b[0]
             if len(like) == N:
-                if answer >= M:
+                if prefer >= M:
                     return b[1]
                 else:
-                    answer -= heappop(like)[0]
+                    prefer -= heappop(like)[0]
     return -1
 
 # 축제가 열리는 기간 N, 선호도의 합 M, 마실 수 있는 맥주 종류의 수 K
@@ -21,7 +21,8 @@ beer.sort(key=lambda x:x[1])
 
 like = []
 
-answer = 0
+# 선호도의 합 prefer
+prefer = 0
 
 # 최솟값을 출력
 # 조건을 만족시킬 수 없으면 -1 출력
