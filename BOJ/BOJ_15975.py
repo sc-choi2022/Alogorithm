0626d28590fdf7fa0eaa33539daf11d772a20b35
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-10 23:56:45 +0900

    0710

--- BOJ/BOJ_15975.py
+++ BOJ/BOJ_15975.py
@@ -6,3 +6,11 @@ N = int(sys.stdin.readline())
 # 점의 정보를 저장하는 배열 dots
 dots = [int(sys.stdin.readline()) for _ in range(N)]
 dots.sort()
+
+for i in range(N):
+    if i == 0:
+        continue
+    elif i == N-1:
+        continue
+    else:
+        
