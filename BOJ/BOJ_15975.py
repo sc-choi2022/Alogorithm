ea7167f187f06a6cdd8c2448068c08acf67c01e4
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-11 23:48:34 +0900

    0711

--- BOJ/BOJ_15975.py
+++ BOJ/BOJ_15975.py
@@ -4,13 +4,17 @@ import sys
 N = int(sys.stdin.readline())
 
 # 점의 정보를 저장하는 배열 dots
-dots = [int(sys.stdin.readline()) for _ in range(N)]
+dots = [map(int, sys.stdin.readline().split()) for _ in range(N)]
 dots.sort()
+
+result = 0
 
 for i in range(N):
     if i == 0:
-        continue
+        if dots[i][0] == dots[i+1][0]:
+            result += result[i+1][1] - result[i][1]
     elif i == N-1:
-        continue
+        if dots[i][0] == dots[i-1][0]:
+            result += result[i][1] - result[i-1][1]
     else:
-        
+        continue
