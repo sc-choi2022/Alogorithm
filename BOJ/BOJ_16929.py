3b5ee674eafd536453c81f44d25be431327e5b17
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-15 23:42:03 +0900

    0715

--- BOJ/BOJ_16929.py
+++ BOJ/BOJ_16929.py
@@ -1,5 +1,17 @@
 import sys
 
-N, M = map(int, sys.stdin.readline().split())
+def cycle():
+    if True:
+        return True
+    else:
+        return False
 
+# 게임판의 크기 N, M
+N, M = map(int, sys.stdin.readline().split())
+# 게이판의 점의 색을 저장하는 배열 dots
 dots = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
+
+if cycle():
+    print('Yes')
+else:
+    print('No')
