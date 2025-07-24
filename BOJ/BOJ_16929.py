aeaee35427386847b869e3d7fc4c0aac7935ba04
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-24 23:47:32 +0900

    0724

--- BOJ/BOJ_16929.py
+++ BOJ/BOJ_16929.py
@@ -1,5 +1,32 @@
 import sys
 
+def solve():
+    global si, sj, answer
+    for si in range(N):
+        for sj in range(M):
+            visit[si][sj] = 1
+            dfs(si, sj, 1)
+            visit[si][sj] = 0
+
+            if answer == 'Yes':
+                return
+
+def dfs(ci, cj, cnt):
+    global answer
+
+    for d in range(4):
+        di, dj = D[d]
+        ni, nj = ci+di, cj+dj
+        if 0 <= ni < N and 0 <= nj < M and dots[ni][nj] == dots[ci][cj]:
+            if (ni, nj) == (si, sj) and cnt > 3:
+                answer = 'Yes'
+                return
+            if not visit[ni][nj]:
+                visit[ni][nj] = 1
+                dfs(ni, nj, cnt+1)
+                visit[ni][nj] = 0
+
+
 # 게임판의 크기 N, M
 N, M = map(int, sys.stdin.readline().split())
 # 게이판의 점의 색을 저장하는 배열 dots
@@ -9,3 +36,8 @@ visit = [[0]*M for _ in range(N)]
 # 방향을 저장하는 딕셔너리 D
 D = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)}
 si, sj = 0, 0
+answer = 'No'
+
+solve()
+
+print(answer)
