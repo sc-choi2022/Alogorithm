bb963da56f3b107ae03af849a1a2f22701e0d04c
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-26 23:56:02 +0900

    0726

--- BOJ/BOJ_16929.py
+++ BOJ/BOJ_16929.py
@@ -15,11 +15,13 @@ def dfs(ci, cj, dn, cnt):
     global answer
 
     for d in range(4):
+        # 왔던 방향을 돌아가는 것을 제한
         if (dn-2)%4 == d:
             continue
         di, dj = D[d]
         ni, nj = ci+di, cj+dj
         if 0 <= ni < N and 0 <= nj < M and dots[ni][nj] == dots[ci][cj]:
+            # 사이클은 찾은 경우
             if (ni, nj) == (si, sj) and cnt > 3:
                 answer = 'Yes'
                 return
@@ -40,6 +42,8 @@ D = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)}
 si, sj = 0, 0
 answer = 'No'
 
+# 사이클을 확인하는 함수 solve
+# 사이클을 찾은 경우 종료
 solve()
 
 print(answer)
