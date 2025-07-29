c245091885a66b05db91c46cb56f73ce205b3645
sc-choi2022 <c.martin20222023@gmail.com>
2025-07-29 23:42:17 +0900

    0729

--- BOJ/BOJ_10835.py
+++ BOJ/BOJ_10835.py
@@ -8,4 +8,9 @@ B = list(map(int, sys.stdin.readline().split()))
 
 dp = [[0]*N for _ in range(N)]
 
+for i in range(1, N):
+    for j in range(1, N):
+        if A[i] == B[j]:
+            continue
+
 print(dp[-1][-1])
