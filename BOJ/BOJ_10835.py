ce7d0cae033534c5772275bd69755e88e9f0e02d
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-01 23:35:37 +0900

    0801

--- BOJ/BOJ_10835.py
+++ BOJ/BOJ_10835.py
@@ -5,3 +5,10 @@ N = int(sys.stdin.readline())
 
 A = list(map(int, sys.stdin.readline().split()))
 B = list(map(int, sys.stdin.readline().split()))
+
+dp = [0] * N
+
+for i in range(N):
+    for j in range(N):
+        if A[i] == B[j]:
+            dp[i][j] = 1
