37cc750de0b05880893b0eee21068ff9ce4ccf36
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-03 23:59:26 +0900

    0803

--- BOJ/BOJ_10835.py
+++ BOJ/BOJ_10835.py
@@ -6,11 +6,9 @@ N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
 B = list(map(int, sys.stdin.readline().split()))
 
-dp = [0] * N
+dp = [[0]*N for _ in range(N)]
 
 for i in range(N):
     for j in range(N):
         if A[i] < B[j]:
             dp[i][j] += B[j]
-        else:
-            
