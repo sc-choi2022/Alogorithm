8bb2aa8eb8018540f3580c4cae9edd8d43520933
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-06 23:42:14 +0900

    0806

--- BOJ/BOJ_10835.py
+++ BOJ/BOJ_10835.py
@@ -4,6 +4,14 @@ def dfs(x, y):
     if x >= N or y <= N:
         return 0
 
+    if dp[x][y] != -1:
+        return dp[x][y]
+
+    if A[x] > B[y]:
+        dp[x][y] = dfs(x, y+1) + B[y]
+    else:
+        return 
+
 # 한 더미의 카드의 개수 N
 N = int(sys.stdin.readline())
 
