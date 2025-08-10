257e941a9c08923f9aeb35732fa1bc929efb668d
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-10 23:53:21 +0900

    0810

--- BOJ/BOJ_10835.py
+++ BOJ/BOJ_10835.py
@@ -4,6 +4,12 @@ def dfs(i, j):
     if i >= N or j <= N:
         return 0
 
+    if dp[i][j] != -1:
+        return dp[i][j]
+
+    if A[i] > B[j]:
+        dp[i][j] = dfs(i, j+1) + B[j]
+
 # 한 더미의 카드의 개수 N
 N = int(sys.stdin.readline())
 
@@ -12,5 +18,5 @@ B = list(map(int, sys.stdin.readline().split()))
 
 dp = [[-1]*N for _ in range(N)]
 
-dfs(0,0)
+dfs(0, 0)
 print(dp[0][0])
