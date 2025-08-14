8587c886d6057a0313dcefaac83266a00c8f381a
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-14 23:38:41 +0900

    0814

---
+++ BOJ/BOJ_13333.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+numbers = sorted(list(map(int, sys.stdin.readline().split())))
+
+M = numbers[-1]
+Q = 0
+
+for i in range(M, 1, -1):
+    tmp = list(filter(lambda x:x >=i, numbers))
+    if len(tmp) >= i:
+        Q = i
+        break
+print(Q)
