60b5106def9f63d5d221a5b354926d6e8f3d9983
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-17 23:56:11 +0900

    0817

--- BOJ/BOJ_10835.py
+++ BOJ/BOJ_10835.py
@@ -2,7 +2,21 @@ import sys
 sys.setrecursionlimit(10**6)
 
 def dfs(i, j):
-    return 
+    if i == N or j == N:
+        return 0
+
+    if dp[i][j] >= 0:
+        return dp[i][j]
+
+    if A[i] > B[j]:
+        dp[i][j] = dfs(i, j+1) + B[j]
+    else:
+        s1, s2 = dfs(i+1, j), dfs(i+1, j+1)
+        if s1 >= s2:
+            dp[i][j] = s1
+        else:
+            dp[i][j] = s2
+    return dp[i][j]
 
 # 한 더미의 카드의 개수 N
 N = int(sys.stdin.readline())
@@ -12,5 +26,4 @@ B = list(map(int, sys.stdin.readline().split()))
 
 dp = [[-1]*N for _ in range(N)]
 
-dfs(0, 0)
-print(dp[0][0])
+print(dfs(0, 0))
