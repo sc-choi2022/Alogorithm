0a86b4a821ae513a6bf4d3c32f2b73c3d3fb3645
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-21 23:52:46 +0900

    0821

--- BOJ/BOJ_10835.py
+++ BOJ/BOJ_10835.py
@@ -40,4 +40,8 @@ dp[0][0] = 0
 for i in range(N):
     for j in range(N):
         if dp[i][j] == -1:
-            
+            dp[i][j+1] = -1
+            continue
+        if dp[i+1][j] < dp[i][j]:
+            dp[i+1][j] = dp[i][j]
+        
