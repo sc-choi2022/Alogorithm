f513525c9f2e4326d5a3eebf5a0bcf0c61849f95
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-27 23:46:27 +0900

    0827

--- BOJ/BOJ_17352.py
+++ BOJ/BOJ_17352.py
@@ -4,7 +4,11 @@ def find():
     return
 
 def union():
-    return 
+    return
 
 N = int(sys.stdin.readline())
 island = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+for _ in range(N-2):
+    a, b = map(int, sys.stdin.readline().split())
+    union(a, b)
