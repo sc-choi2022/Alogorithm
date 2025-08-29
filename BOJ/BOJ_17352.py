48954c1eef77ca38130aa94fc09a873300b5e9f5
sc-choi2022 <c.martin20222023@gmail.com>
2025-08-29 23:48:40 +0900

    0829

--- BOJ/BOJ_17352.py
+++ BOJ/BOJ_17352.py
@@ -8,3 +8,8 @@ island = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 for _ in range(N-2):
     a, b = map(int, sys.stdin.readline().split())
     union(a, b)
+
+for i in range(2, N+1):
+    if find(1) != find(i):
+        print(1, i)
+        break
