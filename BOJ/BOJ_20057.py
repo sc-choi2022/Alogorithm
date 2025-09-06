01f1102fb7ff12ad28d33275d92e3fe7a6074f34
sc-choi2022 <c.martin20222023@gmail.com>
2025-09-06 23:45:45 +0900

    0906

--- BOJ/BOJ_20057.py
+++ BOJ/BOJ_20057.py
@@ -1,7 +1,10 @@
 import sys
 
-N = int(sys.stdin.readline())
+def move(si, sj):
+    global
 
+N = int(sys.stdin.readline())
 A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 direction = {0: (0, -1), 1:(1, 0), 2:(0, 1), 3:(-1, 0)}
+sand = 0
