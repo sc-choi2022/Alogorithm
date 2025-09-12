12e0fc1a3daf6357f94b0351f290bf352bfeb219
sc-choi2022 <c.martin20222023@gmail.com>
2025-09-12 23:41:48 +0900

    0912

--- BOJ/BOJ_20057.py
+++ BOJ/BOJ_20057.py
@@ -1,6 +1,14 @@
 import sys
 
 def move(si, sj):
+    global sand
+
+    ai, aj = -1, -1
+
+    remain = 0
+    for mi in range(-2, 3):
+        for mj in range(-2, 3):
+            return
 
 
 N = int(sys.stdin.readline())
