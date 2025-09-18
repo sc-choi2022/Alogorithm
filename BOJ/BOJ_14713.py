407b35c3b39022d4c5e96bb4c32a9f99d1812ac7
sc-choi2022 <c.martin20222023@gmail.com>
2025-09-18 23:55:47 +0900

    0918

--- BOJ/BOJ_14713.py
+++ BOJ/BOJ_14713.py
@@ -1,16 +1,10 @@
+from collections import deque
 import sys
 
 # 앵무새의 수 N
 N = int(sys.stdin.readline())
 
-# 앵무새가 말한 단어의 총 개수 answer
-answer = 0
-check = 0
-
-for i in range(N):
-    # 앵무새가 말한 문장 S
-    S = sys.stdin.readline().rstrip()
-    L += len(S[i])
+L = deque([])
 
 if check and L == len(R):
     print('Impossible')
