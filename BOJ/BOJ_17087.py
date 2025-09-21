3b2c239894a30dce2518938e97effd53c19ec093
sc-choi2022 <c.martin20222023@gmail.com>
2025-09-21 23:52:56 +0900

    0921

--- BOJ/BOJ_17087.py
+++ BOJ/BOJ_17087.py
@@ -1,7 +1,12 @@
-import sys
+import sys, math
 
 N, S = map(int, sys.stdin.readline().split())
 A = list(map(int, sys.stdin.readline().split()))
 
 Ds = [set(abs(A[i]-S) for i in range(N))]
 D = min(Ds)
+
+for i in range(N):
+    continue
+
+print(D)
