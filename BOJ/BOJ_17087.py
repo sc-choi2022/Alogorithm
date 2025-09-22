eea88a759638a4866dd945dbc4f025f45cbe71ed
sc-choi2022 <c.martin20222023@gmail.com>
2025-09-22 23:48:03 +0900

    0922

--- BOJ/BOJ_17087.py
+++ BOJ/BOJ_17087.py
@@ -1,4 +1,11 @@
-import sys, math
+import sys
+
+def GCD(a, b):
+    while b:
+        mod = b
+        b = a%b
+        a = mod
+    return a
 
 N, S = map(int, sys.stdin.readline().split())
 A = list(map(int, sys.stdin.readline().split()))
@@ -7,6 +14,6 @@ Ds = [set(abs(A[i]-S) for i in range(N))]
 D = min(Ds)
 
 for i in range(N):
-    continue
+    D = GCD(Ds[i], D)
 
 print(D)
