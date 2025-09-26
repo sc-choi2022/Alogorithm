a0e7dc17f28452b5dc3f5bc7f33bef6cb85fa45e
sc-choi2022 <c.martin20222023@gmail.com>
2025-09-26 23:59:11 +0900

    0926

--- BOJ/BOJ_14713.py
+++ BOJ/BOJ_14713.py
@@ -1,11 +1,14 @@
 from collections import deque
 import sys
 
+def check():
+    return 
+
 # 앵무새의 수 N
 N = int(sys.stdin.readline())
 
 L = [deque(map(str, sys.stdin.readline().split())) for _ in range(N)]
-LL = list(sys.stdin.readline().rstrip().split())
+S = list(sys.stdin.readline().rstrip().split())
 
 
 if check and L == len(R):
