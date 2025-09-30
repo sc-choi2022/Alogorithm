9a23be4b75330c505a4a36d0dd6c81fe62685fb9
sc-choi2022 <c.martin20222023@gmail.com>
2025-09-30 23:51:54 +0900

    0929

--- BOJ/BOJ_14713.py
+++ BOJ/BOJ_14713.py
@@ -4,13 +4,25 @@ import sys
 def check(sentence, lines):
     cnt = 0
 
+    i = 0
+    while sentence:
+        if lines[i] and sentence[0] == L[0]:
+            lines[i].popleft()
+            sentence.popleft()
+            cnt = 0
+        else:
+            if cnt == N:
+                return False
+            cnt += 1
+        i = (i+1)%N
+    
     return 
 
 # 앵무새의 수 N
 N = int(sys.stdin.readline())
 
 L = [deque(map(str, sys.stdin.readline().split())) for _ in range(N)]
-S = list(sys.stdin.readline().rstrip().split())
+S = deque(list(sys.stdin.readline().rstrip().split()))
 
 if check(S, L):
     print('Possible')
