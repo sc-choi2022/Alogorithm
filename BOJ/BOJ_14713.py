0b8e54712748e57661be9593f93ce95b8ddf77e8
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-02 23:54:34 +0900

    1001

--- BOJ/BOJ_14713.py
+++ BOJ/BOJ_14713.py
@@ -1,30 +1,7 @@
 from collections import deque
 import sys
 
-def check(sentence, lines):
-    cnt = 0
 
-    i = 0
-    while sentence:
-        if lines[i] and sentence[0] == L[0]:
-            lines[i].popleft()
-            sentence.popleft()
-            cnt = 0
-        else:
-            if cnt == N:
-                return False
-            cnt += 1
-        i = (i+1)%N
-
-    E = 0
-    for j in range(N):
-        if not len(lines[j]):
-            E += 1
-
-    if not sentence and E == N:
-        return True
-    else:
-        return False
 
 # 앵무새의 수 N
 N = int(sys.stdin.readline())
