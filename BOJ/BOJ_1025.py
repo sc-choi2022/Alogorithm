1174f9c3ba5b979a6b9a5b2461ad958a2fb38707
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-06 23:53:17 +0900

    1005

--- BOJ/BOJ_1025.py
+++ BOJ/BOJ_1025.py
@@ -5,3 +5,11 @@ numbers = list(map(int, list(sys.stdin.readline().rstrip())))
 
 answer = -1
 
+pd, qd = [], []
+
+for i in range(1, P+1):
+    if P%i == 0:
+        pd.append(i)
+for j in range(1, Q+1):
+    if Q%i == 0:
+        qd.append(j)
