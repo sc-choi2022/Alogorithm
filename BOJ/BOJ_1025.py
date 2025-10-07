08334019d5c7fa7f692bec48946b3831f118476e
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-07 23:56:33 +0900

    1006

--- BOJ/BOJ_1025.py
+++ BOJ/BOJ_1025.py
@@ -1,15 +1,6 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-numbers = list(map(int, list(sys.stdin.readline().rstrip())))
+numbers = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
 
 answer = -1
-
-pd, qd = [], []
-
-for i in range(1, P+1):
-    if P%i == 0:
-        pd.append(i)
-for j in range(1, Q+1):
-    if Q%i == 0:
-        qd.append(j)
