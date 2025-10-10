5021858ddfd09cb129bd64e4bd0859176f343c1b
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-10 23:13:18 +0900

    1010

--- BOJ/BOJ_15954.py
+++ BOJ/BOJ_15954.py
@@ -4,3 +4,14 @@ import sys
 N, K = map(int, sys.stdin.readline().split())
 # 인형을 선호하는 사람의 수 numbers
 numbers = list(map(int, sys.stdin.readline().split()))
+
+answer = float('inf')
+
+for i in range(N-K+1):
+    for j in range(N-K-i+2):
+        T = numbers[i:i+K+j]
+        L = len(T)
+        M = sum(T)/L
+        answer = min(answer, (sum(map(lambda x:(x-M)**2, T))/L)**0.5)
+
+print(answer)
