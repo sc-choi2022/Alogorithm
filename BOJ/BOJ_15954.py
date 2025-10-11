bea7a81f62d420ab37a152c35634b02b266f3722
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-11 23:38:18 +0900

    1011

--- BOJ/BOJ_15954.py
+++ BOJ/BOJ_15954.py
@@ -1,17 +1,27 @@
+from decimal import Decimal
+from math import sqrt
 import sys
+
+def solution(n, k):
+    global answer
+
+    S = [0] * (N+1)
+    SQ = S[::]
+    for i in range(1, N+1):
+        S[i] = S[i-1] + numbers[i-1]
+        SQ[i] = SQ[i-1] + numbers[i-1]**2
+
+    for j in range(K, N+1):
+        for k in range(N-K+1):
+            M = Decimal(S[k+K]-S[k])/K
+            V = Decimal(SQ[k+K]-SQ[k])/K - M*M
+            answer = min(answer, V)
+    return sqrt(answer)
 
 # 인형의 개수 N, 연속된 개수 K
 N, K = map(int, sys.stdin.readline().split())
 # 인형을 선호하는 사람의 수 numbers
 numbers = list(map(int, sys.stdin.readline().split()))
 
-answer = float('inf')
-
-for i in range(N-K+1):
-    for j in range(N-K-i+2):
-        T = numbers[i:i+K+j]
-        L = len(T)
-        M = sum(T)/L
-        answer = min(answer, (sum(map(lambda x:(x-M)**2, T))/L)**0.5)
-
-print(answer)
+answer = Decimal('inf')
+print(solution(numbers, K))
