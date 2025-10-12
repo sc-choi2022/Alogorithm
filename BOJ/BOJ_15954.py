3b1c452e7f1619b9f16ef3a0d9b9f0ad3adf1125
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-12 23:58:56 +0900

    1012

--- BOJ/BOJ_15954.py
+++ BOJ/BOJ_15954.py
@@ -2,26 +2,26 @@ from decimal import Decimal
 from math import sqrt
 import sys
 
-def solution(n, k):
-    global answer
-
-    S = [0] * (N+1)
-    SQ = S[::]
+def solution(A):
+    sums = [0 for _ in range(N+1)]
+    sq_sums = sums[:]
     for i in range(1, N+1):
-        S[i] = S[i-1] + numbers[i-1]
-        SQ[i] = SQ[i-1] + numbers[i-1]**2
-
+        sums[i] = sums[i-1] + A[i-1]
+        sq_sums[i] = sq_sums[i-1] + A[i-1]**2
+    min_var = Decimal('INF')
     for j in range(K, N+1):
-        for k in range(N-K+1):
-            M = Decimal(S[k+K]-S[k])/K
-            V = Decimal(SQ[k+K]-SQ[k])/K - M*M
-            answer = min(answer, V)
-    return sqrt(answer)
+        for l in range(N-j+1):
+            m = Decimal(sums[l+j] - sums[l]) / j
+            var = Decimal(sq_sums[l+j] - sq_sums[l]) / j - m*m
+            min_var = min(min_var, var)
+    return min_var.sqrt()
 
 # 인형의 개수 N, 연속된 개수 K
 N, K = map(int, sys.stdin.readline().split())
 # 인형을 선호하는 사람의 수 numbers
 numbers = list(map(int, sys.stdin.readline().split()))
 
+# 선택된 인형들을 선호하는 사람의 수의 표준 편차 answer
 answer = Decimal('inf')
-print(solution(numbers, K))
+# answer 출력
+print(solution(numbers))
