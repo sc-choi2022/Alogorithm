71abaeb6c93e6c4683cd038425fa645f55dec51e
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-20 23:58:15 +0900

    1020

--- BOJ/BOJ_1590.py
+++ BOJ/BOJ_1590.py
@@ -2,5 +2,6 @@ import sys
 
 # 버스의 개수 N, 터미널에 도착하는 시간 T
 N, T = map(int, sys.stdin.readline().split())
-# 버스의 정보 B
-B = [tuple(map(int, sys.stdin.readline())) for _ in range(N)]
+
+for _ in range(N):
+    S, I, C = map(int, sys.stdin.readline().split())
