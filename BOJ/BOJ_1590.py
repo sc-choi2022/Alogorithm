c4421f03937297fd0fa3f99434e7f6cc7193d441
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-24 23:44:19 +0900

    1024

--- BOJ/BOJ_1590.py
+++ BOJ/BOJ_1590.py
@@ -2,15 +2,24 @@ import sys
 
 # 버스의 개수 N, 터미널에 도착하는 시간 T
 N, T = map(int, sys.stdin.readline().split())
+result = []
 
 for _ in range(N):
     S, I, C = map(int, sys.stdin.readline().split())
     time = [S+(I*C) for c in range(C)]
     if time[-1] < T:
         continue
-    start, end = 0, C-1
+    start, end, answer = 0, C-1, 0
     while start <= end:
         mid = (start+end)//2
         if time[mid] >= T:
             answer = mid
             end = mid - 1
+        else:
+            start = mid + 1
+    result.append(time[answer]-T)
+
+if result:
+    print(min(result))
+else:
+    print(-1)
