76029b00e8806cbcbd75ab6b82810d55276a4a56
sc-choi2022 <c.martin20222023@gmail.com>
2025-10-29 23:41:03 +0900

    1029

--- BOJ/BOJ_28215.py
+++ BOJ/BOJ_28215.py
@@ -1,9 +1,14 @@
+from itertools import combinations
 import sys
 
 # 집의 개수 N, 설치하는 대피소의 개수 K
 N, K = map(int, sys.stdin.readline().split())
 
 x, y = [0] * N, [0] * N
-
 for i in range(N):
     x[i], y[i] = map(int, sys.stdin.readline().split())
+
+INF = float('INF')
+
+for c in combinations(range(N), K):
+    continue
