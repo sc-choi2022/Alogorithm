c8c6f9c015e77a7b066c3e42fcd19b6a2e03cfc1
sc-choi2022 <c.martin20222023@gmail.com>
2025-11-05 22:20:18 +0900

    1105

--- BOJ/BOJ_28215.py
+++ BOJ/BOJ_28215.py
@@ -1,10 +1,15 @@
 from itertools import combinations
 import sys
 
-def solve():
+def solve(B):
     b = 0
     for idx in range(N):
         a = INF
+        for b in B:
+            tmp = abs(x[idx]-x[b]) + abs(y[idx]-y[b])
+            a = min(a, tmp)
+        b = max(b, a)
+    return b
 
 # 집의 개수 N, 설치하는 대피소의 개수 K
 N, K = map(int, sys.stdin.readline().split())
@@ -14,3 +19,8 @@ for i in range(N):
     x[i], y[i] = map(int, sys.stdin.readline().split())
 
 INF = float('INF')
+
+answer = INF
+for c in combinations(range(N), K):
+    answer = min(answer, solve(c))
+print(answer)
