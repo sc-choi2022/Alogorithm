199f73cb3e1481d656818b5de4df8923fb0d076f
sc-choi2022 <c.martin20222023@gmail.com>
2025-11-14 23:33:58 +0900

    1114

---
+++ BOJ/BOJ_16499.py
@@ -0,0 +1,14 @@
+import sys
+
+# 단어의 개수 N
+N = int(sys.stdin.readline())
+
+# 단어들을 저장하는 셋 words
+words = set()
+for _ in range(N):
+    word = list(sorted(sys.stdin.readline().rstrip()))
+    word = ''.join(word)
+    words.add(word)
+
+# 그룹의 최소 개수를 출력
+print(len(words))
