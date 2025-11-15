5a370ad01e0f2e9a7151d61630f24d4940372a06
sc-choi2022 <c.martin20222023@gmail.com>
2025-11-15 23:53:29 +0900

    1115

--- BOJ/BOJ_16499.py
+++ BOJ/BOJ_16499.py
@@ -6,6 +6,7 @@ N = int(sys.stdin.readline())
 # 단어들을 저장하는 셋 words
 words = set()
 for _ in range(N):
+    # 단어 word
     word = list(sorted(sys.stdin.readline().rstrip()))
     word = ''.join(word)
     words.add(word)
