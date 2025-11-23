0475d6a456cd3bcfa6b847a5719aae8d1e57cd18
sc-choi2022 <c.martin20222023@gmail.com>
2025-11-23 23:56:21 +0900

    1123

---
+++ BOJ/BOJ_6986.py
@@ -0,0 +1,11 @@
+import sys
+
+# 점수의 개수 N, 제외되는 점수의 개수 K
+N, K = map(int, sys.stdin.readline().split())
+# 점수를 저장하는 배열 scores
+scores = list(sorted(float(sys.stdin.readline()) for _ in range(N)))
+
+# 절사 평균(N, K)
+print('{:.2f}'.format(sum(scores[K:N-K])/(N-2*K)))
+# 보정 평균(N, K)
+print('{:.2f}'.format(sum(scores[K:N-K])+scores[K]*K+scores[N-K-1]*K)/N)
