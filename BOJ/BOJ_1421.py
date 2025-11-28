c8d49a0a5b3cbce5e602e24540a1ef4e9c40b13e
sc-choi2022 <c.martin20222023@gmail.com>
2025-11-28 23:41:24 +0900

    1128

--- BOJ/BOJ_1421.py
+++ BOJ/BOJ_1421.py
@@ -1,7 +1,9 @@
 from math import ceil
 import sys
 
+# 나무의 개수 N, 나무를 자를 때 드는 비용 C, 나무한 단위의 가격 W
 N, C, W = map(int, sys.stdin.readline().split())
+# 나무의 길이 trees
 trees = [int(sys.stdin.readline()) for _ in range(N)]
 profits = []
 
