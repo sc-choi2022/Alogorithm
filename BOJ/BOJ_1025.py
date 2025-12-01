eb1e75ff1999c4576426e124971baed28a55df8b
sc-choi2022 <c.martin20222023@gmail.com>
2025-12-01 23:42:57 +0900

    1201

--- BOJ/BOJ_1025.py
+++ BOJ/BOJ_1025.py
@@ -7,3 +7,9 @@ N, M = map(int, sys.stdin.readline().split())
 numbers = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
 
 answer = -1
+
+for i in range(N):
+    for j in range(N):
+        for row in range(-N, N):
+            for col in range(-M, M):
+                continue
