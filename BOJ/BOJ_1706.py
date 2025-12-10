15b79bcdb7120dad36c6b26263f30243700126f8
sc-choi2022 <c.martin20222023@gmail.com>
2025-12-10 23:52:10 +0900

    1210

--- BOJ/BOJ_1706.py
+++ BOJ/BOJ_1706.py
@@ -2,4 +2,12 @@ import sys
 
 # 퍼즐의 길이 R, C
 R, C = map(int, sys.stdin.readline().split())
-words = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
+# 크로스워드 퍼즐
+puzzle = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
+
+# 단어를 저장하는 셋 words
+words = set()
+
+for i in range(R):
+    for j in range(C):
+        continue
