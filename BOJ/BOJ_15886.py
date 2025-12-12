71da968ea7bb5273b5ea57457d32314d8a720795
sc-choi2022 <c.martin20222023@gmail.com>
2025-12-12 23:44:35 +0900

    1212

--- BOJ/BOJ_15886.py
+++ BOJ/BOJ_15886.py
@@ -1,8 +1,7 @@
 import sys
 
-# 골목의 길이 N
-N = int(sys.stdin.readline())
-# 구사과가 있는 곳의 지도 zido
-zido = list(sys.stdin.readline().rstrip())
+def solution():
+    return
 
-answer = N
+N = int(sys.stdin.readline())
+cmd = sys.stdin.readline().rstrip()
