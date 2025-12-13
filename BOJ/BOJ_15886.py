bb1c1e76244743a38b7b0827325bcd20a7f084ff
sc-choi2022 <c.martin20222023@gmail.com>
2025-12-13 23:57:46 +0900

    1213

--- BOJ/BOJ_15886.py
+++ BOJ/BOJ_15886.py
@@ -1,7 +1,12 @@
 import sys
 
-def solution():
-    return
+def solution(A, S):
+    idx, cnt = 0, 0
+    for i in range(N):
+        if S[idx] == 'W' and S[idx-1] == 'E':
+            cnt += 1
+    return cnt
 
 N = int(sys.stdin.readline())
 cmd = sys.stdin.readline().rstrip()
+answer = sol(N, cmd)
