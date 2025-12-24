b6ce70c91ef77985a212c486bf47384758a4f326
sc-choi2022 <c.martin20222023@gmail.com>
2025-12-24 23:52:27 +0900

    1224

--- BOJ/BOJ_16924.py
+++ BOJ/BOJ_16924.py
@@ -2,12 +2,18 @@ from copy import deepcopy
 import sys
 
 def find():
+    for fi in range(1, N):
+        for fj in range(1, M):
+            if board[fi][fj] == '*':
+                return 
     return
 
 # 격자판의 크기 N, M
 N, M = map(int, sys.stdin.readline().split())
 # 격자판의 상태 board
 board = [sys.stdin.readline().rstrip() for _ in range(N)]
+tmp = deepcopy(board)
+visite = [[0]*N for _ in range(M)]
 
 # 필요한 십자가의 수 K
 K = 0
