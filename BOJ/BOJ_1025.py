bc5bfa5acadea384d6e37ca03395fa2739d73968
sc-choi2022 <c.martin20222023@gmail.com>
2026-01-16 23:55:09 +0900

    0116

--- BOJ/BOJ_1025.py
+++ BOJ/BOJ_1025.py
@@ -8,25 +8,4 @@ numbers = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
 
 answer = -1
 
-for i in range(N):
-    for j in range(N):
-        for row in range(-N, N):
-            for col in range(-M, M):
-                if row == 0 and col == 0:
-                    continue
-                step = 0
-                x, y = M, N
-                val = ''
-
-                while (0 <= x < M) and (0 <= y < N):
-                    val += str(numbers[x][y])
-                    step += 1
-
-                    val_int = int(''.join(val))
-                    val_sqrt = (val_int)**0.5
-                    val_deci = val_sqrt - int(val_sqrt)
-                    if val_deci == 0 and val_int > answer:
-                        answer = val_int
-                    x = M + step * row
-                    y = N + step * col
 print(answer)
