a4e77729e300c6dabd9afb8508b1b2e889c967be
sc-choi2022 <c.martin20222023@gmail.com>
2026-01-19 23:49:09 +0900

    0119

--- BOJ/BOJ_11507.py
+++ BOJ/BOJ_11507.py
@@ -7,3 +7,21 @@ cnt = defaultdict(int)
 
 for i in range(0, len(S), 3):
     cnt[S[i:i+3]] += 1
+
+lst = [c for c in cnt.values() if c != 1]
+
+if len(lst):
+    print('GRESKA')
+else:
+    P, K, H, T = 13, 13, 13, 13
+
+    for c in cnt.keys():
+        if c[0] == 'P':
+            P -= 1
+        elif c[0] == 'K':
+            K -= 1
+        elif c[0] == 'H':
+            H -= 1
+        elif c[0] == 'T':
+            T -= 1
+    print(P, K, H, T)
