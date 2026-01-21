2f0072e1c0281e903a1516a531c5b57b943a3de5
sc-choi2022 <c.martin20222023@gmail.com>
2026-01-21 23:47:14 +0900

    0121

--- BOJ/BOJ_11507.py
+++ BOJ/BOJ_11507.py
@@ -3,8 +3,10 @@ import sys
 
 # 카드의 이름 S
 S = sys.stdin.readline().rstrip()
+# 카드의 개수를 저장하는 딕셔너리 cnt
 cnt = defaultdict(int)
 
+# 딕셔너리에 카드의 정보를 저장
 for i in range(0, len(S), 3):
     cnt[S[i:i+3]] += 1
 
@@ -13,7 +15,7 @@ lst = [c for c in cnt.values() if c != 1]
 # 똑같은 카드가 존재하는 경우 GRESKA 출력
 if len(lst):
     print('GRESKA')
-# 그렇지 않은 경우 P, K, H, T 출력
+# 그렇지 않은 경우
 else:
     P, K, H, T = 13, 13, 13, 13
 
@@ -26,4 +28,5 @@ else:
             H -= 1
         elif c[0] == 'T':
             T -= 1
+    # P, K, H, T 출력
     print(P, K, H, T)
