a4be41207e68a9409299baca46046dc296720609
sc-choi2022 <c.martin20222023@gmail.com>
2026-01-22 23:41:11 +0900

    0122

--- BOJ/BOJ_11507.py
+++ BOJ/BOJ_11507.py
@@ -6,10 +6,12 @@ S = sys.stdin.readline().rstrip()
 # 카드의 개수를 저장하는 딕셔너리 cnt
 cnt = defaultdict(int)
 
+
 # 딕셔너리에 카드의 정보를 저장
 for i in range(0, len(S), 3):
     cnt[S[i:i+3]] += 1
 
+# 똑같은 가드가 존재하는 경우를 저장하는 배열 lst
 lst = [c for c in cnt.values() if c != 1]
 
 # 똑같은 카드가 존재하는 경우 GRESKA 출력
