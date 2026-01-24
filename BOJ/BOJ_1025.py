402147765e3fe229dfc45e1a0f6e7c28dbb23917
sc-choi2022 <c.martin20222023@gmail.com>
2026-01-24 23:58:47 +0900

    0124

--- BOJ/BOJ_1025.py
+++ BOJ/BOJ_1025.py
@@ -5,4 +5,10 @@ numbers = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
 
 answer = -1
 
+for i in range(N):
+    for j in range(M):
+        for row in range(-N, N):
+            for col in range(-M, M):
+                continue
+
 print(answer)
