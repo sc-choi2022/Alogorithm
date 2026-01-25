39f6e349ec26d07e39f6d96524d2a5876e3bee37
sc-choi2022 <c.martin20222023@gmail.com>
2026-01-25 23:56:57 +0900

    0125

--- BOJ/BOJ_1025.py
+++ BOJ/BOJ_1025.py
@@ -9,6 +9,12 @@ for i in range(N):
     for j in range(M):
         for row in range(-N, N):
             for col in range(-M, M):
-                continue
+                S = ''
+                x, y = i, j
+                if row == 0 and col == 0:
+                    continue
+                while 0 <= x < N and 0 <= y < M:
+                    S += numbers[x][y]
+                    
 
 print(answer)
