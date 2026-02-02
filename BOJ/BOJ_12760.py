7db90ae0a0576f2c13770ab9e70561aaf8b2498d
sc-choi2022 <c.martin20222023@gmail.com>
2026-02-02 23:56:25 +0900

    0202

--- BOJ/BOJ_12760.py
+++ BOJ/BOJ_12760.py
@@ -3,9 +3,13 @@ import sys
 # 플레이어의 수 N, 가진 카드 수 M
 N, M = map(int, sys.stdin.readline().split())
 cards = [sorted(list(map(int, sys.stdin.readline().split()))) for _ in range(N)]
+answer = [0] * N
 
 for i in range(M):
     tmp = []
     for j in range(N):
         tmp.append(cards[j][i])
     M = max(tmp)
+    for k in range(N):
+        if M == tmp[k]:
+            answer[k] += 1
