5c994d5beaf0df67eeb896e0ad3b75887a0a673b
sc-choi2022 <c.martin20222023@gmail.com>
2026-02-05 23:47:36 +0900

    0205

--- BOJ/BOJ_25193.py
+++ BOJ/BOJ_25193.py
@@ -5,8 +5,9 @@ N = int(sys.stdin.readline())
 # 음식의 리스트인 길이 N의 문자열 S
 S = sys.stdin.readline().rstrip()
 
+# C의 개수 a
 a = S.count('C')
-b = len(S) - a
+b = N - a
 
 # 연속으로 치킨을 먹는 날의 최댓값의 최솟값 출력
 print(math.ceil(a/(b+1)))
