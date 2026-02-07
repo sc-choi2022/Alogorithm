bf2ea0cc4a15e5db3a654753a52dfc417c0cd625
sc-choi2022 <c.martin20222023@gmail.com>
2026-02-07 23:56:34 +0900

    0207

--- BOJ/BOJ_25193.py
+++ BOJ/BOJ_25193.py
@@ -1,4 +1,5 @@
-import math, sys
+from math import ceil
+import sys
 
 # 식단을 정할 일수 N
 N = int(sys.stdin.readline())
@@ -7,7 +8,6 @@ S = sys.stdin.readline().rstrip()
 
 # C의 개수 a
 a = S.count('C')
-b = N - a
 
 # 연속으로 치킨을 먹는 날의 최댓값의 최솟값 출력
-print(math.ceil(a/(b+1)))
+print(ceil(a/(N-a+1)))
