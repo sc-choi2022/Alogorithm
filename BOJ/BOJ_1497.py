ca58aa8cba6dd43e6b4bba0733e7401e4173e0a6
sc-choi2022 <c.martin20222023@gmail.com>
2026-02-15 23:45:38 +0900

    0215

--- BOJ/BOJ_1497.py
+++ BOJ/BOJ_1497.py
@@ -3,5 +3,12 @@ import sys
 # 기타의 개수 N, 곡의 개수 M
 N, M = map(int, sys.stdin.readline().split())
 
+guitar = []
 for _ in range(N):
-    continue
+    name, songs = sys.stdin.readline().rstrip().split()
+    bi = ''
+
+    for song in songs:
+        bi += '1' if song == 'Y' else '0'
+    guitar.append((name, int(bi, 2)))
+    
