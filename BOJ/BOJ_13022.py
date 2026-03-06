2c9f59c93b0bdad88d29aa6db7bc06c75283f023
sc-choi2022 <c.martin20222023@gmail.com>
2026-03-06 22:49:17 +0900

    0306

--- BOJ/BOJ_13022.py
+++ BOJ/BOJ_13022.py
@@ -1,13 +1,19 @@
 import sys
 
+def solve():
+    idx = 0
+    for s in S:
+        if s == right[idx]:
+            continue
+        elif s == right[(idx + 1) % 4]:
+            idx = (idx + 1) % 4
+        else:
+            return 0
+    return 1
+
 # 단어 S
 S = sys.stdin.readline().rstrip()
 
 right = 'wolf'
-idx = 0
 
-for s in S:
-    if s == right[idx]:
-        continue
-    elif s == right[(idx+1)%4]:
-        idx = (idx+1)%4
+print(solve())
