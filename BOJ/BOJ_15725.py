9acd4f63b8583121e4323bb6a35c9593491b51c6
sc-choi2022 <c.martin20222023@gmail.com>
2026-03-10 23:26:39 +0900

    0310

--- BOJ/BOJ_15725.py
+++ BOJ/BOJ_15725.py
@@ -1,7 +1,16 @@
 import sys
 
+# 일변수 다항식
 E = sys.stdin.readline().rstrip()
 
 if E[:2] == '-x':
     E = E.replace('-x', '-1x')
 
+# E의 미분한 결과를 출력
+if 'x' in E:
+    if E[0] == 'x':
+        print(1)
+    else:
+        print(list(map(str, E.split('x')))[0])
+else:
+    print(0)
