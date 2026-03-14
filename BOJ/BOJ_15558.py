d292a575adc0886d3d27056bc24c510f6950c8e3
sc-choi2022 <c.martin20222023@gmail.com>
2026-03-14 23:58:22 +0900

    0314

--- BOJ/BOJ_15558.py
+++ BOJ/BOJ_15558.py
@@ -2,7 +2,9 @@ from collections import deque
 import sys
 
 def bfs():
-    queue = deque()
+    queue = deque([0, 0])
+    time = 0
+    return 
 
 N, K = map(int, sys.stdin.readline().split())
 info = [sys.stdin.readline().rstrip() for _ in range(K)]
