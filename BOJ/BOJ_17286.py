57e06a1ac0cbcc585096f37c7c8a96bb28259abd
sc-choi2022 <c.martin20222023@gmail.com>
2026-03-28 23:55:10 +0900

    0328

--- BOJ/BOJ_17286.py
+++ BOJ/BOJ_17286.py
@@ -4,6 +4,8 @@ import sys
 cat = tuple(map(int, sys.stdin.readline().split()))
 people = list(list(map(int, sys.stdin.readline().split())) for _ in range(3))
 
-move = 1e9
+M_move = 1e9
 for p in permutation([1, 2, 3], 3):
-    continue
+    x, y = people[0][0], people[0][1]
+
+    move = 0
