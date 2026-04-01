ac0b8e4c5630fd9c8633c734523823828f86e36c
sc-choi2022 <c.martin20222023@gmail.com>
2026-04-01 23:49:55 +0900

    0401

--- BOJ/BOJ_13022.py
+++ BOJ/BOJ_13022.py
@@ -3,13 +3,13 @@ import sys
 def solve():
     # 위치를 확인하는 변수 idx
     idx = 0
-    for s in S:
-        if s == right[idx]:
-            continue
-        elif s == right[(idx + 1) % 4]:
-            idx = (idx + 1) % 4
-        else:
-            return 0
+    # for s in S:
+    #     if s == right[idx]:
+    #         continue
+    #     elif s == right[(idx + 1) % 4]:
+    #         idx = (idx + 1) % 4
+    #     else:
+    #         return 0
     if right[idx] != 'f':
         return 0
     return 1
