89d0715e948323d93f510bc5368b14b33dd48964
sc-choi2022 <c.martin20222023@gmail.com>
2026-04-07 23:55:11 +0900

    0407

--- BOJ/BOJ_13022.py
+++ BOJ/BOJ_13022.py
@@ -1,14 +1,8 @@
 import sys
 
-def solve():
-    # 위치를 확인하는 변수 idx
-    idx = 0
-    return 
-
 # 단어 S
 S = sys.stdin.readline().rstrip()
 
 right = 'wolf'
 
 # 입력으로 주어진 단어가 올바른 단어인 경우에는 1을, 아니면 0을 출력
-print(solve())
