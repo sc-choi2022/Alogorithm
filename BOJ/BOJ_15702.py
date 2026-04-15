e0e3fb3481c7706886db721ded618685f15ab0fd
sc-choi2022 <c.martin20222023@gmail.com>
2026-04-15 23:53:48 +0900

    0415

--- BOJ/BOJ_15702.py
+++ BOJ/BOJ_15702.py
@@ -7,3 +7,15 @@ top_score = -1
 top_id = None
 
 for _ in range(M):
+    data = sys.stdin.readline().split()
+    student_id, result = int(data[0]), data[1:]
+
+    total = 0
+    for i in range(N):
+        if result[i] == 'O':
+            total += scores[i]
+    if total > top_score or (total == top_score and student_id < top_id):
+        top_score = total
+        top_id = student_id
+
+print(top_id, top_score)
