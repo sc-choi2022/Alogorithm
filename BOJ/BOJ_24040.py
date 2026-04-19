6a99304ca01b333d0c3260a3d05d911f211d192e
sc-choi2022 <c.martin20222023@gmail.com>
2026-04-19 23:49:48 +0900

    0419

--- BOJ/BOJ_24040.py
+++ BOJ/BOJ_24040.py
@@ -4,9 +4,12 @@ import sys
 T = int(sys.stdin.readline())
 
 for _ in range(T):
+    # 부피 N
     N = int(sys.stdin.readline())
 
+    # 부피 N인 예쁜 케이크를 만들 수 있는 경우
     if N%9 == 0 or N%3 == 2:
         print('TAK')
+    # 만들 수 없는 경우
     else:
         print('NIE')
