3f4f7e3f2085fd6ad5cf9ad7caa5570bb8aab6f0
sc-choi2022 <c.martin20222023@gmail.com>
2026-04-20 23:59:43 +0900

    0420

--- BOJ/BOJ_13022.py
+++ BOJ/BOJ_13022.py
@@ -9,18 +9,7 @@ for i in range(1, 14):
 S = sys.stdin.readline().rstrip()
 flag, idx = True, 0
 
-while idx < len(S):
-    tmp = False
 
-    for word in word_li:
-        if idx + len(word) <= len(S) and S[idx:idx+len(word)] == word:
-            idx += len(word)
-            tmp = True
-            break
-
-    if not tmp:
-        flag = False
-        break
 
 # 입력으로 주어진 단어가 올바른 단어인 경우에는 1을, 아니면 0을 출력
 print(1 if flag else 0)
