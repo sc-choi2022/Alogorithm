fee3339040761c3eb808a12bc77d02bbace03549
sc-choi2022 <c.martin20222023@gmail.com>
2026-04-24 23:53:17 +0900

    0424

--- BOJ/BOJ_15702.py
+++ BOJ/BOJ_15702.py
@@ -1,6 +1,8 @@
 import sys
 
+# 문제의 개수 N, 응시자의 수 M
 N, M = map(int, sys.stdin.readline().split())
+# 문제의 배점을 저장하는 배열 scores
 scores = list(map(int, sys.stdin.readline().split()))
 
 top_score = -1
@@ -18,4 +20,6 @@ for _ in range(M):
         top_score = total
         top_id = student_id
 
+# 가장 높은 점수를 얻은 학생의 번호와 점수를 공백으로 구분해 출력
+# 가장 높은 점수를 얻은 학생이 여러 명이라면, 수험 번호가 가장 작은 것을 출력
 print(top_id, top_score)
