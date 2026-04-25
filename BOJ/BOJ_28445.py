729da3b9d853c50bd29a99971b0dc4671da98190
sc-choi2022 <c.martin20222023@gmail.com>
2026-04-25 23:59:06 +0900

    0425

--- BOJ/BOJ_28445.py
+++ BOJ/BOJ_28445.py
@@ -4,12 +4,14 @@ import sys
 colors = set()
 
 for _ in range(2):
+    # 부모 새의 몸통 색과 꼬리 색 C1, C2
     C1, C2 = sys.stdin.readline().rstrip().split()
     colors.add(C1)
     colors.add(C2)
 
 colors = sorted(list(colors))
 
+# 자식 새의 몸통 색과 꼬리 색의 쌍을 한 줄에 하나씩 사전 순으로 출력
 for c1 in colors:
     for c2 in colors:
         print(c1, c2)
