2bae1b3e81ad564c27995de9ed03503f6a0c3851
sc-choi2022 <c.martin20222023@gmail.com>
2026-05-04 23:47:23 +0900

    0504

--- BOJ/BOJ_1014.py
+++ BOJ/BOJ_1014.py
@@ -8,4 +8,9 @@ for _ in range(C):
     N, M = map(int, sys.stdin.readline().split())
     # 자리를 저장하는 배열 seat
     seat = [list(sys.stdin.readline()) for _ in range(N)]
-    visit = [[0]*M for _ in range(N)]
+
+    make(0, 0, M)
+    make(0, 1, M)
+    make_dp(0, 0, N, M)
+
+    print(dp[0][0])
